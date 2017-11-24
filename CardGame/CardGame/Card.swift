09b610c3145774a24f3c93befbdcde1298d987f4
ChocOZerO <nayalth@gmail.com>
2017-11-24 14:46:35 +0900

    [Comment]카드 클래스 주석 및 공백 정리

--- CardGame/CardGame/Card.swift
+++ CardGame/CardGame/Card.swift
@@ -11,6 +11,7 @@ import Foundation
 class Card {
     let suit: Suit, rank: Rank
 
+    // 카드의 모양 4종류 중 하나를 채택하기 위해 enum 사용.
     enum Suit: String {
         case spades = "\u{2660}"
         case hearts = "\u{2665}"
@@ -18,10 +19,12 @@ class Card {
         case clubs = "\u{2663}"
     }
 
+    // 카드의 숫자를 1~13까지로 고정하기 위해 enum 사용.
     enum Rank: Int {
         case ace = 1, two, three, four, five, six, seven, eight, nine, ten
         case jack, queen, king
 
+        // 1, 11, 12, 13 은 특수하게 숫자대신 문자 출력
         var value: String {
             switch self {
             case .ace:
@@ -46,4 +49,5 @@ class Card {
     var description: String {
         return "\(suit.rawValue)\(rank.value)"
     }
+
 }
