c8419c6f5a802068fcb6007c19888a7ff8f1caee
undervine <undervine@naver.com>
2017-11-27 10:53:10 +0900

    Print test card

--- CardGame/CardGame/main.swift
+++ CardGame/CardGame/main.swift
@@ -3,9 +3,16 @@
 //  CardGame
 //
 //  Created by JK on 10/10/2017.
-//  Copyright © 2017 JK. All rights reserved.
+//  Copyright © 2017 심 승민. All rights reserved.
 //
 
 import Foundation
 
+func main() {
+    let shape = Shape.spades
+    let number = Number.king
+    let testCard = Card(shape, number)
+    print(testCard.description)
+}
 
+main()
