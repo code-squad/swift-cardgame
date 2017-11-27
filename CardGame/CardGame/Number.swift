fa515a2c4e39f2a40dbb61c50cf9d65d242d553e
undervine <undervine@naver.com>
2017-11-27 14:49:47 +0900

    Modify the order of enum cases
    Modify the order of enum cases, to matching rawValues start from 1 to 13.

--- CardGame/CardGame/Number.swift
+++ CardGame/CardGame/Number.swift
@@ -11,11 +11,9 @@ import Foundation
 // Number : King(13), Queen(12), Jack(11), 10 ~ 2, Ace(1) 중 하나
 // 선택이유 : 카드의 모양과 숫자는 선택지가 정해져 있으므로 enum 선택.
 enum Number: Int {
-    case king = 13
-    case queen
-    case jack
-    case ten, nine, eight, seven, six, five, four, three, two
-    case ace
+    case ace = 1
+    case two, three, four, five, six, seven, eight, nine, ten
+    case jack, queen, king
 }
 
 extension Number: CustomStringConvertible {
