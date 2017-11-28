d222113d91c937c3059d517a794a0dfd7b8bdca6
undervine <undervine@naver.com>
2017-11-28 17:37:22 +0900

    Add printResult() to print statements according to GameMenu

--- CardGame/CardGame/OutputView.swift
+++ CardGame/CardGame/OutputView.swift
@@ -7,3 +7,23 @@
 //
 
 import Foundation
+
+struct OutputView {
+    static func printResult(of menu: Dealer.GameMenu, using deck: Deck, _ cardSelected: Card?) {
+        switch menu {
+        case .reset:
+            print("카드 전체를 초기화했습니다.\n총 \(deck.count)장의 카드가 있습니다.")
+        case .shuffle:
+            if deck.count == 52 {
+                print("전체 52장의 카드를 섞었습니다.")
+            }else {
+                print("\(deck.count)장의 카드를 섞었습니다.")
+            }
+        case .remove:
+            guard let selectedCard = cardSelected else { break }
+            print(selectedCard.description)
+            print("총 \(deck.count)장의 카드가 남아있습니다.")
+        }
+    }
+
+}
