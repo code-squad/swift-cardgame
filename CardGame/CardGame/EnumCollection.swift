dd9a055e4013e224be5ce99c9da733bc88dfdd41
undervine <undervine@naver.com>
2017-11-28 17:34:48 +0900

    Make protocol, extension to get sequence of Enum elements

--- CardGame/CardGame/EnumCollection.swift
+++ CardGame/CardGame/EnumCollection.swift
@@ -7,3 +7,34 @@
 //
 
 import Foundation
+
+// enum 등 정수형 해시값 제공하는 타입에 적용 가능.
+protocol EnumCollection: Hashable {
+    // 자기자신 내부 요소로 sequence 만들어 반환.
+    static func cases() -> AnySequence<Self>
+    // sequence를 array로 타입캐스팅하여 반환.
+    static var allValues: [Self] { get }
+}
+
+// enum 내부 값을 배열로 가져오는 확장 기능.
+extension EnumCollection {
+    // 내부 값의 시퀀스 반환.
+    static func cases() -> AnySequence<Self>  {
+        // 전체 요소를 시퀀스로 만들어 반환.
+        return AnySequence { () -> AnyIterator<Self> in
+            var hash = 0
+            return AnyIterator {
+                defer { hash += 1 }
+                let currentElement = withUnsafePointer(to: &hash) {
+                    $0.withMemoryRebound(to: Self.self, capacity: 1) { $0.pointee }
+                }
+                guard currentElement.hashValue == hash else { return nil }
+                return currentElement
+            }
+        }
+    }
+    // 시퀀스를 배열로 캐스팅 후 반환.
+    static var allValues: [Self] {
+        return Array(self.cases())
+    }
+}
