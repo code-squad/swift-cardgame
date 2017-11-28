657cf20820c8d442e79a7632e0e051827a9acf61
undervine <undervine@naver.com>
2017-11-28 17:35:37 +0900

    Add start() to get result of the game

--- CardGame/CardGame/Dealer.swift
+++ CardGame/CardGame/Dealer.swift
@@ -7,3 +7,26 @@
 //
 
 import Foundation
+
+struct Dealer {
+    enum GameMenu: Int {
+        case reset = 1
+        case shuffle
+        case remove
+    }
+    enum CardGameError: String, Error {
+        case lackOfCards = "덱에 카드가 없습니다."
+    }
+
+    static func start(_ menu: GameMenu, with deck: inout Deck) throws -> Card? {
+        switch menu {
+        case .reset: deck.reset()
+        case .shuffle: deck.shuffle()
+        case .remove:
+            guard let selectedCard = deck.removeOne() else { throw CardGameError.lackOfCards  }
+            return selectedCard
+        }
+        return nil
+    }
+
+}
