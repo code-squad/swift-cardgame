9abcdb0380038be4c0fb5edeb2bac648e57cdadb
undervine <undervine@naver.com>
2017-11-30 16:00:01 +0900

    Put only statements that have 'try' keyword in do-catch phrase
    Modify askFor() in InputView struct not to return optional - to use try? in main() func.

--- CardGame/CardGame/InputView.swift
+++ CardGame/CardGame/InputView.swift
@@ -11,16 +11,17 @@ import Foundation
 struct InputView {
     enum InputError: String, Error {
         case invalidMenuNumber = "1,2,3 번 메뉴 중 선택해 주십시오."
+        case quit
     }
 
-    static func askFor(message: String) throws -> Dealer.GameMenu? {
+    static func askFor(message: String) throws -> Dealer.GameMenu {
         // 요구 메시지 출력.
         print("\(message)", terminator: " ")
         // q 또는 quit 입력 시 종료.
-        guard let inputLine = readLine(), inputLine != "q" && inputLine != "quit" else { return nil }
+        guard let inputLine = readLine(), inputLine != "q" && inputLine != "quit" else { throw InputError.quit }
         // 입력값이 숫자가 아니거나 1~3번을 제외한 수인 경우, 에러 전달.
         guard let menuNumber = Int(inputLine), menuNumber == 1 || menuNumber == 2 || menuNumber == 3 else { throw InputError.invalidMenuNumber }
-        return Dealer.GameMenu(rawValue: menuNumber)
+        return Dealer.GameMenu(rawValue: menuNumber)!
     }
     
 }
