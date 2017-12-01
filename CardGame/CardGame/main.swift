e2862ded821da48343b4ca35ce12e4f83f08b256
undervine <undervine@naver.com>
2017-12-01 16:18:23 +0900

    Call askFor() and handleMenus() separately

--- CardGame/CardGame/main.swift
+++ CardGame/CardGame/main.swift
@@ -9,13 +9,22 @@
 import Foundation
 
 func main() {
+    let demand = """
+                    다음 메뉴를 선택해주세요.
+                    1. 카드 초기화
+                    2. 카드 섞기
+                    3. 카드 하나 뽑기
+                    >
+                    """
     let deck = Deck()
     var dealer = Dealer(deck: deck)
     let pilesCount = 7
     while true {
+        var inputMenu: Dealer.GameMenu?
         var selectedCard: Card?
-        guard let userInput = try? InputView.handleMenus(), let inputMenu = userInput else { break }
+        guard let userInput = try? InputView.askFor(message: demand) else { break }
         do {
+            inputMenu = try InputView.handleMenus(input: userInput)
             selectedCard = try dealer.start(inputMenu)
         }catch {
             print(error)
