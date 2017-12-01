f9428fd27d4108f394465817177370319fa54ce5
undervine <undervine@naver.com>
2017-12-01 16:16:18 +0900

    Remove askFor() from handleMenus()

--- CardGame/CardGame/View/InputView.swift
+++ CardGame/CardGame/View/InputView.swift
@@ -14,23 +14,16 @@ struct InputView {
         case quit
     }
 
-    private static func askFor(message: String) -> String? {
+    static func askFor(message: String) throws -> String? {
         // 요구 메시지 출력.
         print("\(message)", terminator: " ")
         // q 또는 quit 입력 시 종료.
-        guard let inputLine = readLine(), inputLine != "q" && inputLine != "quit" else { return nil }
+        guard let inputLine = readLine(), inputLine != "q" && inputLine != "quit" else { throw InputError.quit }
         return inputLine
     }
 
-    static func handleMenus() throws -> Dealer.GameMenu? {
-        let demand = """
-                    다음 메뉴를 선택해주세요.
-                    1. 카드 초기화
-                    2. 카드 섞기
-                    3. 카드 하나 뽑기
-                    >
-                    """
-        guard let userInput = askFor(message: demand) else { return nil }
+    static func handleMenus(input: String?) throws -> Dealer.GameMenu? {
+        guard let userInput = input else { return nil }
         // 입력값이 숫자가 아니거나 1~3번을 제외한 수인 경우, 에러 전달.
         guard let menuNumber = Int(userInput), menuNumber == 1 || menuNumber == 2 || menuNumber == 3 else { throw InputError.invalidMenuNumber }
         return Dealer.GameMenu(rawValue: menuNumber)
