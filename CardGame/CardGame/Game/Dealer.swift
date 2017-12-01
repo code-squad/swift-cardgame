fdb0bb91e76a614d12d2162c0ccf5fd303061dff
undervine <undervine@naver.com>
2017-12-01 16:33:41 +0900

    Modify start() to shuffle deck and set card stacks
    Modify start() to get number of participants and card stud as parameter, and host the game (shuffle, set card stacks)
    Add resetCardStack() to make stacks empty when restart the game.
    Modify setCardStack() to generate dealer's card stack.
    Add enum Stud consist of 5, 7 cases.

--- CardGame/CardGame/Game/Dealer.swift
+++ CardGame/CardGame/Game/Dealer.swift
@@ -11,38 +11,41 @@ import Foundation
 struct Dealer {
     private(set) var deck: Deck
     private(set) var cardStacks: [CardStack]
-    init(deck: Deck) {
-        self.deck = deck
+    init() {
+        self.deck = Deck()
         self.cardStacks = []
     }
-    enum GameMenu: Int {
-        case reset = 1
-        case shuffle
-        case remove
+    static let participantMinimum = 1
+    static let participantMaximum = 4
+    enum Stud: Int {
+        case fiveCard = 5
+        case sevenCard = 7
     }
     enum CardGameError: String, Error {
         case lackOfCards = "덱에 카드가 없습니다."
     }
 
-    mutating func start(_ gameMenu: GameMenu?) throws -> Card? {
-        guard let menu = gameMenu else { return nil }
-        switch menu {
-        case .reset: self.deck.reset()
-        case .shuffle: self.deck.shuffle()
-        case .remove:
-            guard let selectedCard = self.deck.removeOne() else { throw CardGameError.lackOfCards  }
-            return selectedCard
-        }
-        return nil
+    mutating func start(_ numberOfPeople: Int, _ stud: Stud) throws {
+        let cardDemands = (numberOfPeople + 1) * stud.rawValue
+        guard self.deck.count - cardDemands > 0 else { throw CardGameError.lackOfCards }
+        self.deck.shuffle()
+        setCardStack(of: numberOfPeople, with: stud)
     }
 
-    mutating func setCardStack(of pileCount: Int) {
-        var pileCount = pileCount
-        for _ in 0..<pileCount {
-            defer { pileCount -= 1 }
-            guard let cardStack = self.deck.removeMany(selectedCount: pileCount) else { break }
+    private mutating func setCardStack(of numberOfPeople: Int, with stud: Stud) {
+        // 이전 게임 때 스택 지우기. (이전 게임에 사용된 카드는 제외)
+        resetCardStack()
+        var participants = numberOfPeople
+        // 딜러의 스택까지 포함하여 생성.
+        for _ in 0...participants {
+            defer { participants -= 1 }
+            guard let cardStack = self.deck.removeMany(selectedCount: stud.rawValue) else { break }
             self.cardStacks.append(cardStack)
         }
+    }
+
+    mutating func resetCardStack() {
+        self.cardStacks = []
     }
 
 }
