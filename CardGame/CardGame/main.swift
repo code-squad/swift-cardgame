e631a2b6da8a29a0c978058c660ccab18c658ca3
undervine <undervine@naver.com>
2017-12-01 13:45:16 +0900

    Separate handleMenu() from askFor() to make functions independent

--- CardGame/CardGame/main.swift
+++ CardGame/CardGame/main.swift
@@ -11,16 +11,9 @@ import Foundation
 func main() {
     let deck = Deck()
     var dealer = Dealer(deck: deck)
-    let demand = """
-                    다음 메뉴를 선택해주세요.
-                    1. 카드 초기화
-                    2. 카드 섞기
-                    3. 카드 하나 뽑기
-                    >
-                    """
     let pilesCount = 7
     while true {
-        guard let inputMenu = try? InputView.askFor(message: demand) else { break }
+        guard let inputMenu = try? InputView.handleMenus() else { break }
         var selectedCard: Card?
         do {
             selectedCard = try dealer.start(inputMenu)
