9217880178a5e3a8e462b9dc43b815bff0f0e6e9
undervine <undervine@naver.com>
2017-12-01 16:50:38 +0900

    Add functions getting stud and number of participants from users
    Add askForStud() to preprocess user input String to Dealer.Stud type.
    Add askForParticipants() to verify the number of participants in the range of 1 to 4.
    Make static methods to instacne method.

--- CardGame/CardGame/View/InputView.swift
+++ CardGame/CardGame/View/InputView.swift
@@ -10,11 +10,21 @@ import Foundation
 
 struct InputView {
     enum InputError: String, Error {
-        case invalidMenuNumber = "1,2,3 번 메뉴 중 선택해 주십시오."
+        case invalidMenuNumber = "잘못된 메뉴입니다. 다시 선택해 주세요."
+        case outOfParticipantRange = "참여 인원을 다시 입력해 주세요."
         case quit
     }
+    enum InputMessage: String {
+        case studMenu = """
+                        카드 게임 종류를 선택하세요.
+                        1. 7카드
+                        2. 5카드
+                        >
+                        """
+        case participantsMenu = "참여할 사람의 인원을 입력하세요.\n> "
+    }
 
-    static func askFor(message: String) throws -> String? {
+    func askFor(message: String) throws -> String? {
         // 요구 메시지 출력.
         print("\(message)", terminator: " ")
         // q 또는 quit 입력 시 종료.
@@ -22,11 +32,19 @@ struct InputView {
         return inputLine
     }
 
-    static func handleMenus(input: String?) throws -> Dealer.GameMenu? {
-        guard let userInput = input else { return nil }
-        // 입력값이 숫자가 아니거나 1~3번을 제외한 수인 경우, 에러 전달.
-        guard let menuNumber = Int(userInput), menuNumber == 1 || menuNumber == 2 || menuNumber == 3 else { throw InputError.invalidMenuNumber }
-        return Dealer.GameMenu(rawValue: menuNumber)
+    func askForStud(_ input: String?) throws -> Dealer.Stud? {
+        guard let userInput = input, let inputMenu = Int(userInput) else { throw InputError.invalidMenuNumber }
+        switch inputMenu {
+        case 1: return Dealer.Stud.sevenCard
+        case 2: return Dealer.Stud.fiveCard
+        default: throw InputError.invalidMenuNumber
+        }
+    }
+
+    func askForParticipants(_ input: String?) throws -> Int? {
+        guard let userInput = input, let numberOfPeople = Int(userInput) else { throw InputError.invalidMenuNumber }
+        guard numberOfPeople <= Dealer.participantMaximum && numberOfPeople >= Dealer.participantMinimum else { throw InputError.outOfParticipantRange }
+        return numberOfPeople
     }
     
 }
