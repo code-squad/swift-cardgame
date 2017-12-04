ba63947c06c29a30c9845a27ac8590f08c523f90
undervine <undervine@naver.com>
2017-12-04 10:18:29 +0900

    Modify some statements according to deck or cardStacks of dealer changed not accessible

--- CardGame/CardGame/main.swift
+++ CardGame/CardGame/main.swift
@@ -11,10 +11,12 @@ import Foundation
 func main() {
     var dealer = Dealer()
     let inputView = InputView()
-    while !dealer.deck.isEmpty {
+    while !dealer.isDeckEmpty {
         do {
             guard let participants = try inputView.askForParticipants(), let stud = try inputView.askForStud() else { break }
             try dealer.start(participants, stud)
+            // 덱에 카드가 모자른 경우, 에러만 찍고 끝내기 위해 do문 안에 작성. (do문 밖에 작성 시, 에러 출력 후 이전 카드스택을 출력하기 때문)
+            OutputView.printCardStacksWithName(by: dealer)
         }catch let e as InputView.InputError {
             print(e.rawValue)
         }catch let e as Dealer.GameError {
@@ -22,7 +24,6 @@ func main() {
         }catch {
             print(error)
         }
-        OutputView.printCardStacksWithName(by: dealer)
     }
 }
 
