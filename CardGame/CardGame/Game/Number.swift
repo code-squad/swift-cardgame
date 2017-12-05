a14e18076cd94d5fc0aab6ec35f8819baa8b40ec
undervine <undervine@naver.com>
2017-12-05 14:28:46 +0900

    Make structs conform Equatable, Comparable protocols

--- CardGame/CardGame/Game/Number.swift
+++ CardGame/CardGame/Game/Number.swift
@@ -11,9 +11,8 @@ import Foundation
 // Number : King(13), Queen(12), Jack(11), 10 ~ 2, Ace(1) 중 하나
 // 선택이유 : 카드의 모양과 숫자는 선택지가 정해져 있으므로 enum 선택.
 enum Number: Int, EnumCollection {
-    case ace = 1
-    case two, three, four, five, six, seven, eight, nine, ten
-    case jack, queen, king
+    case two = 2, three, four, five, six, seven, eight, nine, ten
+    case jack, queen, king, ace
 }
 
 extension Number: CustomStringConvertible {
@@ -27,4 +26,18 @@ extension Number: CustomStringConvertible {
         }
     }
 
+}
+
+extension Number: Equatable {
+    static func ==(lhs: Number, rhs: Number) -> Bool {
+        guard lhs.hashValue == rhs.hashValue else { return false }
+        return true
+    }
+}
+
+extension Number: Comparable {
+    static func <(lhs: Number, rhs: Number) -> Bool {
+        guard lhs.hashValue < rhs.hashValue else { return false }
+        return true
+    }
 }
