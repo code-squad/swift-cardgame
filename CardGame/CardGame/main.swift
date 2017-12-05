cfed23369fd05eb0cca9746a85dd1b896c7fe3b6
undervine <undervine@naver.com>
2017-12-05 14:05:06 +0900

    Add variable 'isDealer' to distinguish dealer player

--- CardGame/CardGame/main.swift
+++ CardGame/CardGame/main.swift
@@ -9,22 +9,27 @@
 import Foundation
 
 func main() {
-    var dealer = Dealer()
+    var game = StudPokerGame(.sevenCard, with: 1)
     let inputView = InputView()
-    while !dealer.isDeckEmpty {
+
+    // 카드가 다 떨어질 때까지 진행.
+    repeat {
         do {
-            guard let participants = try inputView.askForParticipants(), let stud = try inputView.askForStud() else { break }
-            try dealer.start(participants, stud)
+            guard let numberOfPeople = try inputView.askForParticipants(), let stud = try inputView.askForStud() else { break }
+            game = StudPokerGame(stud, with: numberOfPeople)
+            try game.begin()
             // 덱에 카드가 모자른 경우, 에러만 찍고 끝내기 위해 do문 안에 작성. (do문 밖에 작성 시, 에러 출력 후 이전 카드스택을 출력하기 때문)
-            OutputView.printCardStacksWithName(by: dealer)
+            OutputView.printCardStacks(of: game)
+//            OutputView.printCardStacksWithName(of: game)
         }catch let e as InputView.InputError {
             print(e.rawValue)
-        }catch let e as Dealer.GameError {
+        }catch let e as StudPokerGame.GameError {
             print(e.rawValue)
         }catch {
             print(error)
         }
-    }
+    }while(!game.isDeckEmpty)
+
 }
 
 main()
