0d488784da4543b5dcad8ab2ef90f95b39402947
undervine <undervine@naver.com>
2017-12-05 12:11:30 +0900

    Separate Dealer's role to a new struct StudPokerGame
    StudPokerGame struct manages Stud, GameErrors, etc.
    main() can access the game only by StudPokerGame object.

--- CardGame/CardGame/Game/Dealer.swift
+++ CardGame/CardGame/Game/Dealer.swift
@@ -10,78 +10,52 @@ import Foundation
 
 struct Dealer {
     private var deck: Deck
-    let cardStacks: CardStacks
-    init() {
-        self.deck = Deck()
-        self.cardStacks = CardStacks()
+    init(with deck: Deck) {
+        self.deck = deck
     }
-    var isDeckEmpty: Bool {
-        return self.deck.isEmpty
-    }
-    enum GameMenu: Int {
+    
+    enum Operation: Int {
         case reset = 1
         case shuffle
         case remove
     }
-    static let participantMinimum = 1
-    static let participantMaximum = 4
-    enum Stud: Int {
-        case fiveCard = 5
-        case sevenCard = 7
-    }
-    enum GameError: String, Error {
-        case lackOfCards = "덱에 카드가 없습니다."
-    }
 
-    mutating func start(_ numberOfPeople: Int, _ stud: Stud) throws {
-        let cardDemands = (numberOfPeople + 1) * stud.rawValue
-        guard self.deck.count - cardDemands > 0 else { throw GameError.lackOfCards }
+    mutating func introduce(_ players: [Player], _ stud: StudPokerGame.Stud) throws {
         self.deck.shuffle()
-        setCardStack(of: numberOfPeople, with: stud)
+        try self.dealCards(numberOf: stud.rawValue, to: players)
     }
 
-    private mutating func setCardStack(of numberOfPeople: Int, with stud: Stud) {
+    private mutating func dealCards(numberOf count: StudPokerGame.Stud.RawValue, to players: [Player]) throws {
         // 이전 게임 때 스택 지우기. (이전 게임에 사용된 카드는 제외)
-        self.cardStacks.reset()
-        var participants = numberOfPeople
+        self.resetStacksOf(players)
         // 딜러의 스택까지 포함하여 생성.
-        for _ in 0...participants {
-            defer { participants -= 1 }
-            guard let cardStack = self.deck.removeMany(selectedCount: stud.rawValue) else { break }
-            self.cardStacks.append(cardStack)
+        for player in players {
+            let cardStackForOnePlayer = try drawCards(numberOf: count)
+            player.take(cards: cardStackForOnePlayer)
         }
     }
 
-}
-
-
-class CardStacks: Sequence {
-    private let start: Int
-    private var cardStacks: [CardStack]
-    init() {
-        self.start = 0
-        self.cardStacks = []
-    }
-
-    func makeIterator() -> ClassIteratorOf<CardStack> {
-        return ClassIteratorOf(self.cardStacks)
-    }
-
-    subscript(index: Int) -> Iterator.Element? {
-        guard index > -1 && index < self.cardStacks.count else { return nil }
-        return self.cardStacks[index]
+    private func drawCards(numberOf count: StudPokerGame.Stud.RawValue) throws -> CardStack {
+        guard let newCardStack = self.deck.removeMany(selectedCount: count) else { throw StudPokerGame.GameError.lackOfCards }
+        return newCardStack
     }
 
     var endIndex: Int {
-        return self.cardStacks.endIndex
+        return self.endIndex
     }
 
-    fileprivate func append(_ cardStack: CardStack) {
-        self.cardStacks.append(cardStack)
+    private func append(_ cardStack: CardStack) {
+        self.append(cardStack)
     }
 
-    fileprivate func reset() {
-        self.cardStacks = []
+    private mutating func resetStacksOf(_ players: [Player]) {
+        for player in players {
+            self.resetStackOf(player)
+        }
+    }
+
+    private mutating func resetStackOf(_ player: Player) {
+        player.resetStack()
     }
 
 }
