f34a048d81c6ad49293b8c5711e3d84e422bf1d8
DongJin Lee <comets195@gmail.com>
2017-12-05 10:16:13 +0900

    Change init() Deck..

--- CardGame/CardGame/Deck.swift
+++ CardGame/CardGame/Deck.swift
@@ -9,24 +9,38 @@
 import Foundation
 
 struct Deck {
-    private var cards = [Card]()
+    private var cards: [Card]
+    
+    init() {
+        self.cards = [Card]()
+        for kindOfSuit in 0..<Card.Suit.allValues.count {
+            for kindOfRank in 0..<Card.Rank.allValues.count {
+                cards.append(Card(suit: kindOfSuit, rank: kindOfRank))
+            }
+        }
+    }
     
     func count() -> Int {
         return cards.count
     }
     
-    mutating func reset() {
-        self.cards = [Card]()
-        for kindOfSuit in 0..<Card.Suit.allValues.count {
-            for kindOfRank in 0..<Card.Rank.allValues.count {
-               cards.append(Card(suit: kindOfSuit, rank: kindOfRank))
-            }
-        }
+    mutating func reset() -> Deck {
+        let newDeck = Deck()
+        return newDeck
     }
     
     mutating func removeOne() -> Card? {
         return self.cards.popLast()
     }
     
-    // func shuffle() {}
+    mutating func shuffle() {
+        var sortedCards = self.cards
+        var suffledCards = [Card]()
+        for _ in cards {
+            let roll = arc4random_uniform(UInt32(sortedCards.count))
+            suffledCards.append(sortedCards[Int(roll)])
+            sortedCards.remove(at: Int(roll))
+        }
+        self.cards = suffledCards
+    }
 }
--- CardGame/CardGame/OutputView.swift
+++ CardGame/CardGame/OutputView.swift
@@ -13,6 +13,10 @@ struct OutputView {
         print("총 \(deck.count())장의 카드가 있습니다.\n")
     }
     
+    func countOfRestDeck(_ deck: Deck) {
+        print("전체 \(deck.count())장의 카드를 섞었습니다.\n")
+    }
+    
     func showMeTheCard(_ card: Card?) {
         guard let isCard = card else {
             errorMsg(.zeroCard)
@@ -26,6 +30,6 @@ struct OutputView {
     }
     
     func errorMsg(_ code: ErrorMsg) {
-        print("\(code)")
+        print("\(code.rawValue)")
     }
 }
