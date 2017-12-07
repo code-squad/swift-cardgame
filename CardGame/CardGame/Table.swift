6c708205c34fc6e71878b7df001c85e9c832c366
DongJin Lee <comets195@gmail.com>
2017-12-07 13:53:40 +0900

    Change class name and making propertys to use makeGameTable.

--- CardGame/CardGame/Table.swift
+++ CardGame/CardGame/Table.swift
@@ -8,20 +8,28 @@
 
 import Foundation
 
-class CardStack {
-    private (set) var stacks: [[Card]]
+// 카드게임시 가상의 테이블을 만드는 클래스.
+// 게임을 진행하려면 테이블이 필요한데 테이블은 플레이어들의 스택(카드뭉치)를 가지고있다.
 
-    init() {
-        self.stacks = [[Card]]()
-    }
+class Table {
+    private (set) var cardStacksOfTable: [[Card]]
+    private var gameInfo: CardGameInfo
     
-    func hasStack(deck: Deck) -> Bool {
+    // 게임의 타입과 플레이어의 숫자에 따라, 각자 가지고있는 카드의 수, 플레이어의 수에 맞추어
+    // 테이블을 세팅한다.
+    init(_ gameInfo: CardGameInfo) {
+        self.cardStacksOfTable = [[Card]]()
+        self.gameInfo = gameInfo
+    }
+
+    // 테이블 세팅은 하나의 deck을 공용으로 사용한다.
+    func makeGameTable(deck: Deck) -> Bool {
         var value = deck
-        for numberOfStack in 1...7 {
-            guard let element = try? value.makeStack(numberOfCards: numberOfStack) else {
+        for _ in 0..<gameInfo.players {
+            guard let playerStack = try? value.makeStack(numberOfCards: gameInfo.typeOfGames.rawValue) else {
                 return false
             }
-            stacks.append(element)
+            cardStacksOfTable.append(playerStack)
         }
         return true
     }
