d3444f9a411f62509ba6b2ee404c208fb982d002
DongJin Lee <comets195@gmail.com>
2017-12-07 20:21:49 +0900

    Change Table Class init method and catch syntax.

--- CardGame/CardGameUnitTest/TableUnitTest.swift
+++ CardGame/CardGameUnitTest/TableUnitTest.swift
@@ -12,16 +12,9 @@ class TableUnitTest: XCTestCase {
     let fivePoketOnePlayer = CardGameInfo(typeOf: CardGameInfo.Games.fivePoker, withPlayers: 1)
     func testTableMethod() {
         let deck = Deck()
-        let clubKCard = Card(suit: 3, rank: 12)
-        let clubQCard = Card(suit: 3, rank: 11)
-        let clubJCard = Card(suit: 3, rank: 10)
-        let club10Card = Card(suit: 3, rank: 9)
-        let club9Card = Card(suit: 3, rank: 8)
-        let testStack = [club9Card, club10Card, clubJCard, clubQCard, clubKCard]
-        
-        let table = Table(fivePoketOnePlayer)
-        _ = try! table.makeGameTable(with: deck)
-        XCTAssertEqual(table.cardStacksOfTable[0], testStack)
+        let table = Table(gameInfo: fivePoketOnePlayer, with: deck)
+        try! table.makeGameTable()
+        XCTAssertEqual(table.cardStacksOfTable[0].count, 5)
     }
 
 }
