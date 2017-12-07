47ee19b6d96e9cb6bfad657c98242474744dd34a
DongJin Lee <comets195@gmail.com>
2017-12-07 12:18:10 +0900

    Add GameInputView.

--- CardGame/CardGame/GameInputView.swift
+++ CardGame/CardGame/GameInputView.swift
@@ -7,3 +7,39 @@
 //
 
 import Foundation
+
+struct GameInputView {
+    func playtheGame() throws -> CardGameInfo {
+        return CardGameInfo(typeOfGames: try selectTypesOfGames(), withPlayers: try registerPlayers())
+    }
+    
+    private func selectTypesOfGames() throws -> CardGameInfo.Games {
+        print("카드게임의 종류를 선택하세요.")
+        print("1. 7카드")
+        print("2. 5카드")
+        guard let selectedNumber = readLine(),
+            let number = Int(selectedNumber) else {
+            throw ErrorCode.invaildInput
+        }
+        switch number {
+        case 1:
+            return CardGameInfo.Games.sevenPoker
+        case 2:
+            return CardGameInfo.Games.fivePoker
+        default:
+            throw ErrorCode.noGameNumbers
+        }
+    }
+    
+    private func registerPlayers() throws -> Int {
+        print("참여할 인원을 입력하세요.")
+        guard let playersNumber = readLine(),
+            let players = Int(playersNumber) else {
+                throw ErrorCode.invaildInput
+        }
+        if players < 0 && players > 5 {
+            throw ErrorCode.invaildPlayersNumber
+        }
+        return players
+    }
+}
