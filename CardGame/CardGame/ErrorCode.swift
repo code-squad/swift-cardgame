6ea0c0c23e3f9f1c89e996f4fdd9323a7b6b902e
DongJin Lee <comets195@gmail.com>
2017-12-07 14:38:21 +0900

    Add ErrorCode for invalid Player Numbers, do not define Error and outputView having show the table, dealer stack.

--- CardGame/CardGame/ErrorCode.swift
+++ CardGame/CardGame/ErrorCode.swift
@@ -13,4 +13,5 @@ enum ErrorCode: String, Error {
     case zeroCard = "마지막 카드를 다 쓰셨습니다! 새로운 덱을 가져오세요."
     case noGameNumbers = "지원하진 않는 게임 번호 입니다."
     case invaildPlayersNumber = "최소 참여인원은 1명이며 최대 참여인원은 4명입니다."
+    case notDefineError = "알 수 없는 에러가 발생하였습니다."
 }
