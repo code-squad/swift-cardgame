728a9a43ff2e703e2e6c98dfbd781b6062973ef2
DongJin Lee <comets195@gmail.com>
2017-12-07 14:34:09 +0900

    Add PokerGames with dealer.

--- CardGame/CardGame/main.swift
+++ CardGame/CardGame/main.swift
@@ -12,11 +12,28 @@ func executeCard() {
     let inputView = InputView()
     let outputView = OutputView()
     var deck = Deck()
-    let cardStacks = CardStack()
     
-    deck.shuffle()
-    _ = cardStacks.hasStack(deck: deck)
-    outputView.showMeTheStack(cardStacks)
+    var gameCondition = true
+    repeat {
+        do {
+            let table = Table(try GameInputView.playtheGame())
+            deck = try table.makeGameTable(with: deck)
+            outputView.showMeTheTable(table)
+            outputView.lookDealerCards(of: try deck.makeStack(numberOfCards: table.gameInfo.typeOfGames.rawValue))
+        } catch ErrorCode.invaildInput {
+            outputView.errorMsg(.invaildInput)
+        } catch ErrorCode.invaildPlayersNumber {
+            outputView.errorMsg(.invaildPlayersNumber)
+        } catch ErrorCode.noGameNumbers {
+            outputView.errorMsg(.noGameNumbers)
+        } catch ErrorCode.zeroCard {
+            outputView.errorMsg(.zeroCard)
+            gameCondition = false
+        } catch {
+            outputView.errorMsg(.notDefineError)
+        }
+    } while gameCondition
+    
     // Menu 를 출력하는 while 문을 false로 지정해서 실행하지 않게 하였습니다. 그리고
     // 스택의 값을 출력하는 부분을 while위에 선언하여 해결하였습니다.
     var repeatCondition = false
