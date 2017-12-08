bfd148ce66534eb86d9549f6db52d3d9f5a5053a
undervine <undervine@naver.com>
2017-12-08 17:31:09 +0900

    Make extension conform PokerScoreable protocol

--- CardGame/CardGame/Game/CardStack.swift
+++ CardGame/CardGame/Game/CardStack.swift
@@ -8,26 +8,28 @@
 
 import Foundation
 
+// 특정 개수의 카드 뭉치 클래스.
 class CardStack: Sequence {
     private var cards: [Card]
     let start: Int
+    init() {
+        self.cards = []
+        self.start = 0
+    }
+
+    // 내부 카드들에 접근 가능.
+    func makeIterator() -> ClassIteratorOf<Card> {
+        return ClassIteratorOf(self.cards)
+    }
+}
+
+extension CardStack: CardGameStack {
     var count: Int {
         return self.cards.count
     }
     var isEmpty: Bool {
         return self.cards.isEmpty
     }
-    init() {
-        self.cards = []
-        self.start = 0
-    }
-
-    func makeIterator() -> ClassIteratorOf<Card> {
-        return ClassIteratorOf(self.cards)
-    }
-}
-
-extension CardStack {
 
     func push(card: Card) {
         self.cards.append(card)
@@ -52,6 +54,27 @@ extension CardStack {
 
 }
 
+extension CardStack: PokerScoreable {
+    // 카드 숫자 기준 오름차순으로 정렬된 카드 배열 반환.
+    func sortCards() -> [Card] {
+        return self.sorted(by: <)
+    }
+
+    // 소유한 카드에서 가장 좋은 패의 랭크 반환.
+    func getBestHand() -> PokerHands.HandRanks {
+        let hands = PokerHands(sortCards())
+        let bestHand = hands.findBestHand()
+        return bestHand
+    }
+
+    // 정렬된 카드 배열에서 가장 높은 카드 반환.
+    func getTopCard() -> Card {
+        return self.sortCards().last!
+    }
+
+}
+
+// 스택 내의 모든 카드 출력 포맷. [ 카드, 카드, 카드, 카드, 카드 ] 형태.
 extension CardStack: CustomStringConvertible {
     var description: String {
         var result = "["
