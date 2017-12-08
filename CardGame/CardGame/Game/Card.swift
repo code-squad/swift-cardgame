4a8b12cde2e05ca1fd564f8c2d7634b55975802b
undervine <undervine@naver.com>
2017-12-08 17:29:29 +0900

    Add comments

--- CardGame/CardGame/Game/Card.swift
+++ CardGame/CardGame/Game/Card.swift
@@ -22,6 +22,14 @@ class Card {
         // print("\(self.description): Card instance deinitialized.")
     }
 
+    func getNumber() -> Number {
+        return self.number
+    }
+
+    func getShape() -> Shape {
+        return self.shape
+    }
+
     // 숨겨져있는 카드를 보이도록 뒤집음.
     func turnOver() {
         if !isFaceUp {
@@ -30,9 +38,24 @@ class Card {
     }
 }
 
+// 카드 한 장 출력 포맷. 모양+숫자 형태.
 extension Card: CustomStringConvertible {
     var description: String {
         return self.shape.description + self.number.description
+    }
+
+}
+
+// 카드를 비교할 경우, 카드의 숫자를 비교. (Shape과 Number의 캡슐화를 위함.)
+extension Card: Equatable, Comparable {
+    static func ==(lhs: Card, rhs: Card) -> Bool {
+        guard lhs.number == rhs.number else { return false }
+        return true
+    }
+
+    static func <(lhs: Card, rhs: Card) -> Bool {
+        guard lhs.number < rhs.number else { return false }
+        return true
     }
 
 }
