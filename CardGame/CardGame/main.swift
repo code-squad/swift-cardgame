b0c072e6edfe07577bdfc01b2549b28ad95620a5
undervine <undervine@naver.com>
2017-12-08 17:43:59 +0900

    Add statements to print winner and ask players try again

--- CardGame/CardGame/main.swift
+++ CardGame/CardGame/main.swift
@@ -9,26 +9,34 @@
 import Foundation
 
 func main() {
-    var game = StudPokerGame(.sevenCard, with: 1)
     let inputView = InputView()
+    var game = StudPokerGame(.sevenCard, with: 1)
+    var outputView = OutputView(game)
 
-    // 카드가 다 떨어질 때까지 진행.
-    repeat {
-        do {
-            guard let numberOfPeople = try inputView.askForParticipants(), let stud = try inputView.askForStud() else { break }
-            game = StudPokerGame(stud, with: numberOfPeople)
-            try game.begin()
-            // 덱에 카드가 모자른 경우, 에러만 찍고 끝내기 위해 do문 안에 작성. (do문 밖에 작성 시, 에러 출력 후 이전 카드스택을 출력하기 때문)
-            OutputView.printCardStacks(of: game)
-//            OutputView.printCardStacksWithName(of: game)
-        }catch let e as InputView.InputError {
-            print(e.rawValue)
-        }catch let e as StudPokerGame.GameError {
-            print(e.rawValue)
-        }catch {
-            print(error)
-        }
-    }while(!game.isDeckEmpty)
+    // 같은 멤버, 스터드의 게임에서 이기는 사람이 나올 때까지 한 게임 진행. (덱의 카드가 떨어지면 전체 게임 종료.)
+    do {
+        guard let numberOfPeople = try inputView.askForParticipants(), let stud = try inputView.askForStud() else { return }
+        game = StudPokerGame(stud, with: numberOfPeople)
+        outputView = OutputView(game)
+    }catch let e as InputView.InputError {
+        print(e.rawValue)
+    }catch let e as StudPokerGame.GameError {
+        print(e.rawValue)
+    }catch {
+        print(error)
+    }
+
+    // 카드가 다 떨어질 때까지 같은 멤버로 진행.
+    while !game.isLackOfCards() {
+        // 이기는 사람이 나오면 다시 카드 분배.
+        guard let _ = try? game.begin() else { break }
+        // 덱에 카드가 모자른 경우, 에러만 찍고 끝내기 위해 do문 안에 작성. (do문 밖에 작성 시, 에러 출력 후 이전 카드스택을 출력하기 때문)
+        outputView.printCardStacksWithName()
+        // 승자 출력.
+        outputView.printWinner(game.chooseWinner())
+        if inputView.tryAgain() { continue }
+        else { break }
+    }
 
 }
 
