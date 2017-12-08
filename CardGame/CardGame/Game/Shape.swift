9ad05e18d717e24a2226cf955806c943882c29be
undervine <undervine@naver.com>
2017-12-08 17:29:01 +0900

    Add comments

--- CardGame/CardGame/Game/Shape.swift
+++ CardGame/CardGame/Game/Shape.swift
@@ -17,22 +17,22 @@ enum Shape: String, EnumCollection {
     case clubs = "♣️"
 }
 
+// 모양 출력 포맷. enum의 원시값 형태.
 extension Shape: CustomStringConvertible {
     var description: String {
         return self.rawValue
     }
 }
 
-extension Shape: Equatable {
+// 모양 비교 시, enum의 해시값으로 비교.
+extension Shape: Equatable, Comparable {
     static func ==(lhs: Shape, rhs: Shape) -> Bool {
         guard lhs.hashValue == rhs.hashValue else { return false }
         return true
     }
-}
-
-extension Shape: Comparable {
+    
     static func <(lhs: Shape, rhs: Shape) -> Bool {
-        // 해시값이 작을수록 카드패의 가치가 높아짐.
+        // 해시값이 작을수록 카드패의 가치가 높아지므로, 역순 비교.
         guard lhs.hashValue > rhs.hashValue else { return false }
         return true
     }
