30981f382549c178eb5230abbc2413a5a25bf957
DongJin Lee <comets195@gmail.com>
2017-12-11 18:09:46 +0900

    Add Poker Hand Unit Test for whole Poker Hand Rank.(11).

--- CardGame/CardGameUnitTest/TableUnitTest.swift
+++ CardGame/CardGameUnitTest/TableUnitTest.swift
@@ -16,27 +16,4 @@ class TableUnitTest: XCTestCase {
         _ = try! table.dealTheCardOfGameTable()
         XCTAssertEqual(table.cardStacksOfTable[0].count, 5)
     }
-    // [clubs, hearts, diamonds, spades]
-    func testHighCard() {
-        let diamond_J = Card(suit: 2, rank: 10)
-        let club_Q = Card(suit: 0, rank: 11)
-        let club_7 = Card(suit: 0, rank: 6)
-        let heart_five = Card(suit: 1, rank: 4)
-        let spade_Eight = Card(suit: 3, rank: 7)
-        let testStack = [diamond_J, club_Q, club_7, heart_five, spade_Eight]
-        var testCalculate = PokerHand()
-        XCTAssertEqual(testCalculate.makePokerHandRanking([testStack]).1[0], 13)
-    }
-    
-    func testOnePair() {
-        let diamond_J = Card(suit: 2, rank: 10)
-        let club_J = Card(suit: 0, rank: 10)
-        let spade_J = Card(suit: 3, rank: 10)
-        let heart_J = Card(suit: 1, rank: 10)
-        let testStack1 = [diamond_J, club_J]
-        let testStack2 = [spade_J, heart_J] // spade OnePair is greater than another OnePair.
-        var testCalculate = PokerHand()
-        XCTAssertGreaterThan(testCalculate.makePokerHandRanking([testStack2]).1[0], testCalculate.makePokerHandRanking([testStack1]).1[0])
-    }
-    
 }
