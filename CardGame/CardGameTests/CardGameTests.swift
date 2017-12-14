a0dadd949a23a2a7edc6bf4671fc732d99381abf
YUAMING <miennes3@gmail.com>
2017-12-14 13:46:09 +0900

    Add card class

--- CardGame/CardGameTests/CardGameTests.swift
+++ CardGame/CardGameTests/CardGameTests.swift
@@ -11,17 +11,17 @@ import XCTest
 class CardGameTests: XCTestCase {
     
     func test_Suit_출력() {
-        XCTAssertTrue(Suits.club.description == "♣️")
-        XCTAssertTrue(Suits.heart.description == "♥️")
-        XCTAssertTrue(Suits.diamond.description == "♦️")
-        XCTAssertTrue(Suits.spade.description == "♠️")
+        XCTAssertTrue(Suit.club.description == "♣️")
+        XCTAssertTrue(Suit.heart.description == "♥️")
+        XCTAssertTrue(Suit.diamond.description == "♦️")
+        XCTAssertTrue(Suit.spade.description == "♠️")
     }
     
     func test_숫자_출력() {
-        XCTAssertTrue(Numbers.ace.description == "A")
-        XCTAssertTrue(Numbers.king.description == "K")
-        XCTAssertTrue(Numbers.queen.description == "Q")
-        XCTAssertTrue(Numbers.jack.description == "J")
-        XCTAssertTrue(Numbers.four.description == "4")
+        XCTAssertTrue(Number.ace.description == "A")
+        XCTAssertTrue(Number.king.description == "K")
+        XCTAssertTrue(Number.queen.description == "Q")
+        XCTAssertTrue(Number.jack.description == "J")
+        XCTAssertTrue(Number.four.description == "4")
     }
 }
