7a8888320ce80647b92fac4c5c592363abe5d02d
YUAMING <miennes3@gmail.com>
2017-12-14 12:32:37 +0900

    Add enum of card number

---
+++ CardGame/CardGame/Numbers.swift
@@ -0,0 +1,44 @@
+//
+//  Numbers.swift
+//  CardGame
+//
+//  Created by yuaming on 2017. 12. 14..
+//  Copyright © 2017년 JK. All rights reserved.
+//
+
+import Foundation
+
+enum Numbers {
+    case ace, king, queen, jack, ten, nine, eight, seven, six, five, four, three, two
+    
+    var description: String {
+        switch self {
+        case .ace:
+            return "A"
+        case .king:
+            return "K"
+        case .queen:
+            return "Q"
+        case .jack:
+            return "J"
+        case .ten:
+            return "10"
+        case .nine:
+            return "9"
+        case .eight:
+            return "8"
+        case .seven:
+            return "7"
+        case .six:
+            return "6"
+        case .five:
+            return "5"
+        case .four:
+            return "4"
+        case .three:
+            return "3"
+        case .two:
+            return "2"
+        }
+    }
+}
