15097cac407d6e89f8e82efec3c997bf7a5fe06e
YUAMING <miennes3@gmail.com>
2017-12-14 15:14:43 +0900

    Add raw values of number enum

--- CardGame/CardGame/Number.swift
+++ CardGame/CardGame/Number.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 enum Number: String {
-    case ace, king, queen, jack
+    case ace = "1", king = "13", queen = "12", jack = "11"
     case ten = "10", nine = "9", eight = "8", seven = "7", six = "6", five = "5", four = "4", three = "3", two = "2"
     
     var description: String {
