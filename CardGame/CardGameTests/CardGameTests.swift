864cfacb2418abf628347c978bba2b0db26fd338
YUAMING <miennes3@gmail.com>
2017-12-14 12:32:37 +0900

    Add enum of card number

--- CardGame/CardGameTests/CardGameTests.swift
+++ CardGame/CardGameTests/CardGameTests.swift
@@ -10,10 +10,18 @@ import XCTest
 
 class CardGameTests: XCTestCase {
     
-    func test_Suits_출력() {
+    func test_Suit_출력() {
         XCTAssertTrue(Suits.club.description == "♣️")
         XCTAssertTrue(Suits.heart.description == "♥️")
         XCTAssertTrue(Suits.diamond.description == "♦️")
         XCTAssertTrue(Suits.spade.description == "♠️")
+    }
+    
+    func test_숫자_출력() {
+        XCTAssertTrue(Numbers.ace.description == "A")
+        XCTAssertTrue(Numbers.king.description == "K")
+        XCTAssertTrue(Numbers.queen.description == "Q")
+        XCTAssertTrue(Numbers.jack.description == "J")
+        XCTAssertTrue(Numbers.four.description == "4")
     }
 }
