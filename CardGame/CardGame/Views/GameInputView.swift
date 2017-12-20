39925d90a89abb6fda394d181005427446074c3c
YUAMING <miennes3@gmail.com>
2017-12-20 16:31:01 +0900

    Add GameInputView struct

--- CardGame/CardGame/Views/GameInputView.swift
+++ CardGame/CardGame/Views/GameInputView.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 struct GameInputView {
-    func selectMenus() throws -> CardGame.Action {
+    static func selectMenus() throws -> Game.Menu {
         let question = """
         카드 게임 종류를 선택하세요.
         1. 7카드
@@ -22,15 +22,14 @@ struct GameInputView {
             throw GameError.emptyValue
         }
         
-        
-        guard let action = CardGame.Action(rawValue: Int(answer) ?? 0) else {
+        guard let menu = Game.Menu(rawValue: Int(answer) ?? 0) else {
             throw GameError.invalidSelection
         }
         
-        return action
+        return menu
     }
     
-    func selectNumberOfPlayers() throws -> String {
+    static func selectNumberOfPlayers() throws -> String {
         let question = "참여할 사람의 인원을 입력하세요."
         
         print(question)
