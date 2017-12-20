72b5984dce83f44d05f91ae539cabf0f9805c623
YUAMING <miennes3@gmail.com>
2017-12-20 16:29:47 +0900

    Change from CardGame to Dealer

--- CardGame/CardGameTests/CardStackTests.swift
+++ CardGame/CardGameTests/CardStackTests.swift
@@ -11,16 +11,16 @@ import XCTest
 class CardStackTests: XCTestCase {
     func test_카드_5개_쌓음() throws {
         let cardDeck: CardDeck = CardDeck()
-        var cardGame: CardGame = CardGame(cardDeck)
-        try cardGame.setCardStacks(5)
+        var dealer: Dealer = Dealer(cardDeck)
+        try dealer.setCardStacks(5)
         
-        XCTAssertTrue(cardGame.cardStacks.count == 5)
-        XCTAssertTrue(cardGame.cardDeck.count() == 37)
+        XCTAssertTrue(dealer.cardStacks.count == 5)
+        XCTAssertTrue(dealer.cardDeck.count() == 37)
     }
     
     func test_카드_10개_쌓음() {
         let cardDeck: CardDeck = CardDeck()
-        var cardGame: CardGame = CardGame(cardDeck)
-        XCTAssertThrowsError(try cardGame.setCardStacks(10))
+        var dealer: Dealer = Dealer(cardDeck)
+        XCTAssertThrowsError(try dealer.setCardStacks(10))
     }
 }
