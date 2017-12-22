81f1f546d96e6172ea7facaecc849ae8ef797ec1
dely2p <lhyal2p@gmail.com>
2017-12-22 14:35:21 +0900

    fix bug in shuffle, init, reset method

--- CardGame/CardGame/OutputView.swift
+++ CardGame/CardGame/OutputView.swift
@@ -9,25 +9,34 @@
 import Foundation
 
 class OutputView {
+    
+    private let cardDeck: CardDeck
+    
+    init(cardDeck: CardDeck) {
+        self.cardDeck = cardDeck
+    }
+    
     func printResult(numberOfMenu: Int) {
-        let cardDeck = CardDeck()
         var cardOfAll = [CardData]()
-        var cardOfChange = [CardData]()
-        var cardOfChoice: CardData
+        
         switch numberOfMenu {
-        case 1:
-            cardOfAll = cardDeck.reset()
-            print("카드 전체를 초기화했습니다.")
-            print("총 \(cardDeck.count())장의 카드가 있습니다.")
-        case 2:
-            cardOfChange = cardDeck.shuffle(cardOfChange: cardOfChange, cardOfOrigin: cardOfAll)
-            print("전체 52장의 카드를 섞었습니다.")
-        case 3:
-            cardOfChoice = cardDeck.removeOne()
-            print(cardOfChoice)
-            print("총 51장의 카드가 남아있습니다.")
-        default:
-            break
+            case 1:
+                cardOfAll = cardDeck.reset()
+                print("카드 전체를 초기화했습니다.")
+                print("총 \(cardDeck.count())장의 카드가 있습니다.")
+            case 2:
+                cardOfAll = cardDeck.shuffle()
+                for card in cardOfAll {
+                    print(card.number.rawValue, card.shape.rawValue)
+                }
+                print("전체 52장의 카드를 섞었습니다.")
+            case 3:
+                let cardOfChoice = cardDeck.removeOne()
+                print(cardOfChoice.shape, cardOfChoice.number)
+                print("총 51장의 카드가 남아있습니다.")
+            default:
+                break
         }
     }
+    
 }
