228bc85cb78359a6a54de0d38d76f429ce9abb75
dely2p <lhyal2p@gmail.com>
2017-12-22 15:17:55 +0900

    fix repeat menu in main and fixbug in shuffle

--- CardGame/CardGame/OutputView.swift
+++ CardGame/CardGame/OutputView.swift
@@ -17,23 +17,18 @@ class OutputView {
     }
     
     func printResult(numberOfMenu: Int) {
-        var cardOfAll = [CardData]()
-        
         switch numberOfMenu {
             case 1:
-                cardOfAll = cardDeck.reset()
+                cardDeck.reset()
                 print("카드 전체를 초기화했습니다.")
                 print("총 \(cardDeck.count())장의 카드가 있습니다.")
             case 2:
-                cardOfAll = cardDeck.shuffle()
-                for card in cardOfAll {
-                    print(card.number.rawValue, card.shape.rawValue)
-                }
+                _ = cardDeck.shuffle()
                 print("전체 52장의 카드를 섞었습니다.")
             case 3:
-                let cardOfChoice = cardDeck.removeOne()
-                print(cardOfChoice.shape, cardOfChoice.number)
-                print("총 51장의 카드가 남아있습니다.")
+                let card = cardDeck.removeOne()
+                print(card.shape, card.number)
+                print("총 \(cardDeck.count())장의 카드가 남아있습니다.")
             default:
                 break
         }
