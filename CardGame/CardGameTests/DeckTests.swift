e8e00bdf9c8b764b2461ce213e2f8e31c812b8bd
YUAMING <miennes3@gmail.com>
2017-12-28 17:44:09 +0900

    Add Deck struct related tests

---
+++ CardGame/CardGameTests/DeckTests.swift
@@ -0,0 +1,44 @@
+//
+//  DeckTests.swift
+//  CardGameTests
+//
+//  Created by yuaming on 2017. 12. 28..
+//  Copyright © 2017년 JK. All rights reserved.
+//
+
+import XCTest
+
+class DeckTests: XCTestCase {
+    func test_카드_초기화_성공() {
+        let deck: Deck = Deck()
+        XCTAssertTrue(deck.count() == 52)
+    }
+    
+    func test_카드_하나_뽑기_성공() throws {
+        var deck: Deck = Deck()
+        let card: Card = try deck.remove()
+        let compareCard: Card = Card(Suit.spade, Number.king)
+        XCTAssertTrue(card == compareCard)
+    }
+    
+    func test_카드_섞기_성공() {
+        var deck: Deck = Deck()
+        deck.shuffle()
+        
+        let compareDeck: Deck = Deck()
+        XCTAssertTrue(deck != compareDeck)
+    }
+    
+    func test_카드_하나_뽑기_실패() throws {
+        var deck: Deck = Deck()
+        var cards: [Card] = []
+    
+        for _ in 0..<deck.count() {
+            cards.append(try deck.remove())
+        }
+        
+        XCTAssertTrue(cards.count == 52)
+        XCTAssertTrue(deck.count() == 0)
+        XCTAssertThrowsError(try deck.remove())
+    }
+}
