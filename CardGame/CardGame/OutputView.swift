b9f733d4bd36b36544ff19ccb15c6c4c3fd11d28
dely2p <lhyal2p@gmail.com>
2017-12-28 17:09:17 +0900

    fix card printing updated at regular intervals

--- CardGame/CardGame/OutputView.swift
+++ CardGame/CardGame/OutputView.swift
@@ -20,19 +20,19 @@ class OutputView {
     
     func printResult(numberOfMenu: Int) {
         switch numberOfMenu {
-            case 1:
-                cardDeck.reset()
-                print("카드 전체를 초기화했습니다.")
-                print("총 \(cardDeck.count())장의 카드가 있습니다.")
-            case 2:
-                cardDeck.shuffle()
-                print("전체 \(cardDeck.count())장의 카드를 섞었습니다.")
-            case 3:
-                let card = cardDeck.removeOne()
-                print(card.shape, card.number)
-                print("총 \(cardDeck.count())장의 카드가 남아있습니다.")
-            default:
-                break
+        case 1:
+            cardDeck.reset()
+            print("카드 전체를 초기화했습니다.")
+            print("총 \(cardDeck.count())장의 카드가 있습니다.")
+        case 2:
+            cardDeck.shuffle()
+            print("전체 \(cardDeck.count())장의 카드를 섞었습니다.")
+        case 3:
+            let card = cardDeck.removeOne()
+            print(card.shape, card.number)
+            print("총 \(cardDeck.count())장의 카드가 남아있습니다.")
+        default:
+            break
         }
     }
     
@@ -44,7 +44,6 @@ class OutputView {
     }
     
     func printPokerDealer() -> Bool {
-        var index = 1
         var stack: [CardStack]
         do {
             stack = try cardDeck.makeCardStack(gameInfo: gameInfo)
@@ -52,15 +51,22 @@ class OutputView {
             print("게임종료.")
             return false
         }
-        for card in stack {
-            if stack.count == index {
-                print("딜러 ", card.pop())
-            }else {
-                print("참가자#\(index)", card.pop())
+        for index in 1...stack.count {
+            if index == stack.count {
+                print("딜러 ")
+                break
             }
-            index += 1
+            print("참가자#\(index)", terminator: "\t\t")
+        }
+        for step in 0..<stack[0].pop().count {
+            for player in 0..<stack.count {
+                print(stack[player].pop()[step].description, terminator: "\t\t\t")
+                sleep(1)
+            }
+            print()
         }
         return true
     }
     
 }
+
