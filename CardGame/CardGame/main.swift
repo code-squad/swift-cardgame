34f55f1b28e3f43a843290683501cc44e0059f61
YUAMING <miennes3@gmail.com>
2018-01-04 17:49:15 +0900

    Fix minor things

--- CardGame/CardGame/main.swift
+++ CardGame/CardGame/main.swift
@@ -2,43 +2,21 @@
 //  main.swift
 //  CardGame
 //
-//  Created by JK on 10/10/2017.
-//  Copyright © 2017 JK. All rights reserved.
+//  Created by YUAMING on 10/10/2017.
+//  Copyright © 2017 YUAMING. All rights reserved.
 //
 
 import Foundation
 
-func run() throws {
-    let cardDeck = CardDeck()
-    var dealer = Dealer(cardDeck)
-
-    while(true) {
-        do {
-            let action = try InputView.question()
-            let card = try dealer.executeSelectedAction(action)
-            
-            OutputView.printResult(gameMenu: action, usingDeck: dealer.cardDeck, choiceCard: card)
-        } catch let error as GameError {
-            print(error)
-            break
-        }
-    }
-}
-
-//try run()
-
 func main() throws {
     do {
-        let gameMenu = try GameInputView.selectMenus()
-        let playerCount = Int(try GameInputView.selectNumberOfPlayers()) ?? 0
-        var game = Game(Dealer(CardDeck()))
-        try game.start(gameMenu: gameMenu, playerCount: playerCount)
-        
-        OutputView.printResult(game.dealer)
-    } catch let error as GameError {
-        print(error)
+        let selectedMenu = try GameInputView.selectMenus()
+        let playerCount = try GameInputView.selectNumberOfPlayers()
+        let pokerGameData = try PokerGame.start(choiceMenu: selectedMenu, playerCount: playerCount)
+        GameOutputView.printResult(pokerGameData)
+    } catch let e as GameError {
+        print(e.localizedDescription)
     }
 }
-    
-try main()
 
+try main()
