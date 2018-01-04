275e2c638e47694065c45a3d93fca590343e956b
YUAMING <miennes3@gmail.com>
2018-01-04 17:49:34 +0900

    Create HandType enum

---
+++ CardGame/CardGame/PokerGame/HandType.swift
@@ -0,0 +1,21 @@
+//
+//  HandType.swift
+//  CardGame
+//
+//  Created by yuaming on 2018. 1. 4..
+//  Copyright © 2018년 YUAMING. All rights reserved.
+//
+
+import Foundation
+
+enum HandType: Int {
+    case highCard = 0
+    case pair
+    case twoPair
+    case threeOfKind
+    case straight
+    case flush
+    case fullHouse
+    case fourOfKind
+    case straightFlush
+}
