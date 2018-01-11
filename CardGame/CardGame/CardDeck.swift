34820e2a700b8904ff06d62a17b82e466b0e79a3
youth27 <mayihelpu27@gmail.com>
2018-01-11 15:30:25 +0900

    Add allValues varible in enums, Add reset()

--- CardGame/CardGame/CardDeck.swift
+++ CardGame/CardGame/CardDeck.swift
@@ -11,12 +11,18 @@ import Foundation
 // 카드 패는 네 가지 모양과 숫자 상수로 고정된 값이므로 enum을 사용했습니다.
 
 class CardDeck {
+    
+    // MARK: Enums for card format
+    
     enum Suit: String {
         case heart = "♥️"
         case diamond = "♦️"
         case clover = "♣️"
         case spade = "♠️"
+        
+        static let allValues = [heart, diamond, clover, spade]
     }
+    
     enum Denomination: Int, CustomStringConvertible {
         case one = 1, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve, thirteen
         
@@ -29,7 +35,28 @@ class CardDeck {
             default: return String(self.rawValue)
             }
         }
+        
+        static let allValues = [one, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve, thirteen]
     }
+    
+    // MARK: CardDeck functions for running a program
+    
+    private var cards = [Card]()
+    
+    func reset() {
+        var cards = [Card]()
+        for shape in Suit.allValues {
+            for number in Denomination.allValues {
+                cards.append(Card(suit: shape, denomination: number))
+            }
+        }
+        self.cards = cards
+    }
+    
+    
+    
+    
+    
     
 }
 
