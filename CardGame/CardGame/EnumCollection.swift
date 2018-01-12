49fa4f1d775b34ce0f8413733c3d4b4e3a8c6b4b
youth27 <mayihelpu27@gmail.com>
2018-01-12 19:47:44 +0900

    Add EnumCollection protocol, extension

---
+++ CardGame/CardGame/EnumCollection.swift
@@ -0,0 +1,36 @@
+//
+//  EnumCollection.swift
+//  CardGame
+//
+//  Created by YOUTH on 2018. 1. 12..
+//  Copyright © 2018년 JK. All rights reserved.
+//
+
+import Foundation
+
+public protocol EnumCollection: Hashable {
+    static func cases() -> AnySequence<Self>
+    static var allValues: [Self] { get }
+}
+
+public extension EnumCollection {
+    
+    public static func cases() -> AnySequence<Self> {
+        return AnySequence { () -> AnyIterator<Self> in
+            var raw = 0
+            return AnyIterator {
+                let current: Self = withUnsafePointer(to: &raw) { $0.withMemoryRebound(to: self, capacity: 1) { $0.pointee } }
+                guard current.hashValue == raw else {
+                    return nil
+                }
+                raw += 1
+                return current
+            }
+        }
+    }
+    
+    public static var allValues: [Self] {
+        return Array(self.cases())
+    }
+}
+
