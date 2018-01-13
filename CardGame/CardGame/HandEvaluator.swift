dfbd263a382dd560262ec6db41296f49677392a7
Jack <somedd@naver.com>
2018-01-13 21:38:43 +0900

    Add HandEvaluator for choosing winner

--- CardGame/CardGame/HandEvaluator.swift
+++ CardGame/CardGame/HandEvaluator.swift
@@ -8,20 +8,26 @@
 
 import Foundation
 
-struct HandsRater {
+struct HandEvaluator {
     
-    var stacksOfPlayers : [[Card]] = []
-    var handsOfPlayers : [Hand] = []
-    
+    private var handsOfPlayers : [Hand] = []
+    private var stacksOfPlayers : [[Card]] = []
+
     init(_ userStacks : [[Card]]) {
         self.stacksOfPlayers = userStacks
         for indexOfStack in 0..<userStacks.count {
-            self.handsOfPlayers.append(Hand.init(userStacks[indexOfStack]))
+            handsOfPlayers.append(Hand.init(userStacks[indexOfStack]))
         }
     }
     
-    func compareHands() {
-        
+    func generateHandOfWinner() -> WinnerInfo {
+        var evaluatedHands = self.handsOfPlayers.sorted { $0.hand.hashValue > $1.hand.hashValue }
+        if evaluatedHands.count != 1 && evaluatedHands[0].hand == evaluatedHands[1].hand {
+            evaluatedHands = [evaluatedHands[0],evaluatedHands[1]].sorted { $0.pairVal.hashValue > $1.pairVal.hashValue}
+        }
+        let handOfWinner = evaluatedHands[0].hand
+        let numberOfWinner = self.handsOfPlayers.index(where: {$0.hand == evaluatedHands[0].hand}) ?? 0
+        return WinnerInfo.init(handOfWinner, numberOfWinner)
     }
     
 }
