fea2507e9afe1e259fe791dc57867415ef9ce578
waitNseE <yellow.cjh@gmail.com>
2018-01-15 12:03:32 +0900

    출력부분 분리 및 주석 삽입

---
+++ CardGame/CardGame/OutputView.swift
@@ -0,0 +1,15 @@
+//
+//  OutputView.swift
+//  CardGame
+//
+//  Created by Choi Jeong Hoon on 2018. 1. 15..
+//  Copyright © 2018년 JK. All rights reserved.
+//
+
+import Foundation
+
+class OutputView {
+    static func printCard (result: String) {
+        print (result)
+    }
+}
