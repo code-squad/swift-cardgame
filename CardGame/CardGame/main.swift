3f2a7194832ea1ef0a4af732e95ead0234fb5be2
waitNseE <yellow.cjh@gmail.com>
2018-01-16 16:44:19 +0900

    메인에서 분기하기 위해 Output로직을 옮겨오고 전역변수를 선언하지 않기 위해 함수로 묶어줌

--- CardGame/CardGame/main.swift
+++ CardGame/CardGame/main.swift
@@ -9,5 +9,28 @@
 import Foundation
 
 
-let card = Card(rank: .One, suit: .club)
-OutputView.printCard(card: card)
+func runProgram () {
+    var cardDeck = CardDeck()
+    var isRunning = true
+    while isRunning {
+        print(InputView.InputMessage.welcoming)
+        guard let input = InputView.input() else { continue }
+        switch input {
+        case 1:
+            cardDeck.resetCard()
+            print("카드 전체를 초기화했습니다.")
+            print("총 \(cardDeck.count)장의 카드가 있습니다.")
+        case 2:
+            cardDeck.suffle()
+            print("전체 \(cardDeck.count)장의 카드를 섞었습니다.")
+        case 3:
+            let deletedCard: Card = cardDeck.pickCard()
+            print(deletedCard.description)
+            print("총 \(cardDeck.count)장의 카드가 남아있습니다.")
+        default : isRunning = false
+        }
+    }
+}
+
+runProgram()
+
