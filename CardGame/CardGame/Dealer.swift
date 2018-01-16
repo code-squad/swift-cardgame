61bfb21e6646abd5a4cb25ac09784ea38300fa1a
youth27 <mayihelpu27@gmail.com>
2018-01-16 16:12:20 +0900

    Add Dealer, change Player struct to class, dealer inherit from child

---
+++ CardGame/CardGame/Dealer.swift
@@ -0,0 +1,13 @@
+//
+//  Dealer.swift
+//  CardGame
+//
+//  Created by YOUTH on 2018. 1. 16..
+//  Copyright © 2018년 JK. All rights reserved.
+//
+
+import Foundation
+
+class Dealer: Player {
+
+}
