799976c68de612624af755c3bdb88575487fd4a1
waitNseE <yellow.cjh@gmail.com>
2018-01-16 16:38:51 +0900

    카드덱관련 메서드를 묶기 위해 객체 생성

---
+++ CardGame/CardGame/CardDeck.swift
@@ -0,0 +1,46 @@
+//
+//  CardDeck.swift
+//  CardGame
+//
+//  Created by Choi Jeong Hoon on 2018. 1. 15..
+//  Copyright © 2018년 JK. All rights reserved.
+//
+
+import Foundation
+struct CardDeck {
+    private var cardDeck = [Card]()
+    init() {
+        self.resetCard()
+    }
+    var count: Int {
+        return self.cardDeck.count
+    }
+    
+    mutating func resetCard() {
+        var tempDeck = [Card]()
+        for rank in CardSet.Rank.TotalRank {
+            for suit in 0 ..< CardSet.Suit.TotalSuit.count {
+                let card = Card(rank: CardSet.Rank(rawValue: rank)! , suit: CardSet.Suit.TotalSuit[suit])
+                tempDeck.append(card)
+            }
+            cardDeck = tempDeck
+        }
+    }
+    
+    mutating func suffle () {
+        cardDeck = cardDeck.shuffle()
+    }
+    
+    mutating func pickCard () -> Card {
+        return cardDeck.removeLast()
+    }
+}
+
+extension Array {
+    mutating func shuffle() -> Array{
+        self.sort { _,_ in arc4random_uniform(2) == 1 }
+        return self
+    }
+}
+
+
