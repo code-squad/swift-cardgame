d1df869761765eb0b32f02228e4607e2556be519
waitNseE <yellow.cjh@gmail.com>
2018-01-17 14:19:47 +0900

    메뉴정보만 갖도록 변경

--- CardGame/CardGame/CardGameInfo.swift
+++ CardGame/CardGame/CardGameInfo.swift
@@ -8,46 +8,9 @@
 
 import Foundation
 
-// 카드 정보를 갖는 클래스, 특정숫자는 영문자로 표현하기 위해 enum안에 함수 사용
+// 카드게임 관련 정보를 모아놓은 클래스
 class CardGameInfo {
-    enum SuitsOfCard: Int {
-        case spade, heart, diamond, club
-        var description: String {
-            switch self {
-            case .spade : return "♠️"
-            case .heart : return "💖"
-            case .diamond : return "🔶"
-            case .club : return"♣️"
-            }
-        }
-        static let rangeOfSuits = SuitsOfCard.spade.rawValue ... SuitsOfCard.club.rawValue
-        static let suits = Array(rangeOfSuits.map({ (index) -> SuitsOfCard in
-            SuitsOfCard(rawValue: index)!
-        }))
-        
-        
-    }
-    
-    enum RankOfCard: Int {
-        case one = 1, two, three, four, five, six, seven, eight, nine, ten
-        case eleven, twelve, thirteen
-        static let rangeOfRankOfCard = RankOfCard.one.rawValue ... RankOfCard.thirteen.rawValue
-        static let ranks = Array(rangeOfRankOfCard.map({ (index) -> RankOfCard in
-            RankOfCard(rawValue: index)!
-        }))
-        var description: String {
-            switch self {
-            case .one : return "A"
-            case .eleven : return "J"
-            case .twelve : return "Q"
-            case .thirteen : return "K"
-            default: return String(self.rawValue)
-            }
-        }
-    }
-
     enum Menu: Int {
-        case reset = 1, suffle, pick, end
+        case reset = 1, suffle, pick, end, pleaseInputAgain
     }
-    
 }
