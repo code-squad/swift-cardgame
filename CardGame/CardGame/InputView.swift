386917830ee6c9f684635ac0d9d079d540d2259b
waitNseE <yellow.cjh@gmail.com>
2018-01-17 14:17:05 +0900

    오류시 내부에서 프린트하지 않고 동작만하고 판단은 상위로 넘기도록 개선

--- CardGame/CardGame/InputView.swift
+++ CardGame/CardGame/InputView.swift
@@ -16,42 +16,32 @@ struct InputView {
             switch self {
             case .ofWelcoming:
                 return """
-                    다음 메뉴를 선택해주세요.
-                    1. 카드 초기화
-                    2. 카드 섞기
-                    3. 카드 하나 뽑기
-                    4. 프로그램 종료
-                    >
-                    """
+                다음 메뉴를 선택해주세요.
+                1. 카드 초기화
+                2. 카드 섞기
+                3. 카드 하나 뽑기
+                4. 프로그램 종료
+                >
+                """
             case .ofUnsupportedInput:
                 return "메뉴의 번호 중 한가지를 골라주세요."
             }
         }
     }
     
-    static func input () -> CardGameInfo.Menu? {
-        var pickedNum = 0
-        var menu: CardGameInfo.Menu
+    static func input () -> CardGameInfo.Menu {
+        print(InputView.Message.ofWelcoming)
+        var menu: CardGameInfo.Menu = .end
         let input = readLine() ?? "0"
         if let inputNum = Int(input) {
-            if inputNum < 0 || inputNum > 4 {
-                print (Message.ofUnsupportedInput, terminator: " ")
-                return nil
+            if let tempMenu = CardGameInfo.Menu(rawValue: inputNum) {
+                menu = tempMenu
             } else {
-                pickedNum = inputNum
+                return CardGameInfo.Menu.pleaseInputAgain
             }
         } else {
-            print (Message.ofUnsupportedInput, terminator: " ")
-            return nil
-        }
-       
-        if let tempMenu = CardGameInfo.Menu(rawValue: pickedNum) {
-            menu = tempMenu
-        } else {
-            print (Message.ofUnsupportedInput, terminator: " ")
-            return nil
+            return CardGameInfo.Menu.pleaseInputAgain
         }
         return menu
     }
 }
-
