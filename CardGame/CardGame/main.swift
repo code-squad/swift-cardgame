6ef8f86d635a93189949e885d3683b7e0223daac
youth27 <mayihelpu27@gmail.com>
2018-01-17 14:53:51 +0900

    Delete runInput()

--- CardGame/CardGame/main.swift
+++ CardGame/CardGame/main.swift
@@ -12,51 +12,11 @@ var runProgram : Bool = true
 var cardDeck = CardDeck()
 let outputView = OutputView()
 
-// MARK: runInput
-
-func runInput() {
-    while runProgram{
-        let inputView = InputView()
-        let userInput = inputView.askUserInput(text: OutputView.ProgramDescription.chooseMenu.description)
-        let inputMenu = inputView.select(rawValue: userInput)
-
-        switch inputMenu {
-
-        case .one:
-            cardDeck = cardDeck.reset()
-            outputView.showResult(text: cardDeck.description)
-        case .two:
-            cardDeck.shuffle()
-            outputView.showResult(text: cardDeck.makeShuffleMessage())
-        case .three:
-            let pickedCard = cardDeck.removeOne()
-            outputView.showResult(text: pickedCard.description)
-            outputView.showResult(text: cardDeck.description)
-        case .quitGame:
-            runProgram = false
-            outputView.showResult(text: OutputView.ProgramDescription.quitGame.description)
-            break
-        case .wrongInput:
-            outputView.showResult(text: OutputView.ProgramDescription.wrongInput.description)
-        }
-    }
-}
-
-
-/*
- InputView를 거치지 않고 스택만 표시하기위해서는 아예 로직이 달라지기 때문에
- InputView를 제거하는 대신 다른 함수로 묶어서 스택 출력만 하는 함수를 호출했습니다.
-*/
-
-// MARK: runStud only
-
-
 func makeStack(stud: Int) -> CardStack {
     let cards = cardDeck.makeCards(stud)
     let stack = cardDeck.makeStack(cards: cards)
     return stack
 }
-
 
 func runStud() {
     while runProgram {
