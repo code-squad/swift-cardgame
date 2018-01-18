bf40efad612c5f8b38a2b2097dec3a3a00d0fc13
waitNseE <yellow.cjh@gmail.com>
2018-01-18 17:42:54 +0900

    딜러/플레이어에 따른 카드 출력할 수 있도록 변경

--- CardGame/CardGame/OutputView.swift
+++ CardGame/CardGame/OutputView.swift
@@ -11,12 +11,15 @@ struct OutputView {
     enum Message: CustomStringConvertible{
         case ofResetCard
         case ofEndOfProgram
+        case ofInsufficientCard
         var description: String {
             switch self {
             case .ofResetCard:
                 return "카드가 초기화되었습니다.\n총 52장의 카드가 있습니다."
             case .ofEndOfProgram:
                 return "프로그램이 종료되었습니다."
+            case .ofInsufficientCard:
+                return "카드수가 부족합니다."
             }
         }
     }
@@ -37,10 +40,12 @@ struct OutputView {
         print("총 \(cardDeck.count)장의 카드가 남아있습니다.")
     }
     
-    static func printCardTable (_ cardTable: Array<Array<Card>>) {
-        for cardSet in cardTable {
-            print (cardSet)
+    static func printPlayerCardTable (_ cardTable: Array<Array<Card>>) {
+        print("딜러#", cardTable[cardTable.count - 1])
+        for index in 0 ... cardTable.count - 1 {
+            print ("참가자# \(index + 1)", cardTable[index])
         }
+        
     }
 }
 
