7c5c5303cbb8f38f8d6ef3f976e2ef6c734d0d66
youth27 <mayihelpu27@gmail.com>
2018-01-18 12:18:34 +0900

    Change 'init?' into 'init throws',add Error handling in main

--- CardGame/CardGame/GameController.swift
+++ CardGame/CardGame/GameController.swift
@@ -11,39 +11,53 @@ import Foundation
 struct GameController {
     private (set) var studType: Int
     private (set) var numberOfPlayer: Int
-    let inputView = GameInputView()
+
+    enum InitError: Error, CustomStringConvertible {
+        case quit
+        case wrongMenu
+        case wrongPlayerNumber
+
+        var description: String {
+            switch self {
+            case .quit : return "<< 게임 종료 >>"
+            case .wrongMenu : return "<< 메뉴를 잘못 입력하셨습니다. 게임 메뉴를 다시 선택해주세요. >>\n"
+            case .wrongPlayerNumber : return "<< 잘못 입력하셨습니다. 참여할 인원 수를 다시 입력해주세요. >>\n"
+            }
+        }
+    }
 
     /*
-     상위 모듈에서 GameController객체가 nil이 되면
-     게임 진행이 되지 않도록 init? 설정
+     상위 모듈에서 GameController객체를 init할떄
+     게임을 진행할 수 없는 값이 들어오면
+     진행이 되지 않도록 throws 설정
      */
 
-    init?(stud: GameInputView.CardStudMenu,
-          numberOfPlayer: Int?) {
-        // setting stud
+    init(stud: GameInputView.CardStudMenu,
+         numberOfPlayer: Int?) throws {
+        // set card stud
         switch stud {
         case .one:
             self.studType = 7
         case .two:
             self.studType = 5
         case .quitGame:
-            return nil // 게임 종료 메시지
+            throw InitError.quit // 게임 종료 메시지
         case .wrongInput:
-            return nil // 잘못된 값 입력 메시지
+            throw InitError.wrongMenu // 잘못된 값 입력 메시지
         }
 
-        // setting numberOfPlayer
+        // set numberOfPlayer
         guard let number = numberOfPlayer else {
-            return nil
+            throw InitError.wrongPlayerNumber // 숫자가 없음
         }
         if number >= 0 && number <= 4 {
             self.numberOfPlayer = number
         } else {
-            return nil
+            throw InitError.wrongPlayerNumber // 플레이어 수 오류
         }
-
     }
 
+    // 파라미터 : CardDeck
     func hasEnoughCards(numberOfCard: Int) -> Bool {
         if ((self.numberOfPlayer+1) * self.studType) > numberOfCard {
             return false
--- CardGame/CardGame/main.swift
+++ CardGame/CardGame/main.swift
@@ -41,94 +41,39 @@ func run() {
     while runProgram {
         let gameInput = GameInputView()
         let stud = studType(gameInput)
-
-        let inputNumber = gameInput.askNumberOfPlayer(message:
-            OutputView.ProgramDescription.askNumberOfPlayer)
-
-        let unconfirmedController = GameController(stud: stud, numberOfPlayer: inputNumber)
-
-        guard let controller = unconfirmedController else {
-            runProgram = false
-            // 게임 종료 메시지 출력
-            outputView.showResult(text:
-                OutputView.ProgramDescription.quitGame)
+        if stud == .quitGame {
+            outputView.showResult(text: OutputView.ProgramDescription.quitGame)
             break
-        } // 게임 진행
-        guard controller.hasEnoughCards(numberOfCard: cardDeck.count()) else {
-            runProgram = false
-            // 카드 부족 메시지 출력
-            outputView.showResult(text:
-                OutputView.ProgramDescription.lackOfCard)
-            break
+        } else {
+            let inputNumber = gameInput.askNumberOfPlayer(message:
+                OutputView.ProgramDescription.askNumberOfPlayer)
+            do {
+                let controller = try GameController(stud: stud, numberOfPlayer: inputNumber)
+                // 게임 진행
+                guard controller.hasEnoughCards(numberOfCard: cardDeck.count()) else {
+                    runProgram = false
+                    // 카드 부족 메시지 출력
+                    outputView.showResult(text:
+                        OutputView.ProgramDescription.lackOfCard)
+                    break
+                }
+                play(stud: controller.studType,
+                     numberOfPlayer: controller.numberOfPlayer)
+            } catch let error as GameController.InitError {
+                switch error {
+                case .quit: outputView.showResult(text:error)
+                    break
+                case .wrongMenu: outputView.showResult(text:error)
+                case .wrongPlayerNumber: outputView.showResult(text:error)
+                }
+                continue
+            } catch {
+                print("unknown error")
+                continue
+            }
         }
-        play(stud: controller.studType,
-             numberOfPlayer: controller.numberOfPlayer)
     }
 }
 
 run()
-
-
-
-// MARK: old code
-
-func runStud() {
-    while runProgram {
-        let gameInput = GameInputView()
-        let userInput = gameInput.askGameType(message: OutputView.ProgramDescription.chooseCardStud.description)
-        let inputMenu = gameInput.select(menu: userInput)
-
-        let inputNumber = gameInput.askNumberOfPlayer(message: OutputView.ProgramDescription.askNumberOfPlayer.description)
-        if let numberOfPlayer = inputNumber {
-            if numberOfPlayer >= 0 &&  numberOfPlayer <= 4 {
-                switch inputMenu {
-                case .one:
-                    let studType = 7
-                    if (numberOfPlayer * studType) > cardDeck.count() {
-                        runProgram = false
-                        outputView.showResult(text: OutputView.ProgramDescription.lackOfCard.description)
-                        break
-                    }
-                    for playerNumber in 0..<numberOfPlayer {
-                        let stack = makeStack(stud: studType)
-                        let player = Player(stack: stack, position: playerNumber+1)
-                        outputView.showResult(text: player.description)
-                    }
-                    let dealerStack = makeStack(stud: studType)
-                    let dealer = Dealer(stack: dealerStack)
-                    outputView.showResult(text: dealer.description)
-                    outputView.showResult(text: cardDeck.description)
-                case .two:
-                    let studType = 5
-                    if (numberOfPlayer * studType) > cardDeck.count() {
-                        runProgram = false
-                        outputView.showResult(text: OutputView.ProgramDescription.lackOfCard.description)
-                        break
-                    }
-                    for playerNumber in 0..<numberOfPlayer {
-                        let stack = makeStack(stud: studType)
-                        let player = Player(stack: stack, position: playerNumber+1)
-                        outputView.showResult(text: player.description)
-                    }
-                    let dealerStack = makeStack(stud: studType)
-                    let dealer = Dealer(stack: dealerStack)
-                    outputView.showResult(text: dealer.description)
-                    outputView.showResult(text: cardDeck.description)
-                case .quitGame:
-                    runProgram = false
-                    outputView.showResult(text: OutputView.ProgramDescription.quitGame.description)
-                case .wrongInput:
-                    outputView.showResult(text: OutputView.ProgramDescription.wrongInput.description)
-                }
-            } else {
-                outputView.showResult(text: OutputView.ProgramDescription.wrongInput.description)
-                continue
-            }
-        } else {
-            outputView.showResult(text: OutputView.ProgramDescription.wrongInput.description)
-            continue
-        }
-    }
-}
-
 
