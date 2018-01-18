4eaba723a992b33c4b58ecbee3e60d7b0620b116
waitNseE <yellow.cjh@gmail.com>
2018-01-18 17:43:14 +0900

    메뉴에 따라 분기하도록 메인 구성

--- CardGame/CardGame/main.swift
+++ CardGame/CardGame/main.swift
@@ -10,7 +10,32 @@ import Foundation
 
 // 기존 메인코드는 tempBranchForCradGame 브랜치에 푸시해놓았습니다.
 
-var cardDeck = CardDeck()
-cardDeck.shuffle()
-let cardTable = cardDeck.makeCardTable()
-OutputView.printCardTable(cardTable)
+func runProgram () {
+    var cardDeck = CardDeck()
+    var isRunning = true
+    while isRunning {
+        let menuNum = InputView.inputMenu()
+        switch menuNum {
+        case .FiveCardGame, .SevenCardGame :
+            let numberOfParticipants = InputView.inputPlayer()
+            if numberOfParticipants == CardGameInfo.NumberOfParticipantsCases.InValidNumber {
+                print(InputView.Message.ofExceedPossibleNumberOfParticipants)
+                continue
+            }
+            let gameInfo = CardGameInfo(menuNum: menuNum.rawValue, numberOfPlayers: numberOfParticipants.rawValue)
+            let cardTable = cardDeck.makeCardTable(gameInfo.numberOfPlayers, gameInfo.numberOfCards)
+            OutputView.printPlayerCardTable(cardTable)
+            if cardDeck.isGameRunnable() != true {
+                isRunning = false
+                print(OutputView.Message.ofInsufficientCard)
+            }
+        case .ExitGame :
+            isRunning = false
+        case .PleaseInputAgain:
+            print(InputView.Message.ofUnsupportedInput)
+            continue
+            
+        }
+    }
+}
+runProgram()
