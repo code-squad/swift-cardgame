37456d99152e01d1164e6c656846e018f68fd55d
waitNseE <yellow.cjh@gmail.com>
2018-01-18 17:41:58 +0900

    7카드 5카드에 맞게 메뉴구성

--- CardGame/CardGame/GameInputView.swift
+++ CardGame/CardGame/GameInputView.swift
@@ -1,8 +1,8 @@
 //
-//  InputView.swift
+//  GameInputView.swift
 //  CardGame
 //
-//  Created by Choi Jeong Hoon on 2018. 1. 15..
+//  Created by Choi Jeong Hoon on 2018. 1. 18..
 //  Copyright © 2018년 JK. All rights reserved.
 //
 
@@ -10,11 +10,13 @@ import Foundation
 
 struct InputView {
     enum Message: CustomStringConvertible {
-        case ofWelcoming
+        case ofMenu
+        case ofCasesOfParticipants
         case ofUnsupportedInput
+        case ofExceedPossibleNumberOfParticipants
         var description: String {
             switch self {
-            case .ofWelcoming:
+            case .ofMenu:
                 return """
                 다음 메뉴를 선택해주세요.
                 1. 7카드 게임
@@ -22,18 +24,31 @@ struct InputView {
                 3. 게임 종료
                 >
                 """
+            case .ofCasesOfParticipants:
+                return "참가자수를 입력해주세요. (참가가능 : 1명 ~ 4명)"
             case .ofUnsupportedInput:
                 return "메뉴의 번호 중 한가지를 골라주세요."
+            case .ofExceedPossibleNumberOfParticipants:
+                return "참가 가능한 수는 1-4명입니다."
             }
         }
     }
     
-    static func input () -> CardGameInfo.Menu {
-        print(InputView.Message.ofWelcoming)
+    static func inputMenu () -> CardGameInfo.Menu {
+        print(InputView.Message.ofMenu)
         let input = readLine() ?? "0"
         guard let inputNum = Int(input), let menu = CardGameInfo.Menu(rawValue: inputNum) else {
-            return CardGameInfo.Menu.pleaseInputAgain
+            return CardGameInfo.Menu.PleaseInputAgain
         }
         return menu
+    }
+    
+    static func inputPlayer () -> CardGameInfo.NumberOfParticipantsCases {
+        print(InputView.Message.ofCasesOfParticipants)
+        let input = readLine() ?? "0"
+        guard let inputNum = Int(input), let numberOfParticipants = CardGameInfo.NumberOfParticipantsCases(rawValue: inputNum) else {
+            return CardGameInfo.NumberOfParticipantsCases.InValidNumber
+        }
+        return numberOfParticipants
     }
 }
