48519ad623411be247c2331e1522ed21864cf654
youth27 <mayihelpu27@gmail.com>
2018-01-23 10:49:43 +0900

    Rename Hands, change class to struct

--- CardGame/CardGame/ScoreChecker.swift
+++ CardGame/CardGame/ScoreChecker.swift
@@ -1,5 +1,5 @@
 //
-//  Hands.swift
+//  ScoreChecker.swift
 //  CardGame
 //
 //  Created by YOUTH on 2018. 1. 22..
@@ -8,7 +8,7 @@
 
 import Foundation
 
-class Hands {
+struct ScoreChecker {
 
     enum PokerHands: Int {
         case NoPair = 0
