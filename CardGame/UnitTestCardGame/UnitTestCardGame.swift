cbe2a92fb907a32721d7d949c4a97110768fc1e4
youth27 <mayihelpu27@gmail.com>
2018-01-25 22:40:26 +0900

    Add test codes

--- CardGame/UnitTestCardGame/UnitTestCardGame.swift
+++ CardGame/UnitTestCardGame/UnitTestCardGame.swift
@@ -9,27 +9,165 @@
 import XCTest
 
 class UnitTestCardGame: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+    func testStraightFlush() {
+        let cards = [
+            Card(suit: CardDeck.Suit.clover, denomination:CardDeck.Denomination.two),
+            Card(suit: CardDeck.Suit.clover, denomination:CardDeck.Denomination.three),
+            Card(suit: CardDeck.Suit.clover, denomination:CardDeck.Denomination.four),
+            Card(suit: CardDeck.Suit.clover, denomination:CardDeck.Denomination.five),
+            Card(suit: CardDeck.Suit.clover, denomination:CardDeck.Denomination.six)
+        ]
+
+        let stack = CardStack(cards)
+        let total = ScoreChecker().totalScore(cardStack: stack)
+        print("flush test sorting :\(stack.sort(order: .Descending))")
+        XCTAssertEqual(total, 9000)
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+
+    func testFullHouse() {
+        let cards = [
+            Card(suit: CardDeck.Suit.heart, denomination:CardDeck.Denomination.two),
+            Card(suit: CardDeck.Suit.clover, denomination:CardDeck.Denomination.two),
+            Card(suit: CardDeck.Suit.diamond, denomination:CardDeck.Denomination.thirteen),
+            Card(suit: CardDeck.Suit.spade, denomination:CardDeck.Denomination.thirteen),
+            Card(suit: CardDeck.Suit.clover, denomination:CardDeck.Denomination.thirteen)
+        ]
+        let stack = CardStack(cards)
+        let total = ScoreChecker().totalScore(cardStack: stack)
+        print("total score: \(total)")
+        XCTAssertEqual(total, 9100)
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+
+    func testPairWeight_thirteen() {
+        let cards = [
+            Card(suit: CardDeck.Suit.heart, denomination:CardDeck.Denomination.thirteen),
+            Card(suit: CardDeck.Suit.clover, denomination:CardDeck.Denomination.two),
+            Card(suit: CardDeck.Suit.diamond, denomination:CardDeck.Denomination.three),
+            Card(suit: CardDeck.Suit.spade, denomination:CardDeck.Denomination.ten),
+            Card(suit: CardDeck.Suit.clover, denomination:CardDeck.Denomination.thirteen)
+        ]
+        let stack = CardStack(cards)
+        let total = ScoreChecker().totalScore(cardStack: stack)
+        print("total score: \(total)")
+        XCTAssertEqual(total, 1300)
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+
+    func testPairWeight_Ace() {
+        let cards = [
+            Card(suit: CardDeck.Suit.heart, denomination:CardDeck.Denomination.ace),
+            Card(suit: CardDeck.Suit.clover, denomination:CardDeck.Denomination.two),
+            Card(suit: CardDeck.Suit.diamond, denomination:CardDeck.Denomination.three),
+            Card(suit: CardDeck.Suit.spade, denomination:CardDeck.Denomination.ten),
+            Card(suit: CardDeck.Suit.clover, denomination:CardDeck.Denomination.ace)
+        ]
+        let stack = CardStack(cards)
+        let total = ScoreChecker().totalScore(cardStack: stack)
+        print("total score: \(total)")
+        XCTAssertEqual(total, 1400)
     }
-    
+
+    func testTripleWeight_three() {
+        let cards = [
+            Card(suit: CardDeck.Suit.heart, denomination:CardDeck.Denomination.three),
+            Card(suit: CardDeck.Suit.clover, denomination:CardDeck.Denomination.three),
+            Card(suit: CardDeck.Suit.diamond, denomination:CardDeck.Denomination.three),
+            Card(suit: CardDeck.Suit.spade, denomination:CardDeck.Denomination.ten),
+            Card(suit: CardDeck.Suit.clover, denomination:CardDeck.Denomination.ace)
+        ]
+        let stack = CardStack(cards)
+        print("sorted: \(cards.sorted())")
+        let total = ScoreChecker().totalScore(cardStack: stack)
+        print("total score: \(total)")
+        XCTAssertEqual(total, 1200)
+    }
+
+    func testNoPair() {
+        let cards = [
+            Card(suit: CardDeck.Suit.heart, denomination:CardDeck.Denomination.three),
+            Card(suit: CardDeck.Suit.clover, denomination:CardDeck.Denomination.six),
+            Card(suit: CardDeck.Suit.diamond, denomination:CardDeck.Denomination.nine),
+            Card(suit: CardDeck.Suit.spade, denomination:CardDeck.Denomination.ten),
+            Card(suit: CardDeck.Suit.clover, denomination:CardDeck.Denomination.ace),
+            Card(suit: CardDeck.Suit.heart, denomination:CardDeck.Denomination.four),
+            Card(suit: CardDeck.Suit.diamond, denomination:CardDeck.Denomination.eleven)
+        ]
+        let stack = CardStack(cards)
+        let total = ScoreChecker().totalScore(cardStack: stack)
+        XCTAssertEqual(total, 14)
+    }
+
+    func testFlushWithNoPair() {
+        let cards = [
+            Card(suit: CardDeck.Suit.clover, denomination:CardDeck.Denomination.three),
+            Card(suit: CardDeck.Suit.clover, denomination:CardDeck.Denomination.five),
+            Card(suit: CardDeck.Suit.clover, denomination:CardDeck.Denomination.seven),
+            Card(suit: CardDeck.Suit.clover, denomination:CardDeck.Denomination.thirteen),
+            Card(suit: CardDeck.Suit.clover, denomination:CardDeck.Denomination.six),
+            Card(suit: CardDeck.Suit.heart, denomination:CardDeck.Denomination.eleven),
+            Card(suit: CardDeck.Suit.diamond, denomination:CardDeck.Denomination.two)
+        ]
+        let stack = CardStack(cards)
+        let total = ScoreChecker().totalScore(cardStack: stack)
+        XCTAssertEqual(total, 7800)
+    }
+
+    func testStraight() {
+        let cards = [
+            Card(suit: CardDeck.Suit.heart, denomination:CardDeck.Denomination.ten),
+            Card(suit: CardDeck.Suit.clover, denomination:CardDeck.Denomination.ace),
+            Card(suit: CardDeck.Suit.diamond, denomination:CardDeck.Denomination.thirteen),
+            Card(suit: CardDeck.Suit.spade, denomination:CardDeck.Denomination.twelve),
+            Card(suit: CardDeck.Suit.clover, denomination:CardDeck.Denomination.eleven)
+        ]
+        let stack = CardStack(cards)
+        print("ascending \(stack.sort(order: .Ascending))")
+        print("descending \(stack.sort(order: .Descending))")
+        let total = ScoreChecker().totalScore(cardStack: stack)
+        XCTAssertEqual(total, 7000)
+
+    }
+
+    func testFlush() {
+        let cards = [
+            Card(suit: CardDeck.Suit.clover, denomination:CardDeck.Denomination.two),
+            Card(suit: CardDeck.Suit.clover, denomination:CardDeck.Denomination.five),
+            Card(suit: CardDeck.Suit.clover, denomination:CardDeck.Denomination.seven),
+            Card(suit: CardDeck.Suit.clover, denomination:CardDeck.Denomination.eleven),
+            Card(suit: CardDeck.Suit.clover, denomination:CardDeck.Denomination.six),
+        ]
+        let stack = CardStack(cards)
+        let total = ScoreChecker().totalScore(cardStack: stack)
+        XCTAssertEqual(total, 6600)
+    }
+
+    func testTwoPairs() {
+        let cards = [
+            Card(suit: CardDeck.Suit.heart, denomination:CardDeck.Denomination.ace),
+            Card(suit: CardDeck.Suit.clover, denomination:CardDeck.Denomination.two),
+            Card(suit: CardDeck.Suit.diamond, denomination:CardDeck.Denomination.two),
+            Card(suit: CardDeck.Suit.spade, denomination:CardDeck.Denomination.ten),
+            Card(suit: CardDeck.Suit.clover, denomination:CardDeck.Denomination.ace)
+        ]
+        let stack = CardStack(cards)
+        let total = ScoreChecker().totalScore(cardStack: stack)
+        print("total score: \(total)")
+        XCTAssertEqual(total, 1600)
+    }
+
+    func testFullHouseWithOnePair() {
+        let cards = [
+            Card(suit: CardDeck.Suit.heart, denomination:CardDeck.Denomination.two),
+            Card(suit: CardDeck.Suit.clover, denomination:CardDeck.Denomination.two),
+            Card(suit: CardDeck.Suit.diamond, denomination:CardDeck.Denomination.thirteen),
+            Card(suit: CardDeck.Suit.spade, denomination:CardDeck.Denomination.thirteen),
+            Card(suit: CardDeck.Suit.clover, denomination:CardDeck.Denomination.thirteen),
+            Card(suit: CardDeck.Suit.diamond, denomination:CardDeck.Denomination.three),
+            Card(suit: CardDeck.Suit.clover, denomination:CardDeck.Denomination.three)
+        ]
+        let stack = CardStack(cards)
+        let total = ScoreChecker().totalScore(cardStack: stack)
+        print("total score: \(total)")
+        XCTAssertEqual(total, 9400)
+    }
 }
