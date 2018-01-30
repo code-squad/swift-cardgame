74092a2fe52f8546dcefe7b6b690802c49810b13
waitNseE <yellow.cjh@gmail.com>
2018-01-30 17:22:03 +0900

    플레이어 객체를 만들어 총점, 카드셋, 이름을 속성으로 갖도록하고 객체간 비교를 위해 Comparable 채택

--- CardGame/CardGame/Player.swift
+++ CardGame/CardGame/Player.swift
@@ -7,3 +7,52 @@
 //
 
 import Foundation
+
+struct Player: Comparable {
+    private var point: Int = 0
+    private var name: String = ""
+    private var cardSet: [Card] = [Card]()
+    init(_ nameIndex: Int, _ cardSet: [Card]) {
+        self.name = makeName(nameIndex)
+        self.point = makePoint(cardSet)
+        self.cardSet = cardSet
+    }
+    
+    init(dealer: Dealer, nameIndex: Int, cardSet: [Card]) {
+        self.name = dealer.makeName(nameIndex)
+        self.point = makePoint(cardSet)
+        self.cardSet = cardSet
+    }
+    
+    var nameDescription: String {
+        return self.name
+    }
+    
+    var pointDescription: Int {
+        return self.point
+    }
+    
+    var cardDescription: [Card] {
+        return self.cardSet
+    }
+    
+    static func <(lhs: Player, rhs: Player) -> Bool {
+        return lhs.point < rhs.point
+    }
+    
+    static func ==(lhs: Player, rhs: Player) -> Bool {
+        return lhs.point == rhs.point
+    }
+    
+    static func >(lhs: Player, rhs: Player) -> Bool {
+        return lhs.point > rhs.point
+    }
+    func makePoint(_ cards: [Card]) -> Int {
+        return Hand.countResult(cards)
+    }
+    
+    func makeName(_ index: Int) -> String {
+        return "참가자\(index)#유저 "
+    }
+
+}
