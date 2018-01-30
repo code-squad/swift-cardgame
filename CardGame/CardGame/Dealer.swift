6a802af54230ef01b732bed0648b9439523cb936
waitNseE <yellow.cjh@gmail.com>
2018-01-30 17:19:55 +0900

    카드 나눠주는 역할로 딜러객체생성, 이후역할은 플레이어와 동일

--- CardGame/CardGame/Dealer.swift
+++ CardGame/CardGame/Dealer.swift
@@ -7,3 +7,31 @@
 //
 
 import Foundation
+
+struct Dealer {
+    private var deck: CardDeck = CardDeck()
+    
+    init(_ deck: CardDeck) {
+        self.deck = deck
+    }
+    
+    var deckDescription: CardDeck {
+        return self.deck
+    }
+    mutating func dealCards(_ gameSpecies: CardGameInfo.GameSpecies) -> [Card] {
+        var cards: [Card] = []
+        for _ in 0 ..< gameSpecies.rawValue {
+            cards.append(self.deck.pickCard())
+        }
+        return cards
+    }
+    
+    var remainedCard: Int {
+        return self.deck.count
+    }
+
+    func makeName(_ index: Int) -> String {
+        return "참가자\(index)#딜러 "
+    }
+    
+}
