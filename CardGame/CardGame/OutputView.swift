5d74d8b2ecafb4aad157c0731f55ba8a077cde31
waitNseE <yellow.cjh@gmail.com>
2018-01-31 17:09:51 +0900

    객체 속성에 접근하던 방식에서 객체에 해당정보를 요청하는 방식으로 변경

--- CardGame/CardGame/OutputView.swift
+++ CardGame/CardGame/OutputView.swift
@@ -11,7 +11,7 @@ struct OutputView {
     enum Message: CustomStringConvertible{
         case ofResetCard
         case ofEndOfProgram
-        case ofInsufficientCard
+        case ofInsufficientCardAndResetMessage
         case ofSucceedChargingCard
         var description: String {
             switch self {
@@ -19,8 +19,8 @@ struct OutputView {
                 return "카드가 초기화되었습니다.\n총 52장의 카드가 있습니다."
             case .ofEndOfProgram:
                 return "프로그램이 종료되었습니다."
-            case .ofInsufficientCard:
-                return "❗️카드수가 부족합니다.❗️, 카드를 충전해주세요."
+            case .ofInsufficientCardAndResetMessage:
+                return "❗️카드수가 부족합니다.❗️, 카드가 다시 충전됩니다."
             case .ofSucceedChargingCard:
                 return "카드 52장이 충전되었습니다."
             }
@@ -43,11 +43,6 @@ struct OutputView {
         print("총 \(cardDeck.count)장의 카드가 남아있습니다.")
     }
     
-    private static func printEachCard (_ card: Card) {
-        sleep(1)
-        print(card.description)
-    }
-    
     
     private static func printEachCardSet (_ cards: [Card]) {
         for index in 0 ..< cards.count {
@@ -58,15 +53,14 @@ struct OutputView {
     
     static func printPlayerCardTable (_ players: [Player]) {
         for index in 0 ..< players.count {
-            print ("\(players[index].nameDescription) ", terminator : "[")
-            printEachCardSet(players[index].cardDescription)
+            let cardSetInfo = players[index].getCardSetInfo()
+            print ("\(cardSetInfo.name) ", terminator : "[")
+            printEachCardSet(cardSetInfo.cardSet)
             print ("]")
         }
     }
     
-    static func printWinner (_ players: [Player]) {
-        let winner = PlayingGame.decideWinner(players)
-        let WinnerHandName = PlayingGame.getWinnerHandName(point: winner.point)
-        print("승자는 \(winner.name)이고 \(WinnerHandName.rawValue) \(winner.point)점 입니다.")
+     static func printWinner (_ winner: Player) {
+        print(winner.getWinnerInfo())
     }
 }
