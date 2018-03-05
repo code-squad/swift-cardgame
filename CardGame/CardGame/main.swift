ecb6b979ae25ae18884ca20873bc753611800e72
waitNseE <yellow.cjh@gmail.com>
2018-03-05 23:24:05 +0900

    OutputView변경된 내용에 따른 반영

--- CardGame/CardGame/main.swift
+++ CardGame/CardGame/main.swift
@@ -12,22 +12,21 @@ func runProgram () {
     let dealer = Dealer.shared
     while true {
         guard let kindOfGame = InputView.sevenOrFiveGame() else {
-            print(InputView.Message.ofUnsupportedInput)
+            print(PreGameMessage.ofUnsupportedInput)
             continue
         }
         guard let numberOfParticipants = InputView.numberOfParticipants() else {
-            print(InputView.Message.ofExceedPossibleNumberOfParticipants)
+            print(PreGameMessage.ofExceedPossibleNumberOfParticipants)
             continue
         }
         guard PlayingGame.isRunnable(numberOfParticipants, kindOfGame, dealer) else {
-            OutputView.printOfInsufficientCard()
+            print(InGameMessage.ofInsufficientCard)
             break
         }
         let resultGame = PlayingGame(numberOfParticipants, kindOfGame, dealer)
-        let ouputView = OutputView(resultGame, dealer)
-        ouputView.printCardsOfPlayers()
-        ouputView.printWinner()
-        ouputView.printRemainingCards()
+        OutputView.printCardsOfPlayers(resultGame)
+        OutputView.printContents(message: resultGame.decideWinner().getWinnerInfo())
+        OutputView.printContents(message: dealer.noticeRemainCard())
     }
 }
 
