4802097a03fceb0164683c296473f7f452f77b67
waitNseE <yellow.cjh@gmail.com>
2018-03-05 23:19:36 +0900

    문자열 enum별도 분리

--- CardGame/CardGame/GameInputView.swift
+++ CardGame/CardGame/GameInputView.swift
@@ -9,39 +9,15 @@
 import Foundation
 
 struct InputView {
-    enum Message: CustomStringConvertible {
-        case ofMenu
-        case ofCasesOfParticipants
-        case ofUnsupportedInput
-        case ofExceedPossibleNumberOfParticipants
-        var description: String {
-            switch self {
-            case .ofMenu:
-                return """
-                다음 메뉴를 선택해주세요.
-                1. 7카드 게임
-                2. 5카드 게임
-                >
-                """
-            case .ofCasesOfParticipants:
-                return "참가자수를 입력해주세요. (참가가능 : 1명 ~ 4명)"
-            case .ofUnsupportedInput:
-                return "메뉴의 번호 중 한가지를 골라주세요."
-            case .ofExceedPossibleNumberOfParticipants:
-                return "참가 가능한 수는 1-4명입니다."
-            }
-        }
-    }
-    
     static func sevenOrFiveGame () -> KindOfGame? {
-        print(InputView.Message.ofMenu, terminator: " ")
+        print(PreGameMessage.ofMenu, terminator: " ")
         let input = readLine() ?? "0"
         guard let inputNum = Int(input), let menu = Menu(rawValue: inputNum) else { return nil }
         return convertMenuNumToKindofCard(menuNum: menu)
     }
     
     static func numberOfParticipants () -> NumberOfParticipants? {
-        print(InputView.Message.ofCasesOfParticipants, terminator: " ")
+        print(PreGameMessage.ofCasesOfParticipants, terminator: " ")
         let input = readLine() ?? "0"
         guard let inputNum = Int(input), let numberOfParticipants = NumberOfParticipants(rawValue: inputNum) else { return nil }
         return numberOfParticipants
