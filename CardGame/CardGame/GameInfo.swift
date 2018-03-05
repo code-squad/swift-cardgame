1c960b4e84cce2fbf7632ad96a14290129a86cb5
waitNseE <yellow.cjh@gmail.com>
2018-03-05 23:22:50 +0900

    잘못삭제한 부분 다시추가

---
+++ CardGame/CardGame/GameInfo.swift
@@ -0,0 +1,22 @@
+//
+//  CardSet.swift
+//  CardGame
+//
+//  Created by Choi Jeong Hoon on 2018. 1. 15..
+//  Copyright © 2018년 JK. All rights reserved.
+//
+
+import Foundation
+
+enum Menu: Int {
+    case sevenCardGame = 1 , fiveCardGame
+}
+
+enum KindOfGame: Int {
+    case sevenCard = 7
+    case fiveCard = 5
+}
+
+enum NumberOfParticipants: Int {
+    case one = 1, two, three, four
+}
