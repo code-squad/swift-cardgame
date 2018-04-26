bfd6d8e5e75603e3b062bfcfddf3ba1aa32f0311
cocomilktea <coco_milktea@naver.com>
2018-04-26 14:23:26 +0900

    OutputPritable protocol countOfCards 함수 추가

--- CardGame/CardGame/OutputView.swift
+++ CardGame/CardGame/OutputView.swift
@@ -9,22 +9,21 @@
 import Foundation
 
 struct OutputView {
-    
+
     func printMenu() {
         print("다음 메뉴를 선택해주세요. \n1. 카드 초기화 \n2. 카드 섞기 \n3. 카드 하나 뽑기 \n> ", terminator: "")
     }
     
     func printReset(_ card: OutputPrintable) {
-        print("카드 전체를 초기화했습니다. 총 \(card.reset().count) 장의 카드가 있습니다. \n")
+        print("카드 전체를 초기화했습니다. 총 \(card.countOfCards())의 카드가 있습니다. \n")
     }
     
     func printShuffle(_ card: OutputPrintable) {
-        print("전체 \(card.shuffle().count)장의 카드를 섞었습니다. \n")
+        print("전체 \(card.countOfCards())의 카드를 섞었습니다. \n")
     }
     
-    func printRemoveOne(_ card: OutputPrintable) {
-        let cards = card.removeOne()
-        print("\(cards.pick) \n총 \(cards.basic.count)장의 카드가 남아있습니다. \n")
+    func printRemoveOne(_ pickCard: Card,_ card: OutputPrintable) {
+        print("\(pickCard) \n총 \(card.countOfCards())의 카드가 남아있습니다. \n ")
     }
     
     func printinValid() {
