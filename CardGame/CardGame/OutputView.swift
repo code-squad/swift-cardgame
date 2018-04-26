711265c0787b435519191c8d5bf06c0f6d0fbf90
cocomilktea <coco_milktea@naver.com>
2018-04-26 10:45:22 +0900

    OutputPrintable 프로토콜 수정

--- CardGame/CardGame/OutputView.swift
+++ CardGame/CardGame/OutputView.swift
@@ -8,22 +8,23 @@
 
 import Foundation
 
-struct OutputView: OutputPrintable {
+struct OutputView {
     
     func printMenu() {
         print("다음 메뉴를 선택해주세요. \n1. 카드 초기화 \n2. 카드 섞기 \n3. 카드 하나 뽑기 \n> ", terminator: "")
     }
     
-    func printReset(_ resetCard: [Card]) {
-        print("카드 전체를 초기화했습니다. 총 \(resetCard.count) 장의 카드가 있습니다. \n")
+    func printReset(_ card: OutputPrintable) {
+        print("카드 전체를 초기화했습니다. 총 \(card.reset().count) 장의 카드가 있습니다. \n")
     }
     
-    func printShuffle(_ shuffleCard: [Card]) {
-        print("전체 \(shuffleCard.count)장의 카드를 섞었습니다. \n")
+    func printShuffle(_ card: OutputPrintable) {
+        print("전체 \(card.shuffle().count)장의 카드를 섞었습니다. \n")
     }
     
-    func printRemoveOne(_ basic: [Card],_ pick: Card) {
-        print("\(pick) \n총 \(basic.count)장의 카드가 남아있습니다. \n")
+    func printRemoveOne(_ card: OutputPrintable) {
+        let cards = card.removeOne()
+        print("\(cards.pick) \n총 \(cards.basic.count)장의 카드가 남아있습니다. \n")
     }
     
     func printinValid() {
