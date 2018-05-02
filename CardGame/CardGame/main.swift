d22d04646f07326ad3d728080bf2753fa2ad5e6d
cocomilktea <coco_milktea@naver.com>
2018-05-03 07:19:43 +0900

    Cards 타입을 CardStack으로 넘겨서 활용하는 코드로 변경

--- CardGame/CardGame/main.swift
+++ CardGame/CardGame/main.swift
@@ -12,23 +12,18 @@ while true {
     let inputView = InputView()
     let cardDeck = CardDeck()
     let outputView = OutputView(cardDeck)
-    
     outputView.printNumberOfCard()
     let numberOfCard = inputView.inputCard()
-    var cards = 0
-    switch numberOfCard {
-    case .sevenCards: cards = 7
-    case .fiveCards: cards = 5
-    case .invalidCards: outputView.printInvalidOfCards()
+    guard numberOfCard != .invalidCards else { outputView.printInvalidOfCards()
         continue
     }
     outputView.printNumberOfPlayer()
     let numberOfPlayer = inputView.inputPlyer()
-    let cardStack = CardStack.init(numberOfPlayer.rawValue)
+    let cardStack = CardStack.init(numberOfCard.rawValue, numberOfPlayer.rawValue)
     guard numberOfPlayer != .invalidPlayer else { outputView.printInvalidOfPlayers()
         continue }
-    
-    cardStack.makeCardStack(cardDeck, cards)
+    cardStack.makeCardStack(cardDeck)
     outputView.printCardStack(cardStack)
 }
+
 
