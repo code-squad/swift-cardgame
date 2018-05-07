c78fab0ebceeeef9868bc88363036796244332fc
Moon <myssun0325@gmail.com>
2018-05-07 20:14:18 +0900

    Card를 표현하기 위한 CustomStringConvertible 구현

--- CardGame/CardGame/Card.swift
+++ CardGame/CardGame/Card.swift
@@ -14,23 +14,44 @@ class Card {
     // - 코드의 가독성을 위해 (Card의 suit, number의 프로퍼티 타입을 한 눈에 확인할 수 있다.)
     // - 현재로써는 Card에서만 사용하는 타입이므로 "Card의 Suit, Card의 Number"를 표현하기 위해
     // - Suit와 Number 모두 enum을 사용한 이유 - 카드게임에서 사용되는 모양과 숫자는 예상된 제한된 값
-    enum Suit {
-        case clubs
-        case diamonds
-        case hearts
-        case spades
+    enum Suit: String {
+        case clubs = "♣️"
+        case diamonds = "♦️"
+        case hearts = "♥️"
+        case spades = "♠️"
     }
     
-    enum Number {
-        case two, three, four, five, six, seven, eight, nine, ten
+    enum Number: Int {
+        case two = 2, three, four, five, six, seven, eight, nine, ten
         case jack, queen, king, ace
+        
+        var description: String {
+            switch self {
+            case .jack:
+                return "J"
+            case .queen:
+                return "Q"
+            case .king:
+                return "K"
+            case .ace:
+                return "A"
+            default:
+                return String(self.rawValue)
+            }
+        }
     }
     
-    let suit: Suit
-    let number: Number
+    private let suit: Suit
+    private let number: Number
     
     init(suit: Suit, number: Number) {
         self.suit = suit
         self.number = number
+    }
+}
+
+extension Card: CustomStringConvertible {
+    var description: String {
+        return "\(self.suit.rawValue)\(self.number.description)"
     }
 }
