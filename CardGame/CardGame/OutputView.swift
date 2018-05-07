364b56f5e4fa8ec4452626cb2ff054d3617085ec
Moon <myssun0325@gmail.com>
2018-05-07 20:18:38 +0900

    OutputView 객체 추가

---
+++ CardGame/CardGame/OutputView.swift
@@ -0,0 +1,17 @@
+//
+//  OutputView.swift
+//  CardGame
+//
+//  Created by moon on 2018. 5. 7..
+//  Copyright © 2018년 JK. All rights reserved.
+//
+
+import Foundation
+
+
+struct OutputView {
+    
+    static func printCard(_ card: CustomStringConvertible) {
+        print(card.description)
+    }
+}
