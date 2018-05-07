f5f62a75c9be5e3061dd99cc844d0fb7c4386926
Moon <myssun0325@gmail.com>
2018-05-07 19:47:19 +0900

    구조체 선택이유 주석 추가

--- CardGame/CardGame/Card.swift
+++ CardGame/CardGame/Card.swift
@@ -10,6 +10,9 @@ import Foundation
 
 class Card {
     
+    // Nested enum을 선택한 이유 :
+    // - 코드의 가독성을 위해 (Card의 suit, number의 프로퍼티 타입을 한 눈에 확인할 수 있다.)
+    // - 현재로써는 Card에서만 사용하는 타입이므로 "Card의 Suit, Card의 Number"를 표현하기 위해
     enum Suit {
         case clubs
         case diamonds
