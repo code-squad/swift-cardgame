a946048940e49fc9bb52cd908051c846fbb59667
Moon <myssun0325@gmail.com>
2018-05-09 13:31:41 +0900

    CardDeck이 CardDeckable 프로토콜 채택

--- CardGame/CardGame/CardDeck.swift
+++ CardGame/CardGame/CardDeck.swift
@@ -8,8 +8,29 @@
 
 import Foundation
 
-struct CardDeck {
+struct CardDeck: CardDeckable {
     private var cardDeck: Deck = Deck()
+    
+    private func count() -> Int {
+        return self.cardDeck.cards.count
+    }
+    
+    mutating func reset() -> String {
+        self.cardDeck = Deck()
+        return "카드 전체를 초기화했습니다.\n총 \(self.count())장의 카드가 있습니다."
+    }
+    
+    func shuffle() -> String {
+        return "전체 \(self.count())장의 카드를 섞었습니다."
+    }
+    
+    mutating func removeOne() -> String {
+        guard let popedCard = self.cardDeck.cards.popLast() else {
+            return "남아있는 카드가 없습니다.\n전"
+        }
+        return "\(popedCard)\n총 \(self.count())장의 카드가 남아있습니다."
+    }
+    
 }
 
 fileprivate struct Deck {
