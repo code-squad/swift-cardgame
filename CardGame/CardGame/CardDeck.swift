fbb7b0d21eafd50b9eec8d1b48d5c032930975da
Moon <myssun0325@gmail.com>
2018-05-09 21:50:15 +0900

    피드백반영: 구현분리, 속성접근 수정

--- CardGame/CardGame/CardDeck.swift
+++ CardGame/CardGame/CardDeck.swift
@@ -11,31 +11,38 @@ import Foundation
 struct CardDeck: CardDeckable {
     private var cardDeck: Deck = Deck()
     
-    private func count() -> Int {
-        return self.cardDeck.cards.count
+    mutating private func resetCard() {
+        self.cardDeck = Deck()
     }
     
     mutating func reset() -> String {
-        self.cardDeck = Deck()
-        return "카드 전체를 초기화했습니다.\n총 \(self.count())장의 카드가 있습니다."
+        self.resetCard()
+        return "카드 전체를 초기화했습니다.\n총 \(self.cardDeck.cards.count)장의 카드가 있습니다."
     }
     
-    mutating func shuffle() -> String {
+    mutating private func shuffleCard() {
         var shuffledCards = [Card]()
-        for count in stride(from: UInt32(self.count()), to: 0, by: -1) {
+        for count in stride(from: UInt32(self.cardDeck.cards.count), to: 0, by: -1) {
             shuffledCards.append(self.cardDeck.cards.remove(at: Int(arc4random_uniform(count))))
         }
         self.cardDeck.cards = shuffledCards
-        return "전체 \(self.count())장의 카드를 섞었습니다."
+    }
+    
+    mutating func shuffle() -> String {
+        self.shuffleCard()
+        return "전체 \(self.cardDeck.cards.count)장의 카드를 섞었습니다."
+    }
+    
+    mutating private func removeOneCard() -> Card? {
+        return self.cardDeck.cards.popLast()
     }
     
     mutating func removeOne() -> String {
-        guard let popedCard = self.cardDeck.cards.popLast() else {
+        guard let popedCard = self.removeOneCard() else {
             return "카드덱에 남아있는 카드가 없습니다. 카드를 초기화해주세요\n"
         }
-        return "\(popedCard)\n총 \(self.count())장의 카드가 남아있습니다."
+        return "\(popedCard)\n총 \(self.cardDeck.cards.count)장의 카드가 남아있습니다."
     }
-    
 }
 
 fileprivate struct Deck {
