ae2b0971a762691370619c832373c87a67591260
Moon <myssun0325@gmail.com>
2018-05-10 19:30:47 +0900

    CardStack 객체 추가

---
+++ CardGame/CardGame/CardStack.swift
@@ -0,0 +1,14 @@
+//
+//  CardStack.swift
+//  CardGame
+//
+//  Created by moon on 2018. 5. 10..
+//  Copyright © 2018년 JK. All rights reserved.
+//
+
+import Foundation
+
+
+class CardStack {
+    var cards: [Card] = [Card]()
+}
