d3fbea2368058e73e39ec74cafdb25b76f68c711
Moon <myssun0325@gmail.com>
2018-05-14 14:48:56 +0900

    GameInputView에 참여자 수를 입력받기 위한 메소드 추가

--- CardGame/CardGame/GameInputView.swift
+++ CardGame/CardGame/GameInputView.swift
@@ -10,6 +10,7 @@ import Foundation
 
 enum Question: String {
     case selectMode = "카드 게임 종류를 선택하세요.\n1. 7카드\n2. 5카드\n"
+    case numberOfPlayers = "참여할 사람의 인원을 입력하세요.\n"
 }
 
 struct GameInputView {
@@ -31,12 +32,16 @@ struct GameInputView {
     
     static func readGameMode(askMessage: Question) throws -> Mode {
         print(askMessage.rawValue, terminator: "> ")
-        guard let modeInput = readLine() else {
-            throw self.Error.invalidMode
-        }
+        let modeInput = readLine() ?? ""
         guard let mode = Mode(rawValue: modeInput) else {
             throw self.Error.invalidMode
         }
         return mode
+    }
+    
+    static func readNumberOfPlayers(askMessage: Question) throws -> Int {
+        print(askMessage.rawValue, terminator: "> ")
+        let numberOfPlayers: Int = Int(readLine() ?? "0") ?? 0
+        return numberOfPlayers
     }
 }
