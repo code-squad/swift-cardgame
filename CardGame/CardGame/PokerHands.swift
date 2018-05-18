43be9c5d9c158dfb8464d9494901dff70724720e
Moon <myssun0325@gmail.com>
2018-05-18 23:29:34 +0900

    PokerHands 재구현을 위한 이전 구현 전부삭제

--- CardGame/CardGame/PokerHands.swift
+++ CardGame/CardGame/PokerHands.swift
@@ -9,33 +9,5 @@
 import Foundation
 
 struct PokerHands {
-    
-    // 페어
-    func hasPair(_ cards: [Card]) -> Int {
-        var pairCount: Int = 0
-        for card in cards {
-            if cards.contains(card) {
-                pairCount += 1
-            }
-        }
-        return pairCount
-    }
-    
-    func hasStraight(_ cards: [Card]) -> Bool {
-        var cards = cards
-        var continuousCount: Int = 0
-        var previousCard = cards.removeFirst()
-        for card in cards {
-            if previousCard.isContinous(to: card) {
-                continuousCount += 1
-            }
-            previousCard = card
-        }
-        if continuousCount >= 4 {
-            return true
-        } else {
-            return false
-        }
-    }
 
 }
