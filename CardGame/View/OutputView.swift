30f9b0e2d293149e9dab4c5dfdab1802c35ace1d
Jung seoung Yeo <duwjdtmd91@gmail.com>
2018-05-20 05:01:11 +0900

    출력을 담당할 객체 생성

--- CardGame/View/OutputView.swift
+++ CardGame/View/OutputView.swift
@@ -6,4 +6,11 @@
 //  Copyright © 2018년 JK. All rights reserved.
 //
 
-import Foundation
+// 출력을 담당하는 객체
+struct OutputView {
+    
+    // static을 선언 이유 : OutputView를 객체 생성하지 않고 바로 showCard함수를 실행하기 위하여 선언
+    static func showCard(_ card: Card) {
+        print(card.desription())
+    }
+}
