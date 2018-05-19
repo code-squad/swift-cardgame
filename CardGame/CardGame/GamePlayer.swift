d3a058b3b88818fe31a463fb3f1f12f42d671373
Moon <myssun0325@gmail.com>
2018-05-19 14:22:55 +0900

    이상한거 삭제

--- CardGame/CardGame/GamePlayer.swift
+++ CardGame/CardGame/GamePlayer.swift
@@ -11,7 +11,6 @@ import Foundation
 
 class GamePlayer {
     private var cards: [Card] = [Card]()
-    private let pokerHands = PokerHands()
     func add(cards: [Card]) {
         self.cards = cards
     }
@@ -25,31 +24,6 @@ class GamePlayer {
     }
     
     func handCheck() {
-        if self.pokerHands.hasRoyalStraightFlush(self.cards) {
-            print("로얄스트레이트플러시")
-        } else if self.pokerHands.hasBackStraightFlush(self.cards) {
-            print("백스트레이트플러시")
-        } else if self.pokerHands.hasStraightFlush(self.cards) {
-            print("스트레이트플러시")
-        } else if self.pokerHands.hasFourCard(self.cards) {
-            print("포카드")
-        } else if self.pokerHands.hasFullHous(self.cards) {
-            print("풀하우스")
-        } else if self.pokerHands.hasFlush(self.cards) {
-            print("플러시")
-        } else if self.pokerHands.hasMountain(self.cards) {
-            print("마운틴")
-        } else if self.pokerHands.hasStraight(self.cards) {
-            print("스트레이트")
-        } else if self.pokerHands.hasTriple(self.cards) {
-            print("트리플")
-        } else if self.pokerHands.hasTwoPair(self.cards) {
-            print("투페어")
-        } else if self.pokerHands.hasOnePair(self.cards) {
-            print("원페어")
-        } else {
-            print("탑: \(self.cards.max()!)")
-        }
     }
 }
 
