4e31c922fbb9004032008c3c49803483932b339f
Jung seoung Yeo <duwjdtmd91@gmail.com>
2018-05-21 14:31:13 +0900

    outputview : 에러, selelcetMessage, showResult 함수 추가

--- CardGame/View/OutputView.swift
+++ CardGame/View/OutputView.swift
@@ -10,7 +10,26 @@
 struct OutputView {
     
     // static을 선언 이유 : OutputView를 객체 생성하지 않고 바로 showCard함수를 실행하기 위하여 선언
+    
     static func showCard(_ card: Card) {
         print(card.desription())
     }
+    
+    // 시작 시 실행 선택하는 메시지
+    static func cardGameSelectMassgae() {
+        print(CARDGAME_SELECT_MSG)
+        print(CARDGAME_SELECT_RESET_MSG)
+        print(CARDGAME_SELECT_SHUFFLE_MSG)
+        print(CARDGAME_SELECT_DRAW_MSG)
+    }
+    
+    // 에러 메시지를 출력 하는 함수
+    static func errorMessage(_ e : CardGaemError) {
+        print(e.description)
+    }
+    
+    static func showResult(_ resultMessag: String) {
+        print(resultMessag)
+    }
+    
 }
