2f8c61def626c590aec69619c75624a23f5252fe
Jung seoung Yeo <duwjdtmd91@gmail.com>
2018-05-21 14:35:50 +0900

    main -> 입력을 받고 selectNumber에 따라 실행을 다르게 하는 것으로 변경

--- CardGame/CardGame/main.swift
+++ CardGame/CardGame/main.swift
@@ -6,9 +6,34 @@
 //  Copyright © 2017 JK. All rights reserved.
 //
 
-func main() {
-    let card  = Card(Suits.spade, Rank.Ace)
-    OutputView.showCard(card)
+import Foundation
+
+func main() throws {
+    var deck = Deck()
+    while let selectNumber = try InputView.readInput() {
+        var resultMessgae = ""
+        do {
+            switch selectNumber {
+                case CARD_RESET:
+                    deck.reset()
+                    resultMessgae = CARDGAME_RESET_MSG
+                case CARD_SHUFFLE:
+                    deck.shuffle()
+                    resultMessgae = "\(CARDGAME_SHUFFLE_TOTAL_COMAND)\(deck.count())\(CARDGAME_SHUFFLE_BACK_COMAND)"
+                case CARD_DRAW:
+                    let drawCard = deck.removeOne()
+                    resultMessgae = drawCard.desription()
+                    resultMessgae += "\n"
+                    resultMessgae += "\(CARDGAME_DRAW_TOTAL_COMAND)\(deck.count())\(CARDGAME_DRAW_BACK_COMAND)"
+                default :
+                    throw CardGaemError.isSelectNumber
+                }
+        }catch let e as CardGaemError {
+            OutputView.errorMessage(e)
+        }
+        
+        OutputView.showResult(resultMessgae)
+    }
 }
 
-main()
+try main()
