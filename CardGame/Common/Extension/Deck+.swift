597d875b87d01cad76bcb49be6565518a9598f93
Jung seoung Yeo <duwjdtmd91@gmail.com>
2018-05-21 15:26:55 +0900

    Deck+ : equaltable 구현

--- CardGame/Common/Extension/Deck+.swift
+++ CardGame/Common/Extension/Deck+.swift
@@ -6,4 +6,17 @@
 //  Copyright © 2018년 JK. All rights reserved.
 //
 
-import Foundation
+extension Deck: Equatable {
+    static func == (lhs: Deck, rhs: Deck) -> Bool {
+        var lhsTmp = lhs
+        var rhsTmp = rhs
+    
+        for _ in 0 ..< lhs.count() {
+            if lhsTmp.removeOne().desription() != rhsTmp.removeOne().desription() {
+                return false
+            }
+        }
+        
+        return true
+    }
+}
