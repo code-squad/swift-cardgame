2e738ae8b73421cadcb76b915515c8cb497eb235
Jung seoung Yeo <duwjdtmd91@gmail.com>
2018-05-25 21:00:58 +0900

    중첩 enum 형식으로 상수 변경

--- CardGame/Common/Strings.swift
+++ CardGame/Common/Strings.swift
@@ -5,34 +5,75 @@
 //  Created by Jung seoung Yeo on 2018. 5. 20..
 //  Copyright © 2018년 JK. All rights reserved.
 //
+import Foundation
+enum CARDGAME {
+    
+    enum MENU: String {
+        case ASK
+        case RESET = "1"
+        case SHUFFLE = "2"
+        case DRAW = "3"
+        
+        var desription: String {
+            switch self {
+                case .ASK:
+                    return "다음 메뉴를 선택해주세요."
+                case .RESET:
+                    return "1. 카드 초기화"
+                case .SHUFFLE:
+                    return "2. 카드 섞기"
+                case .DRAW:
+                    return "3. 카드 하나 뽑기"
+            }
+        }
+    }
+    
+    enum ERROR: Swift.Error {
+        case isNil
+        case isOrder
+        case unExpected
+        
+        var desription: String {
+            switch self {
+                case .isNil:
+                    return "선택한 값이 없습니다."
+                case .isOrder:
+                    return "잘 못 된 주문입니다."
+                case .unExpected:
+                    return "unExpected Error"
+            }
+        }
+    }
+}
 
-// 카드 게임 시작 메시지
-let CARDGAME_SELECT_MSG = "다음 메뉴를 선택해주세요."
-let CARDGAME_SELECT_RESET_MSG = "1. 카드 초기화"
-let CARDGAME_SELECT_SHUFFLE_MSG = "2. 카드 섞기"
-let CARDGAME_SELECT_DRAW_MSG = "3. 카드 하나 뽑기"
-
-// 카드 select 메시지
-
-// RESET
-let CARDGAME_RESET_MSG = "카드 전체를 초기화했습니다."
-
-// Shuffle
-let CARDGAME_SHUFFLE_TOTAL_COMAND = "총 "
-let CARDGAME_SHUFFLE_BACK_COMAND = "장의 카드를 섞었습니다."
-
-// DRAW
-let CARDGAME_DRAW_TOTAL_COMAND = "전체 "
-let CARDGAME_DRAW_BACK_COMAND = "의 카드가 남았습니다."
-
-
-// selectNumber
-let CARD_RESET = "1"
-let CARD_SHUFFLE = "2"
-let CARD_DRAW = "3"
-
-// ERROR
-let CARDGAME_ISNIL_ERROR_MSG = "선택한 값이 없습니다."
-let CARDGAME_ISORDER_ERROR_MSG = "잘 못 된 주문입니다."
-let CARDGAME_UNEXPECTED_ERROR_MSG = "unExpected Error"
-
+enum GAMEMENU: String {
+    case RESET = "카드 전체를 초기화 했습니다."
+    
+    enum DRAW {
+        case TotalCommand
+        case BackCommand
+        
+        var desription: String {
+            switch self {
+                case .TotalCommand:
+                    return "총"
+                case .BackCommand:
+                    return "장의 카드를 섞었습니다."
+            }
+        }
+    }
+    
+    enum SHUFFLE {
+        case TotalCommand
+        case BackCommand
+        
+        var desription: String {
+            switch self {
+                case .TotalCommand:
+                    return "전체"
+                case .BackCommand:
+                    return "의 카드가 남았습니다."
+            }
+        }
+    }
+}
