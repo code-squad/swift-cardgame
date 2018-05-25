7f03cc5ceb0bb7108a3fb7331cb65e1ec9e48338
Jung seoung Yeo <duwjdtmd91@gmail.com>
2018-05-25 16:08:58 +0900

    deeler protocol 채택

--- CardGame/CardGame/Deeler.swift
+++ CardGame/CardGame/Deeler.swift
@@ -6,4 +6,46 @@
 //  Copyright © 2018년 JK. All rights reserved.
 //
 
-import Foundation
+struct Deeler: RecivedAsk {
+    
+    private var deck: Deck
+    private var oder: Order? = nil
+    private var drawCard: Card? = nil
+    
+    init(_ deck: Deck) {
+        self.deck = deck
+    }
+    
+    mutating func receivedOrder(_ order: Order?) throws {
+       
+        guard let order = order else {
+            throw CardGaemError.isOrder
+        }
+        
+        self.oder = order
+        
+        switch order {
+            case .reset:
+                deck.reset()
+            case .shuffle:
+                deck.shuffle()
+            case .draw:
+                drawCard = deck.removeOne()
+        }
+    }
+    
+    func answer() -> String {
+        
+        switch self.oder {
+            case .reset?:
+                return CARDGAME_RESET_MSG
+            case .shuffle?:
+                return "\(CARDGAME_SHUFFLE_TOTAL_COMAND) \(deck.count()) \(CARDGAME_SHUFFLE_BACK_COMAND)"
+            case .draw?:
+                guard let card = drawCard else { return CARDGAME_UNEXPECTED_ERROR_MSG }
+                return "\(card.desription()) \n\(CARDGAME_DRAW_TOTAL_COMAND) \(deck.count()) \(CARDGAME_DRAW_BACK_COMAND)"
+            case .none:
+                return CARDGAME_UNEXPECTED_ERROR_MSG
+        }
+    }
+}
