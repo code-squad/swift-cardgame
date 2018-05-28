d571340fb13a13fcb89580756a3b7fe8b294e89e
linsaeng <duwjdtmd91@gmail.com>
2018-05-28 17:44:15 +0900

    cardstack을 출력 하는 부분 구현
    deeler.receivedOrder에 셔플후 fieldCardMaker로 통해 생성
    OutputView에서 프로토콜을 받아서 출력 #1

--- CardGame/CardGame/main.swift
+++ CardGame/CardGame/main.swift
@@ -11,22 +11,15 @@ import Foundation
 func main() {
     let deck = Deck()
     var deeler = Deeler(deck)
-    
-    while true {
-//        입력 부 주석 처리
-//        OutputView.cardGameSelectMassgae()
-//        var order: CARDGAME.MENU?
-        
-        do {
-//            order = try InputView.order()
-//            try deeler.receivedOrder(order)
-        }catch let e as CardGaemError {
-            OutputView.errorMessage(e)
-        }catch {
-            print(error)
-        }
-        OutputView.deelerMessage(deeler)
+    do {
+        try deeler.receivedOrder(CARDGAME.MENU.SHUFFLE)
+    }catch let e as CardGaemError {
+        OutputView.errorMessage(e)
+    }catch {
+        print(error)
     }
+    deeler.fieldCardMaker()
+    OutputView.fieldAsk(deeler)
 }
 
 main()
