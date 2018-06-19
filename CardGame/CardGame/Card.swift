f0f1e2aee74121a805609d9c9d8d933a2b0f840a
DrakeYang <drake.the.programmer@gmail.com>
2018-06-19 17:43:37 +0900

    파일추가로 인한 수정

---
+++ CardGame/CardGame/Card.swift
@@ -0,0 +1,9 @@
+//
+//  Card.swift
+//  CardGame
+//
+//  Created by Yoda Codd on 2018. 6. 19..
+//  Copyright © 2018년 JK. All rights reserved.
+//
+
+import Foundation
---
+++ CardGame/CardGame/GameBoard.swift
@@ -0,0 +1,9 @@
+//
+//  GameBoard.swift
+//  CardGame
+//
+//  Created by Yoda Codd on 2018. 6. 19..
+//  Copyright © 2018년 JK. All rights reserved.
+//
+
+import Foundation
