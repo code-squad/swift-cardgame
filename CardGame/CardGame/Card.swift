b294e1c346e199cc9ef21b619187c24fe007ff3d
DrakeYang <drake.the.programmer@gmail.com>
2018-06-20 11:41:11 +0900

    카드객체 내부 프로퍼티들 private 선언

--- CardGame/CardGame/Card.swift
+++ CardGame/CardGame/Card.swift
@@ -11,8 +11,8 @@ import Foundation
 /// 카드 객체를 만든다
 class Card {
     // 숫자, 마크는 계산의 편의성을 위해서 단순인트로 선언. 이니셜라이저에서 범위로 정해진 값만 입력받게함.
-    let numbering : Int
-    let mark : Int
+    private let numbering : Int
+    private let mark : Int
     
     init?(mark: Int, numbering: Int){
         guard  numbering > 0 && numbering < 14 && mark > 0 && mark < 5 else {
