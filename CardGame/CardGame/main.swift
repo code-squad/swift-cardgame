5bc97cfb5817d0ac980f32bc885e6245b5c8f820
DrakeYang <drake.the.programmer@gmail.com>
2018-06-20 15:16:20 +0900

    순서 수정 - 유저입력을 받는 반복문 생성

--- CardGame/CardGame/main.swift
+++ CardGame/CardGame/main.swift
@@ -9,15 +9,31 @@
 import Foundation
 
 func main(){
-    // 보드 생성
-    let gameBoard = GameBoard()
-    
-    // 카드 생성
-    guard let card = gameBoard.makeRandomCard() else {
-        return ()
-    }
-    // 생성된 카드 정보를 프린트
-    print(card.getInfo())
+    // 덱 생성
+    var deck = Deck()
+    // 인풋뷰 생성
+    let inputView = InputView()
+    // 반복문용 플래그
+    var flag = true
+    repeat {
+        // 유저입력을 받는다
+        inputView.printMenuMessage()
+        let userInput = inputView.getUserInput()
+        switch userInput {
+        case "1" :
+            deck.reset()
+            inputView.printResetMessage()
+        case "2" :
+            deck.shuffle()
+            inputView.printShuffleMessage(cardCount: deck.count())
+        case "3" :
+            let removedCard = deck.removeOne()
+            print(removedCard.getInfo())
+            inputView.printRmoveOneMessage(cardCount: deck.count())
+        default : flag = false
+        }
+        
+    } while flag
 }
 
 main()
