addb5d45c10bf0a21b7a424deee40e8e02d2537f
DrakeYang <drake.the.programmer@gmail.com>
2018-06-20 16:07:13 +0900

    카드가 없을경우 카드뽑기,섞기 예외처리. 주석추가

--- CardGame/CardGame/main.swift
+++ CardGame/CardGame/main.swift
@@ -16,25 +16,40 @@ func main(){
     // 반복문용 플래그
     var flag = true
     repeat {
-        // 유저입력을 받는다
+        // 유저입력 요구 메세지
         inputView.printMenuMessage()
+        // 유저입력을 받는다
         let userInput = inputView.getUserInput()
+        // 입력값으로 분기
         switch userInput {
+        // 리셋
         case "1" :
             deck.reset()
             inputView.printResetMessage()
+        // 셔플
         case "2" :
-            deck.shuffle()
-            inputView.printShuffleMessage(cardCount: deck.count())
+            // 카드가 없을경우 에러메세지 출력
+            if deck.count() == 0 {
+                inputView.noMoreCardMessage()
+            }
+            else {
+                deck.shuffle()
+                inputView.printShuffleMessage(cardCount: deck.count())
+            }
+        // 카드 뽑기
         case "3" :
-            let removedCard = deck.removeOne()
-            print(removedCard.getInfo())
-            inputView.printRmoveOneMessage(cardCount: deck.count())
+            if let removedCard = deck.removeOne() {
+                print(removedCard.getInfo())
+                inputView.printRmoveOneMessage(cardCount: deck.count())
+            }
+            // 카드가 없을경우 에러메세지 출력
+            else {
+                inputView.noMoreCardMessage()
+            }
+        // 이외의 경우는 프로그램 종료
         default : flag = false
         }
-        
     } while flag
 }
 
 main()
-
