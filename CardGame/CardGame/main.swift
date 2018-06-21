16bec2713155e067695647ab9a6b6f2547abdda0
DrakeYang <drake.the.programmer@gmail.com>
2018-06-21 11:32:13 +0900

    강제 언래핑 제거

--- CardGame/CardGame/main.swift
+++ CardGame/CardGame/main.swift
@@ -54,27 +54,51 @@ func main(){
 
 /// 기존 기능은 손대지 않고 새로운 메인함수를 만들어서 실행하도록 수정.
 func main2(){
+    // 아웃풋뷰 생성
+    let outputView = OutputView()
     // 덱 생성
     var deck = Deck()
     // 덱에서 카드리스트 생성 후 셔플
     deck.reset()
     deck.shuffle()
     // 슬롯 7개 생성
-    let slot1 = Slot(deck.removeCards(1)!)
-    let slot2 = Slot(deck.removeCards(2)!)
-    let slot3 = Slot(deck.removeCards(3)!)
-    let slot4 = Slot(deck.removeCards(4)!)
-    let slot5 = Slot(deck.removeCards(5)!)
-    let slot6 = Slot(deck.removeCards(6)!)
-    let slot7 = Slot(deck.removeCards(7)!)
+    guard let cards1 = deck.removeCards(1) else {
+        outputView.noMoreCardMessage()
+        return ()
+    }
+    guard let cards2 = deck.removeCards(2) else {
+        outputView.noMoreCardMessage()
+        return ()
+    }
+    guard let cards3 = deck.removeCards(3) else {
+        outputView.noMoreCardMessage()
+        return ()
+    }
+    guard let cards4 = deck.removeCards(4) else {
+        outputView.noMoreCardMessage()
+        return ()
+    }
+    guard let cards5 = deck.removeCards(5) else {
+        outputView.noMoreCardMessage()
+        return ()
+    }
+    guard let cards6 = deck.removeCards(6) else {
+        outputView.noMoreCardMessage()
+        return ()
+    }
+    guard let cards7 = deck.removeCards(7) else {
+        outputView.noMoreCardMessage()
+        return ()
+    }
     
-    print(slot1.getInfo())
-    print(slot2.getInfo())
-    print(slot3.getInfo())
-    print(slot4.getInfo())
-    print(slot5.getInfo())
-    print(slot6.getInfo())
-    print(slot7.getInfo())
+    print(Slot(cards1).getInfo())
+    print(Slot(cards2).getInfo())
+    print(Slot(cards3).getInfo())
+    print(Slot(cards4).getInfo())
+    print(Slot(cards5).getInfo())
+    print(Slot(cards6).getInfo())
+    print(Slot(cards7).getInfo())
+    
 }
 
 main2()
