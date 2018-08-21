42710017333244876d1b78f29bd83d6e5e88d41d
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-08-21 23:08:44 +0900

    OutputView

---
+++ CardGame/CardGame/OutputView.swift
@@ -0,0 +1,15 @@
+//
+//  OutputView.swift
+//  CardGame
+//
+//  Created by 이동건 on 21/08/2018.
+//  Copyright © 2018 JK. All rights reserved.
+//
+
+import Foundation
+
+struct OutputView {
+    static func display(_ data: CustomStringConvertible) {
+        print(data)
+    }
+}
