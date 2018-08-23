12ab479c5fe4680471577a1e1649f2af14ae9d8b
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-08-23 17:30:18 +0900

    CaseIterable 프로토콜을 체택하여 allCases 사용

--- CardGame/CardGame/CardDeckGenerator.swift
+++ CardGame/CardGame/CardDeckGenerator.swift
@@ -9,31 +9,15 @@
 import Foundation
 
 struct CardDeckGenerator {
-    static func generateCards() throws -> [Card] {
+    static func generateCards() -> [Card] {
         var cards: [Card] = []
         
-        for indexOfSymbol in (1...Symbol.availables) {
-            for indexOfNumber in (1...Number.availables) {
-                do {
-                    let card = try generateCard(symbol: indexOfSymbol, number: indexOfNumber)
-                    cards.append(card)
-                }catch let err {
-                    throw err
-                }
+        for symbol in Symbol.allCases {
+            for number in Number.allCases {
+                cards.append(Card(symbol: symbol, number: number))
             }
         }
         
         return cards
-    }
-    
-    static private func generateCard(symbol: Int, number: Int) throws -> Card {
-        guard let symbol = Symbol(rawValue: symbol) else {
-            throw CardGameError.invalidSymbol
-        }
-        guard let number = Number(rawValue: number) else {
-            throw CardGameError.invalidNumber
-        }
-        
-        return Card(symbol: symbol, number: number)
     }
 }
