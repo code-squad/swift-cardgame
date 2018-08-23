1abdfe5a0052c838ea4bd33f18e808e662746d78
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-08-23 15:19:23 +0900

    에러 케이스 추가

--- CardGame/CardGame/CardGameError.swift
+++ CardGame/CardGame/CardGameError.swift
@@ -13,10 +13,16 @@ enum CardGameError: Error, CustomStringConvertible {
         switch self {
         case .invalidMenu:
             return "올바르지 않은 메뉴입니다."
+        case .invalidSymbol:
+            return "유효하지 않은 카드 심볼입니다."
+        case .invalidNumber:
+            return "유효하지 않은 카드 숫자입니다."
         case .unknown:
             return "예상치 못한 에러가 발생하였습니다."
         }
     }
     case invalidMenu
+    case invalidSymbol
+    case invalidNumber
     case unknown
 }
