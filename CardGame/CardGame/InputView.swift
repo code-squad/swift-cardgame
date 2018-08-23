c93db864f4a7d6b586125b8a3f7aa787a34badb4
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-08-23 15:22:19 +0900

    InputView

---
+++ CardGame/CardGame/InputView.swift
@@ -0,0 +1,45 @@
+//
+//  InputView.swift
+//  CardGame
+//
+//  Created by 이동건 on 23/08/2018.
+//  Copyright © 2018 JK. All rights reserved.
+//
+
+import Foundation
+
+enum MenuCategory: String {
+    var description: String {
+        switch self {
+        case .reset:
+            return "1. 카드 초기화\n"
+        case .shuffle:
+            return "2. 카드 섞기\n"
+        case .pick:
+            return "3. 카드 하나 뽑기\n"
+        }
+    }
+    case reset = "1"
+    case shuffle = "2"
+    case pick = "3"
+}
+
+struct InputView {
+    static private var menuDiscriptions: String {
+        var menus = ""
+        menus += MenuCategory.reset.description
+        menus += MenuCategory.shuffle.description
+        menus += MenuCategory.pick.description
+        return menus
+    }
+    
+    static func read() throws -> MenuCategory {
+        print(menuDiscriptions)
+        print("> ", terminator: "")
+        let picked = readLine() ?? ""
+        if let menu = MenuCategory(rawValue: picked) {
+            return menu
+        }
+        throw CardGameError.invalidMenu
+    }
+}
