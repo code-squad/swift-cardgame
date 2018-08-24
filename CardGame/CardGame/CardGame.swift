b7501716d5af11b60e72c7ef6dcbdba1ece1d7ae
oingbong <oingbong@gmail.com>
2018-08-24 19:44:01 +0900

    Add CardGame struct & Divided with main

--- CardGame/CardGame/CardGame.swift
+++ CardGame/CardGame/CardGame.swift
@@ -7,3 +7,22 @@
 //
 
 import Foundation
+
+struct CardGame {
+    public static func playGame() throws {
+        let inputValue = InputView.readInput()
+        
+        // 입력값 비어있는지 확인
+        guard let input = InputView.isEmpty(to: inputValue) else { throw CardError.inputNil }
+        
+        guard let action = Action.init(rawValue: input) else { throw CardError.inputError }
+        
+        do {
+            try action.execute()
+        } catch CardError.noCardsRemaining {
+            throw CardError.noCardsRemaining
+        } catch {
+            throw CardError.unknown
+        }
+    }
+}
--- CardGame/CardGame/main.swift
+++ CardGame/CardGame/main.swift
@@ -8,52 +8,11 @@
 
 import Foundation
 
-enum Action : Int {
-    case reset = 1
-    case shuffle = 2
-    case removeOne = 3
-}
-
-func playGame() throws {
-    let inputValue = InputView.readInput()
-    
-    // 입력값 비어있는지 확인
-    guard let input = InputView.isEmpty(to: inputValue) else { throw CardError.inputNil }
-    
-    guard let action = Int(input) else { throw CardError.inputError }
-    
-    switch action {
-    case Action.reset.rawValue:
-        // reset
-        CardDeck.reset()
-        let action = "카드 전체를 초기화했습니다."
-        let count = CardDeck.count()
-        let countAction = "장의 카드가 있습니다."
-        OutputView.printCard(action: action, count: count, countAction: countAction)
-    case Action.shuffle.rawValue:
-        // shuffle
-        CardDeck.shuffle()
-        let action = ""
-        let count = CardDeck.count()
-        let countAction = "장의 카드를 섞었습니다."
-        OutputView.printCard(action: action, count: count, countAction: countAction)
-    case Action.removeOne.rawValue:
-        // removeOne
-        guard let removeCard = CardDeck.removeOne() else { throw CardError.noCardsRemaining }
-        let action = "\(removeCard)"
-        let count = CardDeck.count()
-        let countAction = "장의 카드가 남아있습니다."
-        OutputView.printCard(action: action, count: count, countAction: countAction)
-    default:
-        throw CardError.inputError
-    }
-}
-
 var play = true
 
 while play {
     do {
-        try playGame()
+        try CardGame.playGame()
     } catch CardError.inputError {
         print(CardError.inputError.rawValue)
         print()
