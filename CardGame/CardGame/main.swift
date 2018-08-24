eb6695f34b07f880a8116714e6949cce6d3c8f2c
oingbong <oingbong@gmail.com>
2018-08-24 19:57:16 +0900

    Edited : func unit -> struct unit in main.swift

--- CardGame/CardGame/main.swift
+++ CardGame/CardGame/main.swift
@@ -8,22 +8,42 @@
 
 import Foundation
 
-var play = true
+struct CardGame {
+    public static func play() {
+        while true {
+            do {
+                let inputValue = InputView.readInput()
+                guard let input = InputView.isEmpty(to: inputValue) else { throw CardError.inputNil }
+                
+                try self.pickMenu(input)
+            } catch CardError.inputError {
+                print(CardError.inputError.rawValue)
+                print()
+            } catch CardError.inputNil {
+                print(CardError.inputNil.rawValue)
+                print()
+            } catch CardError.noCardsRemaining {
+                print(CardError.noCardsRemaining.rawValue)
+                print()
+            } catch {
+                print(CardError.unknown.rawValue)
+                print()
+            }
+        }
+    }
+    
+    private static func pickMenu(_ input: String) throws {
 
-while play {
-    do {
-        try CardGame.playGame()
-    } catch CardError.inputError {
-        print(CardError.inputError.rawValue)
-        print()
-    } catch CardError.inputNil {
-        print(CardError.inputNil.rawValue)
-        print()
-    } catch CardError.noCardsRemaining {
-        print(CardError.noCardsRemaining.rawValue)
-        print()
-    } catch {
-        print(CardError.unknown.rawValue)
-        print()
+        guard let action = Action.init(rawValue: input) else { throw CardError.inputError }
+        
+        do {
+            try action.execute()
+        } catch CardError.noCardsRemaining {
+            throw CardError.noCardsRemaining
+        } catch {
+            throw CardError.unknown
+        }
     }
 }
+
+CardGame.play()
