3502484ba10356c805e5de877cad245c79a23017
oingbong <oingbong@gmail.com>
2018-08-27 11:59:33 +0900

    Edited : case by case -> get variable error in do-catch part

--- CardGame/CardGame/CardError.swift
+++ CardGame/CardGame/CardError.swift
@@ -8,10 +8,25 @@
 
 import Foundation
 
-enum CardError:String , Error {
-    case cardNotFound = "카드를 찾을 수 없습니다."
-    case unknown = "알 수 없는 에러가 발생하였습니다."
-    case inputError = "잘못 입력하셨습니다."
-    case inputNil = "입력값이 비어있습니다."
-    case noCardsRemaining = "남아있는 카드가 없습니다."
+enum CardError: Error , CustomStringConvertible {
+    case cardNotFound
+    case unknown
+    case inputError
+    case inputNil
+    case noCardsRemaining
+    
+    var description: String {
+        switch self {
+        case .cardNotFound:
+            return "카드를 찾을 수 없습니다."
+        case .unknown:
+            return "알 수 없는 에러가 발생하였습니다."
+        case .inputError:
+            return "잘못 입력하셨습니다."
+        case .inputNil:
+            return "입력값이 비어있습니다."
+        case .noCardsRemaining:
+            return "남아있는 카드가 없습니다."
+        }
+    }
 }
