09d492ab2036469c09d4d1796a6ee7b299f25111
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-09-21 12:27:03 -0500

    CardStack

---
+++ CardGame/CardGame/CardStack.swift
@@ -0,0 +1,29 @@
+//
+//  CardStack.swift
+//  CardGame
+//
+//  Created by 이동건 on 21/09/2018.
+//  Copyright © 2018 JK. All rights reserved.
+//
+
+import Foundation
+
+class CardStack: CustomStringConvertible {
+    private var cards: [Card] = []
+    
+    var description: String {
+        var result = "["
+        for i in 0..<cards.count {
+            result += "\(cards[i])"
+            if i != cards.count - 1 {
+                result += ","
+            }
+        }
+        result += "]"
+        return result
+    }
+    
+    init(cards: [Card]) {
+        self.cards = cards
+    }
+}
