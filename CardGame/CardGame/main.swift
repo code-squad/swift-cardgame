202969d208a6e03940a906043a9b32a3a1f1245d
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-10-11 15:34:46 +0900

    Card Game play

--- CardGame/CardGame/main.swift
+++ CardGame/CardGame/main.swift
@@ -8,24 +8,47 @@
 
 import Foundation
 
-struct Main {
-    static func run(){
+class Game {
+    var deck: CardDeck
+    
+    init() {
+        let cards = CardDeckGenerator.generateCards()
+        self.deck = CardDeck(cards: cards)
+    }
+    
+    func play() {
+        deck.shuffle()
         do {
-            let originCards = CardDeckGenerator.generateCards()
-            var deck = CardDeck(cards: originCards)
-            deck.shuffle()
-            
-            while true { // true -> false
-                let picked = try InputView<GameCategory>.read()
-                let player = try InputView<GamePlayer>.read()
-                
+            while deck.total >= 15 {
+                guard let result = try playRound() else { break }
+                OutputView.display(CardGameResult.players(result))
             }
-        } catch let err as CardGameError {
+        }catch let err as CardGameError {
             OutputView.display(err)
-        } catch {
+        }catch {
             OutputView.display(.unknown)
         }
     }
+    
+    private func playRound() throws -> [Player]? {
+        let picked = try InputView<GameCategory>.read()
+        let player = try InputView<GamePlayerCategory>.read()
+        
+        let required = picked.count * (player.count + 1)
+        
+        if deck.total < required {
+            return nil
+        }
+        
+        var players: [Player] = []
+        
+        for _ in 0..<player.count {
+            players.append(Player(.player, with: deck.remove(picked.count)))
+        }
+        players.append(Player(.dealer, with: deck.remove(picked.count)))
+        
+        return players
+    }
 }
 
-Main.run()
+Game().play()
