9b05924064570f17716a5f6779d4708a74e9932f
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-10-11 13:47:08 +0900

    Read several types of menu

--- CardGame/CardGame/main.swift
+++ CardGame/CardGame/main.swift
@@ -13,22 +13,12 @@ struct Main {
         do {
             let originCards = CardDeckGenerator.generateCards()
             var deck = CardDeck(cards: originCards)
-            let stacks = deck.generateStacks()
-            OutputView.display(.stack(stacks))
-            // while문의 true로 입력을 받는 기존의 기능 프로그램에서 false를 넣어 입력문으로 진입하지 않게 막는다.
-            while false { // true -> false 
-                let picked = try InputView.read()
-                switch picked {
-                case .reset:
-                    deck.reset(with: originCards)
-                    OutputView.display(.reset(deck.count))
-                case .shuffle:
-                    deck.shuffle()
-                    OutputView.display(.shuffle(deck.count))
-                case .pick:
-                    let picked = deck.removeOne()
-                    OutputView.display(.pick(picked, deck.count))
-                }
+            deck.shuffle()
+            
+            while true { // true -> false
+                let picked = try InputView<GameCategory>.read()
+                let player = try InputView<GamePlayer>.read()
+                
             }
         } catch let err as CardGameError {
             OutputView.display(err)
