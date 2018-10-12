703d070ac210d2c7ca8d6ab9c917bdcab8483c70
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-10-12 14:36:15 +0900

    Return String => Print Itself

--- CardGame/CardGame/OutputView.swift
+++ CardGame/CardGame/OutputView.swift
@@ -15,32 +15,23 @@ struct OutputView {
     }
     
     static func display(_ values: CardGameResult) {
-        print(convert(from: values))
+        convert(from: values)
     }
     
-    static private func convert(from type: CardGameResult) -> String {
+    static private func convert(from type: CardGameResult) {
         switch type {
         case .reset(let count):
-            return """
-            카드 전체를 초기화했습니다.
-            총 \(count)장의 카드가 있습니다.
-            """
+            print("카드 전체를 초기화했습니다.\n총 \(count)장의 카드가 있습니다.")
         case .shuffle(let count):
-            return "전체 \(count)장의 카드를 섞었습니다."
+            print("전체 \(count)장의 카드를 섞었습니다.")
         case .pick(let card, let remains):
-            return """
-            \(card)
-            총 \(remains)장의 카드가 남았습니다.
-            """
+            print("\(card)\n총 \(remains)장의 카드가 남았습니다.")
         case .group(let group):
-            let result = group.display { (group) -> (String) in
-                var text = ""
+            group.display { (group) in
                 group.forEach {
-                    text += "\($0)\n"
+                    print($0)
                 }
-                return text
             }
-            return result
         }
     }
     
