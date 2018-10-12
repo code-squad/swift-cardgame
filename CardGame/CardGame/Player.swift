d6c64ab4eb1fb61d88c44eb5fd846fc0b52c3985
ehdrjsdlzzzz <ehdrjsdlzzzz@gmail.com>
2018-10-12 11:26:28 +0900

    Set type to private & Set PlayerType conform CustomStringConvertible

--- CardGame/CardGame/Player.swift
+++ CardGame/CardGame/Player.swift
@@ -8,13 +8,25 @@
 
 import Foundation
 
-enum PlayerType {
-    case player
+enum PlayerType: CustomStringConvertible {
+    var description: String {
+        switch self {
+        case .player(let index):
+            return "참가자#\(index + 1)"
+        case .dealer:
+            return "딜러"
+        }
+    }
+    
+    case player(Int)
     case dealer
 }
 
 class Player: CardStack {
-    private (set) var type: PlayerType
+    private var type: PlayerType
+    override var description: String {
+        return "\(type) \(super.description)"
+    }
     
     init(_ type: PlayerType, with cards: [Card]) {
         self.type = type
