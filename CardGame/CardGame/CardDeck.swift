191f0a8f624061bff3925709980384186be8bffb
popsmile <popsmile7@gmail.com>
2018-11-13 16:15:17 +0900

    Create a struct CardDeck

---
+++ CardGame/CardGame/CardDeck.swift
@@ -0,0 +1,13 @@
+//
+//  CardDeck.swift
+//  CardGame
+//
+//  Created by 윤지영 on 13/11/2018.
+//  Copyright © 2018 JK. All rights reserved.
+//
+
+import Foundation
+
+struct CardDeck {
+    private let cards: [Card]
+}
