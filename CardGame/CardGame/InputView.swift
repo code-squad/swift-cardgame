41fb024ef9255aa2132a8f8526d3770541267abc
popsmile <popsmile7@gmail.com>
2018-11-13 20:48:06 +0900

    Create a struct InputView

---
+++ CardGame/CardGame/InputView.swift
@@ -0,0 +1,29 @@
+//
+//  InputView.swift
+//  CardGame
+//
+//  Created by 윤지영 on 13/11/2018.
+//  Copyright © 2018 JK. All rights reserved.
+//
+
+import Foundation
+
+struct InputView {
+    private struct Guide {
+        static let start = "다음 메뉴를 선택해주세요."
+        static let menu = MenuItem.allCases
+            .map { "\($0.rawValue). \($0.message)" }
+            .joined(separator: "\n")
+    }
+
+    private static func showGuide() {
+        print(Guide.start)
+        print(Guide.menu)
+    }
+
+    static func readInput() -> String {
+        showGuide()
+        guard let input: String = readLine() else { return String() }
+        return input
+    }
+}
