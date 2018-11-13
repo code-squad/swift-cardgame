7a25c0926f064deb70056935b488b90f35b98652
popsmile <popsmile7@gmail.com>
2018-11-13 14:12:35 +0900

    Create a class Card
    It uses enum Suit and Rank as types of its private constants.

---
+++ CardGame/CardGame/Card.swift
@@ -0,0 +1,23 @@
+//
+//  File.swift
+//  CardGame
+//
+//  Created by 윤지영 on 13/11/2018.
+//  Copyright © 2018 JK. All rights reserved.
+//
+
+import Foundation
+
+class Card {
+    private let suit: Suit
+    private let rank: Rank
+    
+    init(suit: Suit, rank: Rank) {
+        self.suit = suit
+        self.rank = rank
+    }
+    
+    var description: String {
+        return "\(suit.value)\(rank.value)"
+    }
+}
