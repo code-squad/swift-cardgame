057796b99afea33cd6fbc55ef288d16b279e1305
popsmile <popsmile7@gmail.com>
2018-11-13 20:42:38 +0900

    Create an enum MenuItem including item cases supported

---
+++ CardGame/CardGame/MenuItem.swift
@@ -0,0 +1,24 @@
+//
+//  MenuItem.swift
+//  CardGame
+//
+//  Created by 윤지영 on 13/11/2018.
+//  Copyright © 2018 JK. All rights reserved.
+//
+
+import Foundation
+
+enum MenuItem: Int, CaseIterable {
+    case reset = 1, shuffle, draw
+    
+    var message: String{
+        switch self {
+        case .reset:
+            return "카드 초기화"
+        case .shuffle:
+            return "카드 섞기"
+        case .draw:
+            return "카드 하나 뽑기"
+        }
+    }
+}
