13606eeac7eb82d0d9e93bf4c60311753662a0da
popsmile <popsmile7@gmail.com>
2018-11-14 15:15:08 +0900

    Remove unecessary methods to avoid over-engineering

--- CardGame/CardGame/Menu/MenuController.swift
+++ CardGame/CardGame/Menu/MenuController.swift
@@ -10,20 +10,12 @@ import Foundation
 
 struct MenuController {
 
-    private static func makeInt(str: String) -> Int? {
-        return Int(str)
-    }
-
-    private static func makeMenu(at num: Int) -> MenuItem? {
-        return MenuItem(rawValue: num)
-    }
-
     static func getMenu(of stringSelected: String) -> MenuItem? {
-        guard let numSelected = makeInt(str: stringSelected) else {
+        guard let numSelected = Int(stringSelected) else {
             OutputView.notifyError(.notInt)
             return nil
         }
-        guard let menuSelected = makeMenu(at: numSelected) else {
+        guard let menuSelected = MenuItem(rawValue: numSelected) else {
             OutputView.notifyError(.outOfMenu)
             return nil
         }
