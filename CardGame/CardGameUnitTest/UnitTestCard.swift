472eb90a5d9c5d29bb0a47eacaf397ec933a2900
popsmile <popsmile7@gmail.com>
2018-11-14 15:39:08 +0900

    Add parameter names to the init method of the struct Card
    The parameter names of the init method were removed, because the names were repeated when doing init() Card.  But, the parameter names are necessary to make its role clear, considering the other future case to init Card,

--- CardGame/CardGameUnitTest/UnitTestCard.swift
+++ CardGame/CardGameUnitTest/UnitTestCard.swift
@@ -11,31 +11,31 @@ import XCTest
 class UnitTestCard: XCTestCase {
 
     func testCardDescription_withNumberRank() {
-        let card = Card(.clubs, .eight)
+        let card = Card(suit: .clubs, rank: .eight)
         XCTAssertEqual(card.description, "♣️8")
     }
 
     func testCardDescription_withClubsAndA() {
         let rank = Rank(rawValue: 1)!
-        let card = Card(.clubs, rank)
+        let card = Card(suit: .clubs, rank: rank)
         XCTAssertEqual(card.description, "♣️A")
     }
 
     func testCardDescription_withDiamondsAndJ() {
         let rank = Rank(rawValue: 11)!
-        let card = Card(.diamonds, rank)
+        let card = Card(suit: .diamonds, rank: rank)
         XCTAssertEqual(card.description, "♦️J")
     }
 
     func testCardDescription_withHeartsAndQ() {
         let rank = Rank(rawValue: 12)!
-        let card = Card(.hearts, rank)
+        let card = Card(suit: .hearts, rank: rank)
         XCTAssertEqual(card.description, "♥️Q")
     }
 
     func testCardDescription_withSpadesAndK() {
         let rank = Rank(rawValue: 13)!
-        let card = Card(.spades, rank)
+        let card = Card(suit: .spades, rank: rank)
         XCTAssertEqual(card.description, "♠️K")
     }
 
