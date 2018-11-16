9818f8a651df5431eb097539aeb1af93402f51f8
popsmile <popsmile7@gmail.com>
2018-11-16 20:48:46 +0900

    Create a struct GameInputView

--- CardGame/CardGame/View/GameInputView.swift
+++ CardGame/CardGame/View/GameInputView.swift
@@ -7,3 +7,38 @@
 //
 
 import Foundation
+
+struct GameInputView {
+    private struct Guide {
+        static let start = "카드 게임 종류를 선택하세요."
+        static let gameMode = GameMode.allCases
+            .map { "\($0.rawValue). \($0.numberOfCards)카드" }
+            .joined(separator: "\n")
+        static let numberOfPlayers = "참여할 사람의 인원을 입력하세요."
+    }
+
+    static private func readNumber(with guide: String) -> Int? {
+        print(guide)
+        guard let input: String = readLine() else { return nil }
+        guard let number: Int = Int(input) else { return nil }
+        return number
+    }
+    
+    static func readGameInfo() throws -> (mode: GameMode, numberOfPlayers: Int) {
+        print(Guide.start)
+        guard let menuNumber = readNumber(with: Guide.gameMode) else { throw GameInputError.notInt }
+        guard let mode = GameMode(rawValue: menuNumber) else { throw GameInputError.notMenuSupported }
+        guard let numberOfPlayers = readNumber(with: Guide.numberOfPlayers) else { throw GameInputError.notInt}
+        guard numberOfPlayers > 0 else { throw GameInputError.noPlayer }
+        guard numberOfPlayers < 5 else { throw GameInputError.tooManyPlayer }
+        return (mode, numberOfPlayers)
+    }
+
+}
+
+enum GameInputError: String, Error {
+    case notInt = "숫자로 입력해주세요."
+    case notMenuSupported = "메뉴 내에서 골라주세요"
+    case noPlayer = "1명 이상의 플레이어가 필요합니다."
+    case tooManyPlayer = "최대 4명까지 플레이가 가능합니다."
+}
