65942060162140567a2b43cbbfb1899b6625625b
popsmile <popsmile7@gmail.com>
2018-11-16 20:58:42 +0900

    Remove past run methods of Main

--- CardGame/CardGame/main.swift
+++ CardGame/CardGame/main.swift
@@ -10,24 +10,6 @@ import Foundation
 
 struct Main {
 
-    static func run() {
-        var cardDeck = CardDeck.init()
-        while(!cardDeck.isEmpty) {
-            let numSelected = InputView.readInput()
-            guard let menuSelected = MenuController.getMenu(of: numSelected) else { continue }
-            let result = cardDeck.makeResult(of: menuSelected)
-            OutputView.show(result)
-            guard menuSelected != .quit else { break }
-        }
-    }
-
-    // 임시 실행 함수를 따로 구현하여 기존의 입력, 처리 및 출력 기능을 잠시 비활성화합니다.
-    static func runTest() {
-        let cardDeck = CardDeck()
-        let cardStacks = CardStacks(from: cardDeck)
-        OutputView.showDescription(of: cardStacks)
-    }
-
     static func runGame() {
         do {
             let gameInfo = try GameInputView.readGameInfo()
