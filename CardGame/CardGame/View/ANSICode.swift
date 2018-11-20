58a2e8ee9f47018177262b3ee471aa0473e14750
popsmile <popsmile7@gmail.com>
2018-11-20 23:29:54 +0900

    Remove unnecessary structs in ANSICode

--- CardGame/CardGame/View/ANSICode.swift
+++ CardGame/CardGame/View/ANSICode.swift
@@ -14,7 +14,7 @@ public struct ANSICode {
     static let clear    = "\(escape)2J"
     static let home     = "\(escape)0;0H"
     static let none     = "\(escape)0;0m\(escape)1;0m"
-    
+
     struct text {
         static let black   = escape + "0;30m"
         static let red     = escape + "0;31m"
@@ -37,7 +37,7 @@ public struct ANSICode {
             return "\(escape)38;2;\(R);\(G);\(B)m"
         }
     }
-    
+
     struct bgText {
         static let red     = escape + "0;41m"
         static let green   = escape + "0;42m"
@@ -58,82 +58,10 @@ public struct ANSICode {
             return "\(escape)48;2;\(R);\(G);\(B)m"
         }
     }
-    
+
     struct cursor {
         static func move(row : Int, col : Int) -> String {
             return "\(escape)\(row);\(col)f"
-        }
-    }
-    
-    struct axis {
-        static let AxisLimit = 24
-        private static func drawX() -> String {
-            var result = ""
-            for xLoop in 1...AxisLimit {
-                result += cursor.move(row: AxisLimit+1, col: xLoop*2+2)
-                result += "━━"
-            }
-            for xLoop in 1...(AxisLimit/2) {
-                result += cursor.move(row: AxisLimit+2, col: xLoop*4+1)
-                result += String.init(format: "%3d", xLoop*2)
-            }
-            return result
-        }
-        
-        private static func drawY() -> String {
-            var result = ""
-            for yLoop in 1...AxisLimit {
-                result += cursor.move(row: yLoop, col: 1)
-                if yLoop % 2 == 1 {
-                    result += String.init(format: "%2d|", AxisLimit-yLoop+1)
-                }
-                else {
-                    result += "  |"
-                }
-            }
-            result += cursor.move(row: AxisLimit+1, col: 3)
-            result += "+"
-            result += cursor.move(row: AxisLimit+2, col: 2)
-            result += "0"
-            return result
-        }
-        
-        static func draw() -> String {
-            let result = drawX() + drawY()
-            return result
-        }
-    }
-    
-    struct rect {
-        static func draw(origin : (x:Int,y:Int), size : (width:Int,height:Int), isFill : Bool) -> String {
-            var result = ""
-            for yLoop in origin.y...(origin.y+size.height) {
-                for xLoop in origin.x...(origin.x+size.width) {
-                    result += cursor.move(row: yLoop, col: xLoop)
-                    switch (xLoop,yLoop) {
-                    case (origin.x, origin.y):
-                        result += "┏"
-                    case (origin.x+size.width,origin.y+size.height):
-                        result += "┛"
-                    case (origin.x, origin.y+size.height):
-                        result += "┗"
-                    case (origin.x+size.width,origin.y):
-                        result += "┓"
-                    case (origin.x, _):
-                        fallthrough
-                    case (origin.x+size.width, _):
-                        result += "┃"
-                    case (_, origin.y):
-                        fallthrough
-                    case (_, origin.y+size.height):
-                        result += "━"
-                    default:
-                        if (isFill) { result += " " }
-                        break
-                    }
-                }
-            }
-            return result
         }
     }
 }
