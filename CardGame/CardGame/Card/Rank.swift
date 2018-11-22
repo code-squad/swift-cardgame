2da5ecc00a0f221517438a290d75d30703b2c5bc
popsmile <popsmile7@gmail.com>
2018-11-22 12:11:29 +0900

    Modify sequences of Rank

--- CardGame/CardGame/Card/Rank.swift
+++ CardGame/CardGame/Card/Rank.swift
@@ -13,20 +13,19 @@ import Foundation
 // 또한, int 값을 저장하는 enum 은 case 에 순차적으로 값을 자동 할당해주기 때문에 선언부를 간략화할 수 있다고 생각했습니다.
 
 enum Rank: Int, CaseIterable {
-    case A = 1
-    case two, three, four, five, six, seven, eight, nine, ten
-    case J, Q, K
+    case two = 2, three, four, five, six, seven, eight, nine, ten
+    case J, Q, K, A
 
     var value: String {
         switch self {
-        case .A:
-            return "A"
         case .J:
             return "J"
         case .Q:
             return "Q"
         case .K:
             return "K"
+        case .A:
+            return "A"
         default:
             return "\(self.rawValue)"
         }
