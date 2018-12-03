509863f412503ce22417216a2be0aac33306717e
김장수 <jangsookim.me@gmail.com>
2018-12-03 21:35:36 +0900

    프로토콜을 상속하는 카드덱 구조체 생성

--- CardGame/CardGame/Deck.swift
+++ CardGame/CardGame/Deck.swift
@@ -7,3 +7,42 @@
 //
 
 import Foundation
+
+struct CardDeck: DeckFormat {
+    private var trumps = [Trump]()
+    
+    mutating func reset() {
+        self.trumps = [Trump]()
+        
+        while true {
+            let trump = Trump()
+            if !isDuplicate(trump, in: self.trumps) {
+                self.trumps.append(trump)
+            }
+            
+            if self.trumps.count == 52 { break }
+        }
+    }
+    
+    private func isDuplicate(_ trump: Trump, in trumps: [Trump]) -> Bool {
+        for card in trumps {
+            if trump.description() == card.description() {
+                return true
+            }
+        }
+        
+        return false
+    }
+    
+    mutating func shuffle() {
+        self.trumps.shuffle()
+    }
+    
+    mutating func removeOne() -> CardFormat {
+        return self.trumps.removeFirst()
+    }
+    
+    func count() -> Int {
+        return self.trumps.count
+    }
+}
