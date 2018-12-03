3be4b929ae4e6bca858e5198c98d0afa72edb867
김장수 <jangsookim.me@gmail.com>
2018-12-03 19:28:27 +0900

    InputView 구조체를 요구사항에 맞게 수정

--- CardGame/CardGame/InputView.swift
+++ CardGame/CardGame/InputView.swift
@@ -9,7 +9,11 @@
 import Foundation
 
 struct InputView {
-    static func readStart(ment: String) {
-        print(ment)
+    static func readStart() -> String {
+        print("다음 메뉴(종료하려면 q 또는 quit을 입력)를 선택해주세요.")
+        print("1. 카드 초기화")
+        print("2. 카드 섞기")
+        print("3. 카드 하나 뽑기")
+        return readLine() ?? ""
     }
 }
