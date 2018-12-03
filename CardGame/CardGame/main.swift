08a249334967c1056c52a52025fcc8854e1f6f5c
김장수 <jangsookim.me@gmail.com>
2018-12-03 21:36:07 +0900

    올바른 출력이 이뤄지도록 outputview & main 파일 수정

--- CardGame/CardGame/main.swift
+++ CardGame/CardGame/main.swift
@@ -9,9 +9,37 @@
 import Foundation
 
 func main() {
-    InputView.readStart(ment: "카드를 무작위로 생성합니다.")
-    let trump = Trump()
-    OutputView.printCard(with: trump)
+    var menu = InputView.readStart()
+    var cardDeck = CardDeck()
+    let output = OutputView()
+    cardDeck.reset()
+    
+    while true {
+        switch menu {
+        case "1":
+            cardDeck.reset()
+            output.printReset(by: cardDeck)
+            menu = InputView.readStart()
+        case "2":
+            cardDeck.shuffle()
+            output.printShuffle(by: cardDeck)
+            menu = InputView.readStart()
+        case "3":
+            if cardDeck.count() == 1 {
+                print("카드덱에 더 이상 카드가 없습니다.")
+                return
+            }
+            let firstCard = cardDeck.removeOne()
+            output.printRemoveOne(by: cardDeck, firstCard)
+            menu = InputView.readStart()
+        case "q":
+            print("카드 게임을 종료합니다.")
+            return
+        default:
+            print("올바른 메뉴를 선택하세요.")
+            menu = InputView.readStart()
+        }
+    }
 }
 
 main()
