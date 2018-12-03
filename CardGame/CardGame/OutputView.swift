0e09148b2e1442f942c1728f696680ee02bf2509
김장수 <jangsookim.me@gmail.com>
2018-12-03 21:36:07 +0900

    올바른 출력이 이뤄지도록 outputview & main 파일 수정

--- CardGame/CardGame/OutputView.swift
+++ CardGame/CardGame/OutputView.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 struct OutputView {
-    static func printCard(with trump: CardFormat) {
+    private func printCard(with trump: CardFormat) {
         let trumpData = trump.description()
         guard let suits = trumpData.suits, let rank = trumpData.rank else {
             print("예상하지 못한 오류가 발생했습니다")
@@ -17,5 +17,19 @@ struct OutputView {
         }
 
         print("\(suits.description)\(rank.description)")
+    }
+    
+    func printReset(by deck: DeckFormat) {
+        print("카드 전체를 초기화했습니다.")
+        print("총 \(deck.count())장의 카드가 있습니다.\n")
+    }
+    
+    func printShuffle(by deck: DeckFormat) {
+        print("총 \(deck.count())장의 카드를 섞었습니다.\n")
+    }
+    
+    func printRemoveOne(by deck: DeckFormat, _ trump: CardFormat) {
+        printCard(with: trump)
+        print("총 \(deck.count())장의 카드가 남았습니다.\n")
     }
 }
