ae31fc994e38b5926875cf4cadc49d14f6cd3153
김장수 <jangsookim.me@gmail.com>
2018-12-04 10:59:36 +0900

    main 파일에서 중복문을 줄이도록 수정

--- CardGame/CardGame/main.swift
+++ CardGame/CardGame/main.swift
@@ -9,35 +9,32 @@
 import Foundation
 
 func main() {
-    var menu = InputView.readStart()
     var cardDeck = CardDeck()
     let output = OutputView()
     var turn = 0
     cardDeck.reset()
     
     while turn < 52 {
-        switch menu {
+        switch InputView.readStart() {
         case "1":
             cardDeck.reset()
             output.printReset(by: cardDeck)
-            menu = InputView.readStart()
         case "2":
             cardDeck.shuffle()
             output.printShuffle(by: cardDeck)
-            menu = InputView.readStart()
         case "3":
             let firstCard = cardDeck.removeOne()
             output.printRemoveOne(by: cardDeck, firstCard)
-            menu = InputView.readStart()
         case "q":
             print("카드 게임을 종료합니다.")
             return
         default:
             print("올바른 메뉴를 선택하세요.")
-            menu = InputView.readStart()
         }
+        
         turn += 1
-        if turn == 51 {
+        
+        if turn == 52 {
             print("카드덱이 비었으므로 초기화합니다.")
             cardDeck.reset()
             turn = 0
