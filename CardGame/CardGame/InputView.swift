bc9a89adde86c0d57008ff455cd2a771b5f5e564
김장수 <jangsookim.me@gmail.com>
2018-12-04 15:42:13 +0900

    main에서 입력 기능을 잠시 제거하면서, 매개변수를 따라 불필요한 출력과 로직부분까지 동시에 제거

--- CardGame/CardGame/InputView.swift
+++ CardGame/CardGame/InputView.swift
@@ -9,11 +9,7 @@
 import Foundation
 
 struct InputView {
-    static func readStart() -> String {
-        print("다음 메뉴(종료하려면 q 또는 quit을 입력)를 선택해주세요.")
-        print("1. 카드 초기화")
-        print("2. 카드 섞기")
-        print("3. 카드 하나 뽑기")
-        return readLine() ?? ""
-    }
+    /*
+     입력, 로직, 출력에서 입력에 해당하는 부분을 제거
+     */
 }
--- CardGame/CardGame/main.swift
+++ CardGame/CardGame/main.swift
@@ -11,35 +11,19 @@ import Foundation
 func main() {
     var cardDeck = CardDeck()
     let output = OutputView()
-    var turn = 0
     cardDeck.reset()
     
-    while turn < 52 {
-        switch InputView.readStart() {
-        case "1":
-            cardDeck.reset()
-            output.printReset(by: cardDeck)
-        case "2":
-            cardDeck.shuffle()
-            output.printShuffle(by: cardDeck)
-        case "3":
-            let firstCard = cardDeck.removeOne()
-            output.printRemoveOne(by: cardDeck, firstCard)
-        case "q":
-            print("카드 게임을 종료합니다.")
-            return
-        default:
-            print("올바른 메뉴를 선택하세요.")
-        }
-
-        turn += 1
-
-        if turn == 52 {
-            print("카드덱이 비었으므로 초기화합니다.")
-            cardDeck.reset()
-            turn = 0
-        }
+    // 카드스택을 바로 출력하는 반복문
+    for i in 0...6 {
+        let stud = cardDeck.drawCardStud(of: i)
+        let stack = CardStack(stud: stud)
+        output.printCardStack(by: stack)
     }
+    
+    /*
+        입력, 로직, 출력에서 로직에 해당하는 부분을 제거
+        일시적으로 기능을 제거하는데 입력부터 출력까지 매개변수를 따라가면서 불필요한 부분을 순차적으로 제거
+     */
 }
 
 main()
