c6256567da0356511d4ab0ed15aced2a1ec5b65e
hngfu <djsl1018@naver.com>
2018-12-06 19:30:55 +0900

    CardStack 생성

---
+++ CardGame/CardGame/CardStack.swift
@@ -0,0 +1,33 @@
+//
+//  CardStack.swift
+//  CardGame
+//
+//  Created by 조재흥 on 18. 12. 6..
+//  Copyright © 2018 JK. All rights reserved.
+//
+
+import Foundation
+
+class CardStack {
+    var cards : [Card]
+    
+    init(cards:[Card]) {
+        self.cards = cards
+    }
+    
+    func push(card:Card) {
+        self.cards.append(card)
+    }
+    
+    func pop() -> Card? {
+        return cards.popLast()
+    }
+    
+    func peek() -> Card? {
+        return cards.last
+    }
+    
+    func show() {
+        print(self.cards)
+    }
+}
