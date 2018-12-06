d1828a641347df7cd41b4183d75db6c93393fa7c
hngfu <djsl1018@naver.com>
2018-12-06 11:10:39 +0900

    OutputView 생성

---
+++ CardGame/CardGame/OutputView.swift
@@ -0,0 +1,15 @@
+//
+//  OutputView.swift
+//  CardGame
+//
+//  Created by 조재흥 on 18. 12. 6..
+//  Copyright © 2018 JK. All rights reserved.
+//
+
+import Foundation
+
+struct OutputView {
+    static func outputState(ment:String) {
+        print(ment)
+    }
+}
