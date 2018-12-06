f33258cf3e413dcad8fcb1b11bdf38b973289e34
hngfu <djsl1018@naver.com>
2018-12-06 17:27:08 +0900

    CardGameSetter 생성

---
+++ CardGame/CardGame/CardGameSetter.swift
@@ -0,0 +1,15 @@
+//
+//  CardGameSetter.swift
+//  CardGame
+//
+//  Created by 조재흥 on 18. 12. 6..
+//  Copyright © 2018 JK. All rights reserved.
+//
+
+import Foundation
+
+class CardGameSetter {
+    static func printSetting(of cardSets:[[Card]]) {
+        
+    }
+}
