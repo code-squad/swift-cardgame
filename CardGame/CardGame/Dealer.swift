98383fd145296392711f094ce8e739bee2048f59
hngfu <djsl1018@naver.com>
2018-12-07 15:42:14 +0900

    카드 분배를 딜러가 하도록 수정

--- CardGame/CardGame/Dealer.swift
+++ CardGame/CardGame/Dealer.swift
@@ -9,6 +9,8 @@
 import Foundation
 
 struct Dealer: GamePlayer {
+    static var deck : CardGameDeck = Deck()
+    
     private let name = "딜러"
     private var cards : [Card]
     
@@ -26,6 +28,20 @@ struct Dealer: GamePlayer {
     
     func showName() {
         print(self.name, terminator:"")
+    }
+    
+    static func distributeCard(gameType:Int, numberOfParticipant:Int) -> [GamePlayer] {
+        var gamePlayers = [GamePlayer]()
+        deck.shuffle()
+        for number in 1...numberOfParticipant {
+            gamePlayers.append(Participant.init(name: "참가자#\(number)",cards: deck.draw(few: gameType)))
+        }
+        gamePlayers.append(Dealer.init(cards: deck.draw(few: gameType)))
+        return gamePlayers
+    }
+    
+    static func numberOfDeck() -> Int {
+        return self.deck.count()
     }
 }
 
