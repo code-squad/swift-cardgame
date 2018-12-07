c306c6f02a3bdb6ba7317154b0372f968ae6c3ec
Dongmin.yoon <ehdals8965@naver.com>
2018-12-07 16:58:12 +0900

    comments add and delete

--- CardGame/CardGame/OutputView.swift
+++ CardGame/CardGame/OutputView.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 struct OutputView {
-    
+    // 사용자들의 카드패를 출력
     static func printPlayerCasds(with players: [Player], _ dealer: Dealer) {
         var playerNumber = 1
         for player in players {
@@ -18,34 +18,6 @@ struct OutputView {
         }
         print("딜러 \(dealer.cards)")
     }
-    
-    // 카드 초기화 명령어일 시 콘솔 출력
-//    static func printResetText(in count : Int) {
-//        print("카드 전체를 초기화했습니다.")
-//        print("총 \(count)의 카드가 있습니다.\n")
-//    }
-//
-    // 셔플하는 명령어일 시 콘솔 출력
-//    static func printShuffleText(in count : Int) {
-//        print("전체 \(count)장의 카드를 섞었습니다.\n")
-//
-//    }
-    
-    // 카드를 고르는 명령어일 시 콘솔 출력
-//    static func printPickText(in count : Int, with card : Card?) {
-//        guard let card = card else {
-//            print("카드 덱에 카드가 없습니다.")
-//            return
-//        }
-//        print("\(card)")
-//        print("총 \(count)의 카드가 남아있습니다.\n")
-//    }
-//
-//    static func printCardStack(with stack: [[Card]]) {
-//        for raw in stack {
-//            print(raw)
-//        }
-//    }
 }
 
 
