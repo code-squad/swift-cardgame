c0dbd3e0c4b206ff8e4bad6e592709224fa84125
hngfu <djsl1018@naver.com>
2018-12-10 15:04:46 +0900

    HandRanking 추가

--- CardGame/CardGame/ScoreCalculator.swift
+++ CardGame/CardGame/ScoreCalculator.swift
@@ -24,31 +24,31 @@ struct ScoreCalculator {
         var numberOfPair = self.numberOfPairs(pairs: pairs)
         
         if numberOfPair[4] == 1 {
-            return calculateScore(pairs:pairs, handRankingScore: 4000, bundle: 4)
+            return calculateScore(pairs:pairs, handRanking:.fourCard)
         }
         
         if numberOfPair[3] >= 1 {
-            return calculateScore(pairs:pairs, handRankingScore: 3000, bundle: 3)
+            return calculateScore(pairs:pairs, handRanking:.triple)
         }
         
         if numberOfPair[2] >= 2 {
-            return calculateScore(pairs:pairs, handRankingScore: 2000, bundle: 2)
+            return calculateScore(pairs:pairs, handRanking:.twoPair)
         }
         
         if numberOfPair[2] == 1 {
-            return calculateScore(pairs:pairs, handRankingScore: 1000, bundle: 2)
+            return calculateScore(pairs:pairs, handRanking:.onePair)
         }
         
         if numberOfPair[1] >= 1 {
-            return calculateScore(pairs:pairs, handRankingScore: 0, bundle: 1)
+            return calculateScore(pairs:pairs, handRanking:.highCard)
         }
         
         return 0
     }
     
-    private static func calculateScore(pairs:[[Card]], handRankingScore:Int, bundle:Int) -> Int {
-        var score = handRankingScore
-        let handRanking = pairs.filter(){$0.count == bundle}
+    private static func calculateScore(pairs:[[Card]], handRanking:HandRanking) -> Int {
+        var score = handRanking.rawValue
+        let handRanking = pairs.filter(){$0.count == handRanking.bundle()}
         let maxValueCard = handRanking[0].max { cardA, cardB in cardA.score() < cardB.score()}
         guard let suitRankScore = maxValueCard?.score() else {return 0}
         score += suitRankScore
