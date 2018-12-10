c88a30a4302e95dd9d2cf4b55e043196fce7655d
hngfu <djsl1018@naver.com>
2018-12-10 17:43:21 +0900

    CardStack 객체한테 물어봐서 하도록 수정

--- CardGame/CardGame/ScoreCalculator.swift
+++ CardGame/CardGame/ScoreCalculator.swift
@@ -8,47 +8,37 @@
 
 import Foundation
 
-struct ScoreCalculator {
-    private static func numberOfPairs(pairs:[[Card]]) -> [Int] {
-        var numberOfPair = [0,0,0,0,0]
-        
-        for pair in pairs {
-            numberOfPair[pair.count] += 1
-        }
-        
-        return numberOfPair
-    }
-    
+struct ScoreCalculator {    
     static func calculateScore(cardStack:CardStack) -> Int {
-        let pairs = cardStack.gatherInPairs()
-        var numberOfPair = self.numberOfPairs(pairs: pairs)
+        cardStack.gatherInPairs()
+        cardStack.countNumberOfPair()
         
-        if numberOfPair[4] == 1 {
-            return calculateScore(pairs:pairs, handRanking:.fourCard)
+        if cardStack.numberPair(4) == 1 {
+            return calculateScore(cardStack:cardStack, handRanking:.fourCard)
         }
         
-        if numberOfPair[3] >= 1 {
-            return calculateScore(pairs:pairs, handRanking:.triple)
+        if cardStack.numberPair(3) >= 1 {
+            return calculateScore(cardStack:cardStack, handRanking:.triple)
         }
         
-        if numberOfPair[2] >= 2 {
-            return calculateScore(pairs:pairs, handRanking:.twoPair)
+        if cardStack.numberPair(2) >= 2 {
+            return calculateScore(cardStack:cardStack, handRanking:.twoPair)
         }
         
-        if numberOfPair[2] == 1 {
-            return calculateScore(pairs:pairs, handRanking:.onePair)
+        if cardStack.numberPair(2) == 1 {
+            return calculateScore(cardStack:cardStack, handRanking:.onePair)
         }
         
-        if numberOfPair[1] >= 1 {
-            return calculateScore(pairs:pairs, handRanking:.highCard)
+        if cardStack.numberPair(1) >= 1 {
+            return calculateScore(cardStack:cardStack, handRanking:.highCard)
         }
         
         return 0
     }
     
-    private static func calculateScore(pairs:[[Card]], handRanking:HandRanking) -> Int {
+    private static func calculateScore(cardStack:CardStack, handRanking:HandRanking) -> Int {
         var score = handRanking.rawValue
-        let handRanking = pairs.filter(){$0.count == handRanking.bundle()}
+        guard let handRanking = cardStack.handRanking(bundle: handRanking.bundle()) else {return 0}
         let maxValueCard = handRanking[0].max { cardA, cardB in cardA.score() < cardB.score()}
         guard let suitRankScore = maxValueCard?.score() else {return 0}
         score += suitRankScore
