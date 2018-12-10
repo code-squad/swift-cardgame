eeceee9b5098940b948c826fa3dd2e8e300403cf
hngfu <djsl1018@naver.com>
2018-12-11 01:01:07 +0900

    점수를 계산하는 부분만 ScoreCalculator에 있도록 수정

--- CardGame/CardGame/ScoreCalculator.swift
+++ CardGame/CardGame/ScoreCalculator.swift
@@ -14,34 +14,30 @@ struct ScoreCalculator {
         cardStack.countNumberOfPair()
         
         if cardStack.numberPair(4) == 1 {
-            return calculateScore(cardStack:cardStack, handRanking:.fourCard)
+            return cardStack.maxValuedCardScore(bundle: HandRanking.fourCard.bundle()) +
+            HandRanking.fourCard.rawValue
         }
         
         if cardStack.numberPair(3) >= 1 {
-            return calculateScore(cardStack:cardStack, handRanking:.triple)
+            return cardStack.maxValuedCardScore(bundle: HandRanking.triple.bundle()) +
+            HandRanking.triple.rawValue
         }
         
         if cardStack.numberPair(2) >= 2 {
-            return calculateScore(cardStack:cardStack, handRanking:.twoPair)
+            return cardStack.maxValuedCardScore(bundle: HandRanking.twoPair.bundle()) +
+            HandRanking.twoPair.rawValue
         }
         
         if cardStack.numberPair(2) == 1 {
-            return calculateScore(cardStack:cardStack, handRanking:.onePair)
+            return cardStack.maxValuedCardScore(bundle: HandRanking.onePair.bundle()) +
+            HandRanking.onePair.rawValue
         }
         
         if cardStack.numberPair(1) >= 1 {
-            return calculateScore(cardStack:cardStack, handRanking:.highCard)
+            return cardStack.maxValuedCardScore(bundle: HandRanking.highCard.bundle()) +
+            HandRanking.highCard.rawValue
         }
         
         return 0
-    }
-    
-    private static func calculateScore(cardStack:CardStack, handRanking:HandRanking) -> Int {
-        var score = handRanking.rawValue
-        guard let handRanking = cardStack.handRanking(bundle: handRanking.bundle()) else {return 0}
-        let maxValueCard = handRanking[0].max { cardA, cardB in cardA.score() < cardB.score()}
-        guard let suitRankScore = maxValueCard?.score() else {return 0}
-        score += suitRankScore
-        return score
     }
 }
