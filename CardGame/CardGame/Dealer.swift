0f91f056f99e9b292e03f431a67b2faee81cf984
Dongmin.yoon <ehdals8965@naver.com>
2018-12-11 14:52:54 +0900

    Do Work players object itself

--- CardGame/CardGame/Dealer.swift
+++ CardGame/CardGame/Dealer.swift
@@ -18,7 +18,14 @@ class Dealer : GameParticipate {
     
     // 카드패를 사용자들에게 나누어줌
     func distributeCardToPlayer(to players: Players, by count: Int) {
-        for player in players.getPlayers() { player.receiveCard(makeOneStack(by: count)) }
+        players.distributeCard(cardCount: count) { (cardCount : Int) -> [Card] in
+            var playerCards : [Card] = []
+            for _ in 0..<cardCount {
+                guard let pickCard = deck.removeOne() else { return [] }
+                playerCards.append(pickCard)
+            }
+            return playerCards
+        }
     }
     
     // 한 사용자의 패를 만듦
