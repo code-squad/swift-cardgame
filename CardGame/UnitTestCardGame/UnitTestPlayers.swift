b8f95849f810e87176ccbdc83b1df96cd9c96c8a
Dongmin.yoon <ehdals8965@naver.com>
2018-12-13 17:28:03 +0900

    Add Unit Test Players

--- CardGame/UnitTestCardGame/UnitTestPlayers.swift
+++ CardGame/UnitTestCardGame/UnitTestPlayers.swift
@@ -20,7 +20,15 @@ class UnitTestPlayers: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
     
+    // playersCount() 초기 플레이어 0명인지 확인
+    func testCountPlayers() {
+        XCTAssertEqual(players.countPlayers(), 0, "CountPlayers() don't excute exactly")
+    }
+    
+    // makePlayer() 플레이어 숫자만큼 생성되었는지 확인
     func testMakePlayer() {
-        
+        let dealer: Dealer = Dealer(of: CardDeck())
+        players.makePlayer(by: .three, dealer)
+        XCTAssertEqual(players.countPlayers(), 4, "MakePlayers() method don't excute exactly")
     }
 }
