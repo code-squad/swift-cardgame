c98881ce266bf7fb924dffef5f06445fcdbfe707
Dongmin.yoon <ehdals8965@naver.com>
2018-12-13 17:18:21 +0900

    Add Unit Test Dealer

--- CardGame/UnitTestCardGame/UnitTestDealer.swift
+++ CardGame/UnitTestCardGame/UnitTestDealer.swift
@@ -10,10 +10,11 @@ import XCTest
 
 class UnitTestDealer: XCTestCase {
     var dealer: Dealer!
+    var deck: CardDeck!
     
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
-        var deck: CardDeck = CardDeck()
+        deck = CardDeck()
         dealer = Dealer(of: deck)
     }
 
@@ -22,8 +23,11 @@ class UnitTestDealer: XCTestCase {
         dealer = nil
     }
     
+    // 카드를 플레이어에게 나누어 주고 난 후 카드 수 확인
     func testDistributeCardToPlayer() {
-        
+        let players: Players = Players()
+        players.makePlayer(by: .three, dealer)
+        dealer.distributeCardToPlayer(to: players, by: 5)
+        XCTAssertEqual(deck.count(), 32, "not Distribute Card To Player")
     }
-
 }
---
+++ CardGame/UnitTestCardGame/UnitTestPlayers.swift
@@ -0,0 +1,26 @@
+//
+//  UnitTestPlayers.swift
+//  UnitTestCardGame
+//
+//  Created by 윤동민 on 13/12/2018.
+//  Copyright © 2018 JK. All rights reserved.
+//
+
+import XCTest
+
+class UnitTestPlayers: XCTestCase {
+    var players: Players!
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        players = Players()
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func testMakePlayer() {
+        
+    }
+}
