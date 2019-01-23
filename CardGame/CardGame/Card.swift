f34ace2d2ffd571eba5c107c2e54d8cb2efb3f2e
conyconydev <conyconydev@gmail.com>
2019-01-23 17:29:38 +0900

    전체적인 구조 설정

---
+++ CardGame/CardGame/Card.swift
@@ -0,0 +1,13 @@
+//
+//  Card.swift
+//  CardGame
+//
+//  Created by Elena on 23/01/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+class Card {
+    
+}
---
+++ CardGame/CardGame/Shape.swift
@@ -0,0 +1,9 @@
+//
+//  Shape.swift
+//  CardGame
+//
+//  Created by Elena on 23/01/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
