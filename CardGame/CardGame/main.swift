1b25924280dd0ec1962c858b76edb63cddefca67
conyconydev <conyconydev@gmail.com>
2019-01-23 19:46:16 +0900

    main 만듬

--- CardGame/CardGame/main.swift
+++ CardGame/CardGame/main.swift
@@ -8,4 +8,17 @@
 
 import Foundation
 
+// 숫자와 모양을 랜덤으로 생성해주는 함수
+func createRandomCard() -> Card {
+    let number = Number.allCases.randomElement() ?? .ace
+    let shape = Shape.allCases.randomElement() ?? .spades
+    return Card(number: number, shape: shape)
+}
 
+// 만들고 -> 출력
+func main() {
+    let card = createRandomCard()
+    OutputView.output(card)
+}
+
+main()
