9506fe1fe6238ce329bf217295b002f262bf395a
conyconydev <conyconydev@gmail.com>
2019-01-23 19:45:51 +0900

    카드 만듬

--- CardGame/CardGame/Card.swift
+++ CardGame/CardGame/Card.swift
@@ -8,6 +8,17 @@
 
 import Foundation
 
-class Card {
+// 카드에 숫자와 도형을 표현하고, 카드를 표현하는 문자열을 정의
+class Card: CustomStringConvertible {
+    private let number: Number
+    private let shape: Shape
     
+    init(number: Number, shape: Shape) {
+        self.number = number
+        self.shape = shape
+    }
+    
+    var description: String {
+        return "\(shape.rawValue)\(number.value)"
+    }
 }
