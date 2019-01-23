f9f850cb7fc8a4304cd5cba76fd34ee875a01080
conyconydev <conyconydev@gmail.com>
2019-01-23 19:44:46 +0900

    숫자를 만들어줌 ,  enum을 만들고 CaseIterable 추가

--- CardGame/CardGame/Number.swift
+++ CardGame/CardGame/Number.swift
@@ -7,3 +7,25 @@
 //
 
 import Foundation
+
+//enum을 만들고 스위프트에게 allCases 배열을 자동으로 만들어달라고 하는 구조
+enum Number: Int, CaseIterable {
+    case ace = 1
+    case two, three, four, five, six, seven, eight, nine, ten
+    case jack, queen, king
+    
+    var value: String {
+        switch self {
+        case .ace:
+            return "A"
+        case .jack:
+            return "J"
+        case .queen:
+            return "Q"
+        case .king:
+            return "K"
+        default:
+            return "\(self.rawValue)"
+        }
+    }
+}
