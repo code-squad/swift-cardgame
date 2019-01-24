81905fb1143da5d28e6c60ade1112feddff60d1d
conyconydev <conyconydev@gmail.com>
2019-01-25 01:01:10 +0900

    카드덱파일을 만들어서 요구사항인 모든 종류의 카드 객체 인스턴스를 포함하는 카드덱 구조체를 구현

---
+++ CardGame/CardGame/cardDeck.swift
@@ -0,0 +1,77 @@
+//
+//  cardDeck.swift
+//  CardGame
+//
+//  Created by Elena on 24/01/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+protocol CardGameSet{
+    func count() -> Int
+    mutating func shuffle()
+    mutating func removeOne() -> Card
+    mutating func reset()
+}
+
+struct CardDeck: CardGameSet{
+
+    private var cards: [Card] = []
+    
+    init() {
+        reset()
+    }
+    
+    // 갖고 있는 카드 개수를 반환
+    func count() -> Int {
+        return cards.count
+    }
+    // 전체 카드를 랜덤하게 섞는다.
+    mutating func shuffle() {
+        cards.shuffle()
+    }
+    // 카드 인스턴스 중에 하나를 반환하고 목록에서 삭제한다.
+    mutating func removeOne() -> Card {
+        return cards.removeFirst()
+    }
+    // 처음처럼 모든 카드를 다시 채워 넣는다.
+    mutating func reset() {
+        cards.removeAll()
+        addAllFiller()
+        shuffle()
+    }
+    
+    // 전체 채우기
+    private mutating func addAllFiller() {
+        for shape in Shape.allCases {
+            for number in Number.allCases {
+                self.cards.append(Card(number: number, shape: shape))
+            }
+        }
+    }
+    
+    // 카드가 비어있는지 확인하기 위해 만듬.
+    var isEmpty: Bool {
+        return cards.isEmpty
+    }
+    
+    // 역할 : 메뉴선택에 따라 출력결과를 다르게 반환
+    // 매개변수 : Menu , 반환형 : String
+    mutating func getResult(_ menu: Menu) -> String {
+        switch menu {
+        case .reset:
+            reset()
+            return "카드 전체를 초기화했습니다."
+        case .shuffle:
+            shuffle()
+            return "전체 \(self.count())장의 카드를 섞었습니다."
+        case .pullOut:
+            let card = removeOne()
+            return "\(card)\n총 \(self.count())장의 카드가 남아있습니다."
+        case .quit:
+            return "종료"
+        }
+    }
+   
+}
