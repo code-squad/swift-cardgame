0ddbd27d56dbcc2fff907670aa0aa9001820d2f3
conyconydev <conyconydev@gmail.com>
2019-01-24 11:09:30 +0900

    CardFactory 파일을 만들어서 main createRandomCard() 함수 분리

---
+++ CardGame/CardGame/CardFactory.swift
@@ -0,0 +1,18 @@
+//
+//  CardFactory.swift
+//  CardGame
+//
+//  Created by Elena on 24/01/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+struct CardFactory {
+    // 숫자와 모양을 랜덤으로 생성해주는 함수
+    static func createRandomCard() -> Card {
+        let number = Number.allCases.randomElement() ?? .ace
+        let shape = Shape.allCases.randomElement() ?? .spades
+        return Card(number: number, shape: shape)
+    }
+}
