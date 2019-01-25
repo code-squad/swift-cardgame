170ccab4038509362d72cfed9084f534134f2aab
conyconydev <conyconydev@gmail.com>
2019-01-25 15:11:31 +0900

    테스트코드 추가

--- CardGame/CardGame/CardFactory.swift
+++ CardGame/CardGame/CardFactory.swift
@@ -15,11 +15,5 @@ struct CardFactory {
         let shape = Shape.allCases.randomElement() ?? .spades
         return Card(number: number, shape: shape)
     }
+    
 }
-
-/*
- count() 갖고 있는 카드 개수를 반환한다.
- shuffle() 기능은 전체 카드를 랜덤하게 섞는다.
- removeOne() 기능은 카드 인스턴스 중에 하나를 반환하고 목록에서 삭제한다.
- reset() 처음처럼 모든 카드를 다시 채워넣는다.
- */
