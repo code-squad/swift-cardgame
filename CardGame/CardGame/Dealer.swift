63f30d40025a16f42555936ca8ce81269749baec
conyconydev <conyconydev@gmail.com>
2019-01-31 15:35:49 +0900

    카드게임 시작과 카드게임 모드를 추가함

---
+++ CardGame/CardGame/Dealer.swift
@@ -0,0 +1,9 @@
+//
+//  Dealer.swift
+//  CardGame
+//
+//  Created by Elena on 30/01/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
---
+++ CardGame/CardGame/Player.swift
@@ -0,0 +1,9 @@
+//
+//  Player.swift
+//  CardGame
+//
+//  Created by Elena on 30/01/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
