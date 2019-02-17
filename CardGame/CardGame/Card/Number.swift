7a95b48fadd6cc8e741166a09eff3e7b1db6b18b
conyconydev <conyconydev@gmail.com>
2019-02-18 06:04:07 +0900

    socre 계산을 위해 변경 , 추가

--- CardGame/CardGame/Card/Number.swift
+++ CardGame/CardGame/Card/Number.swift
@@ -9,23 +9,24 @@
 import Foundation
 
 //enum을 만들고 스위프트에게 allCases 배열을 자동으로 만들어달라고 하는 구조
+// A가 1 일수도 있고, 아닐수도 있다. A는 Ace다.
 enum Number: Int, CaseIterable {
-    case ace = 1
-    case two, three, four, five, six, seven, eight, nine, ten
-    case jack, queen, king
+    case two = 2, three, four, five, six, seven, eight, nine, ten
+    case jack, queen, king, ace
     
     var value: String {
         switch self {
-        case .ace:
-            return "A"
         case .jack:
             return "J"
         case .queen:
             return "Q"
         case .king:
             return "K"
+        case .ace:
+            return "A"
         default:
             return "\(self.rawValue)"
         }
     }
 }
+
