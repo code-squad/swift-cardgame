b9489f9eee370f6804c722fa07231decca237c15
conyconydev <conyconydev@gmail.com>
2019-02-18 05:57:17 +0900

    HandBetting 카드 추가 , 비교 구문 추가

---
+++ CardGame/TestCardGame/TestCardGameMock.swift
@@ -0,0 +1,15 @@
+//
+//  TestCardGameMock.swift
+//  TestCardGameMock
+//
+//  Created by Elena on 14/02/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import XCTest
+
+class TestCardGameMock: XCTestCase {
+
+    
+
+}
