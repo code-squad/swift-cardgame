65c4e549eb0f79c73c9bfe47854150af898d8850
kiljh <kiljh@me.com>
2019-05-21 17:30:51 +0900

    추가 - input 받은 값을 저장할 프로퍼티 선언

--- CardGame/CardGame/InputView.swift
+++ CardGame/CardGame/InputView.swift
@@ -9,6 +9,8 @@
 import Foundation
 
 struct InputView {
+    private(set) var valueEntered: String = ""
+    
     func printMenu () {
         print("다음 메뉴를 선택해주세요.")
         print("1. 카드 초기화")
@@ -16,8 +18,8 @@ struct InputView {
         print("3. 카드 하나 뽑기")
     }
     
-    mutating func readInput () -> String {
-        print(">", terminator: "")
-        return readLine() ?? "0"
+    mutating func readInput () {
+        print("> ", terminator: "")
+        valueEntered = readLine() ?? "0"
     }
 }
