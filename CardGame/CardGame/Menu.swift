6b3c8e9bc8aa0668443821d6ef1bda42383b9d49
kiljh <kiljh@me.com>
2019-05-23 15:04:05 +0900

    생성 - Menu enum 타입 생성
    메뉴를 저장할 Menu enum 생성

---
+++ CardGame/CardGame/Menu.swift
@@ -0,0 +1,15 @@
+//
+//  Menu.swift
+//  CardGame
+//
+//  Created by joon-ho kil on 5/23/19.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+enum Menu: Int {
+    case reset = 1
+    case shuffle = 2
+    case removeOne = 3
+}
