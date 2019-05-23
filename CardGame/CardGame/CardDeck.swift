ef96dd66ba79b4b653e79b9de4fd24f28ebe53e6
kiljh <kiljh@me.com>
2019-05-23 17:41:17 +0900

    수정 - 카드덱에서 자체적으로 요청하던 것을 상위 모듈로 이동

--- CardGame/CardGame/CardDeck.swift
+++ CardGame/CardGame/CardDeck.swift
@@ -23,43 +23,4 @@ struct CardDeck {
             cards.append(Card(rank: rank, suit: suit))
         }
     }
-    
-    /// 갖고 있는 카드 개수를 반환한다.
-    func count () -> Int {
-        return cards.count
-    }
-    
-    /// 기능은 전체 카드를 랜덤하게 섞는다.
-    mutating func shuffle () {
-        cards = cards.shuffled()
-    }
-    
-    /// 기능은 카드 인스턴스 중에 하나를 반환하고 목록에서 삭제한다.
-    mutating func removeOne () throws -> Card {
-        guard let firstCard = cards.first else { throw CardError.NotExistCard }
-        
-        cards.removeFirst()
-        return firstCard
-    }
-    
-    /// 처음처럼 모든 카드를 다시 채워넣는다.
-    mutating func reset () {
-        cards.removeAll()
-        
-        for suit in Card.Suit.allCases {
-            initSuit(suit: suit)
-        }
-    }
-
-    /// 메뉴 숫자에 따라 함수를 실행시켜준다.
-    mutating func executeMenu (_ menu: Menu) throws -> Card? {
-        var returnCard: Card? = nil
-        switch menu {
-        case .removeOne: returnCard = try removeOne()
-        case .reset: reset()
-        case .shuffle: shuffle()
-        }
-        
-        return returnCard
-    }
 }
