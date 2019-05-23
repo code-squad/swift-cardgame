2b5f59872ec2e902cdd779808a06998724a7379b
kiljh <kiljh@me.com>
2019-05-23 17:42:40 +0900

    구현 - 카드덱에 요청하는 상위 모듈 구현

--- CardGame/CardGame/CardGame.swift
+++ CardGame/CardGame/CardGame.swift
@@ -7,3 +7,47 @@
 //
 
 import Foundation
+
+struct CardGame {
+    var cardDeck = CardDeck()
+    
+    /// 메뉴 숫자에 따라 함수를 실행시켜준다.
+    mutating func executeMenu (_ menu: Menu) throws -> (Card?, Int) {
+        var returnCard: Card? = nil
+        switch menu {
+        case .removeOne: returnCard = try removeOne()
+        case .reset: reset()
+        case .shuffle: shuffle()
+        }
+        
+        return (returnCard, count())
+    }
+
+    /// 갖고 있는 카드 개수를 반환한다.
+    private func count () -> Int {
+        return cardDeck.cards.count
+    }
+    
+    /// 기능은 전체 카드를 랜덤하게 섞는다.
+    private mutating func shuffle () {
+        cardDeck.cards = cardDeck.cards.shuffled()
+    }
+    
+    /// 기능은 카드 인스턴스 중에 하나를 반환하고 목록에서 삭제한다.
+    private mutating func removeOne () throws -> Card {
+        guard let firstCard = cardDeck.cards.first else {
+            throw CardError.NotExistCard
+        }
+        
+        cardDeck.cards.removeFirst()
+        
+        return firstCard
+    }
+    
+    /// 처음처럼 모든 카드를 다시 채워넣는다.
+    private mutating func reset () {
+        cardDeck.cards.removeAll()
+        
+        cardDeck = CardDeck()
+    }
+}
