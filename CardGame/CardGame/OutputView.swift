ce47c86ef9f82ddbab81e2db5c2d859c1e75228f
kiljh <kiljh@me.com>
2019-05-23 15:05:02 +0900

    구현 - 출력하는 OutputView 구조체 구현

--- CardGame/CardGame/OutputView.swift
+++ CardGame/CardGame/OutputView.swift
@@ -9,5 +9,11 @@
 import Foundation
 
 struct OutputView {
-    
+    func printMessage (_ menu: Menu, cardReturned: Card?, _ cardCount: Int) {
+        switch menu {
+        case .reset: print("카드 전체를 초기화했습니다.\n총 \(cardCount)장의 카드가 있습니다.")
+        case .shuffle: print("전체 \(cardCount) 장의 카드를 섞었습니다.")
+        case .removeOne: print("\(String(describing: cardReturned))\n총 \(cardCount)장의 카드가 남아있습니다.")
+        }
+    }
 }
