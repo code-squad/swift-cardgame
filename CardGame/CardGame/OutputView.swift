ecb116fc0e4ff401da019de4c35f894df6cd5ce2
kiljh <kiljh@me.com>
2019-05-23 10:53:57 +0900

    추가 - OutputView 구조체 생성

---
+++ CardGame/CardGame/OutputView.swift
@@ -0,0 +1,13 @@
+//
+//  OutputView.swift
+//  CardGame
+//
+//  Created by joon-ho kil on 5/23/19.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+struct OutputView {
+    
+}
