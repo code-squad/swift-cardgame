39a703829047c7dad0aabad970c8359a35d59fac
kiljh <kiljh@me.com>
2019-05-25 13:24:52 +0900

    수정 - 참가인원을 입력받도록 소스 추가

--- CardGame/CardGame/InputView.swift
+++ CardGame/CardGame/InputView.swift
@@ -9,19 +9,29 @@
 import Foundation
 
 struct InputView {
-    func printMenu () {
+    private func printMenu () {
         print("카드 게임 종류를 선택하세요.")
         print("1. 7카드")
         print("2. 5카드")
         print("9. 종료")
     }
     
-    func printPeopleInput () {
+    private func printUserCountInputGuidance () {
         print("참여할 사람의 인원을 입력하세요.")
     }
     
-    func readInput () -> String {
+    func readInput () -> (String, String) {
+        let menu: String
+        let people: String
+        
+        printMenu()
         print("> ", terminator: "")
-        return readLine() ?? "0"
+        menu = readLine() ?? "0"
+        
+        printUserCountInputGuidance()
+        print("> ", terminator: "")
+        people = readLine() ?? "0"
+        
+        return (menu, people)
     }
 }
