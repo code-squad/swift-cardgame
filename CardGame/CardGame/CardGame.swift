d632c0264b3172160ac162d995d0fcc16ed42bd6
kiljh <kiljh@me.com>
2019-05-26 00:18:15 +0900

    수정 - 바뀐 요구사항에 따른 CardGame 구조체 수정

--- CardGame/CardGame/CardGame.swift
+++ CardGame/CardGame/CardGame.swift
@@ -12,20 +12,24 @@ struct CardGame {
     private var cardDeck = CardDeck()
     
     /// 메뉴 숫자에 따라 함수를 실행시켜준다.
-    mutating func executeMenu (_ menu: Menu) throws -> (Card?, Int) {
-        var returnCard: Card? = nil
+    mutating func executeMenu (_ menu: Menu, _ userCount: UserCount) throws -> [String: [Card]] {
+        var result = [String: [Card]]()
         
-        switch menu {
-        case .removeOne:
-            returnCard = try cardDeck.removeOne()
-        case .reset:
-            cardDeck.reset()
-        case .shuffle:
-            cardDeck.shuffle()
-        case .exit:
-            break
+        for user in 1...userCount.rawValue {
+            result["참가자#"+String(user)] = try getOneUserCard(menu)
         }
         
-        return (returnCard, cardDeck.count())
+        return result
+    }
+    
+    /// 한 유저의 모든 카드 리턴 {
+    private mutating func getOneUserCard (_ menu: Menu) throws -> [Card] {
+        var oneUserCards = [Card]()
+        
+        for _ in 0..<menu.rawValue {
+            oneUserCards.append(try cardDeck.removeOne())
+        }
+        
+        return oneUserCards
     }
 }
