5e93497fe67f02835871cb9962ae11b4afd46af4
kiljh <kiljh@me.com>
2019-05-27 14:48:00 +0900

    생성 - 딜러 구조체 생성

---
+++ CardGame/CardGame/Dealer.swift
@@ -0,0 +1,14 @@
+//
+//  Dealer.swift
+//  CardGame
+//
+//  Created by joon-ho kil on 5/27/19.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+struct Dealer: Human {
+    let name = "딜러"
+    var cards = [Card]()
+}
