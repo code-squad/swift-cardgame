10f5405e3d450ed4545e34b4c8230ec27bacfbb0
kiljh <kiljh@me.com>
2019-05-27 14:48:16 +0900

    생성 - 사람 프로토콜 생성

---
+++ CardGame/CardGame/Human.swift
@@ -0,0 +1,13 @@
+//
+//  Human.swift
+//  CardGame
+//
+//  Created by joon-ho kil on 5/27/19.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+protocol Human {
+    var name: String { get }
+}
