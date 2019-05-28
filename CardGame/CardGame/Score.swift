f2665569af2bad85f8d1d3984631002f3515033f
kiljh <kiljh@me.com>
2019-05-28 11:15:35 +0900

    생성 - 점수를 저장할 enum 타입 생성

---
+++ CardGame/CardGame/Score.swift
@@ -0,0 +1,17 @@
+//
+//  Score.swift
+//  CardGame
+//
+//  Created by joon-ho kil on 5/28/19.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+enum Score: Int {
+    case pair = 1
+    case twoPair
+    case threeOfAKind
+    case fourOfAKind
+    case straight
+}
