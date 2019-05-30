72c4751745ef3a98f820aa7a37f0f0407d2129ec
kiljh <kiljh@me.com>
2019-05-30 16:18:44 +0900

    구현 - 같은 카드 갯수에 따른 순위 결정
    앞으로 스트라이트와 같은 숫자일 때 카드 숫자로 비교, 같은 카드 숫자이면 카드 모양으로 비교하도록 구현 필요

--- CardGame/CardGame/OutputView.swift
+++ CardGame/CardGame/OutputView.swift
@@ -20,4 +20,13 @@ struct OutputView {
         
         print(Participant.dealer)
     }
+    
+    func printResult (_ players: [Player]) {
+        let playersSorted = players.sorted(by: { (arg0, arg1) -> Bool in
+            return arg0.getScore() > arg1.getScore()
+        })
+        
+        print("이번 게임의 승자는 \(playersSorted[0].getName()) 입니다.")
+//      이번 게임의 승자는 참가자#2 입니다.
+    }
 }
