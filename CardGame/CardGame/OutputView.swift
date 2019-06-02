692c8e80bc7098523d2da25371c1bbd9efa0e67c
Sunghyun Kim <brm0821@me.com>
2019-06-02 17:48:42 +0900

    OutputView 작성

---
+++ CardGame/CardGame/OutputView.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+struct OutputView {
+    
+    static func show(_ text: String) { print(text) }
+    
+    static func nextLine() { print() }
+    
+    static func show(card: Card) { print("[\(card)]") }
+}
