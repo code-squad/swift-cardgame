39f52a9ddd82f73572f466821735cc32209fe241
Sunghyun Kim <brm0821@me.com>
2019-06-02 17:47:00 +0900

    show(options:) 작성

--- CardGame/CardGame/InputView.swift
+++ CardGame/CardGame/InputView.swift
@@ -6,4 +6,10 @@ struct InputView {
         print("\(something): ", terminator: "")
         return readLine() ?? ""
     }
+    
+    static func show(options: [String]) {
+        for index in options.indices {
+            print("\(index + 1). \(options[index])")
+        }
+    }
 }
