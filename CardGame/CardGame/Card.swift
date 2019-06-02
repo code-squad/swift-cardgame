1bdd540d0e69bf38b1d2220bf8dcbcc0df2e6785
Sunghyun Kim <brm0821@me.com>
2019-06-02 09:18:34 +0900

    Card에 Rank 열거형 작성

---
+++ CardGame/CardGame/Card.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+class Card: CustomStringConvertible {
+    // 카드 숫자는 종류가 13가지로 제한되어 있기 때문에 열거형을 선택함. 또한 Card 유형과 연관된 유형이기에 내포되도록 함. RawValue로는 카드 실제 숫자를 나타내는 Int를 가지도록 함.
+    enum Rank: Int, CaseIterable, CustomStringConvertible {
+        case ace = 1
+        case two, three, four, five, six, seven, eight, nine, ten
+        case jack, queen, king
+        
+        var description: String {
+            switch self {
+            case .ace: return "A"
+            case .jack: return "J"
+            case .queen: return "Q"
+            case .king: return "K"
+            default: return String(self.rawValue)
+            }
+        }
+    }
+}
