864aed4a839da1287c4706fc8329f95667533a83
Sunghyun Kim <brm0821@me.com>
2019-06-02 17:50:27 +0900

    InputControl 작성

---
+++ CardGame/CardGame/InputControl.swift
@@ -0,0 +1,26 @@
+import Foundation
+
+struct InputControl {
+    
+    private var deck = Deck()
+    
+    func playCardGame() {
+        
+    }
+    
+    private func showDeckCount() {
+        
+    }
+    
+    private func resetDeck() {
+        
+    }
+    
+    private func shuffleDeck() {
+        
+    }
+    
+    private func drawCard() {
+        
+    }
+}
