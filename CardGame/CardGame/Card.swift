4526408bfdeb2c0c3b1c26f6d8102fd7e37d39a3
Sunghyun Kim <brm0821@me.com>
2019-06-02 09:19:36 +0900

    rank와 suit를 속성으로 추가함

--- CardGame/CardGame/Card.swift
+++ CardGame/CardGame/Card.swift
@@ -31,4 +31,16 @@ class Card: CustomStringConvertible {
             }
         }
     }
+    
+    let rank: Rank
+    let suit: Suit
+    
+    init(rank: Rank, suit: Suit) {
+        self.rank = rank
+        self.suit = suit
+    }
+    
+    var description: String {
+        return "\(suit) \(rank)"
+    }
 }
