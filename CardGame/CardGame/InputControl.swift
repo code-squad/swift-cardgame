ff144a8cdb720907805ed998252621956303483d
Sunghyun Kim <brm0821@me.com>
2019-06-02 17:51:24 +0900

    Option 열거형 작성

--- CardGame/CardGame/InputControl.swift
+++ CardGame/CardGame/InputControl.swift
@@ -4,6 +4,21 @@ struct InputControl {
     
     private var deck = Deck()
     
+    enum Option: Int, CaseIterable, CustomStringConvertible {
+        case resetDeck = 1
+        case shuffleDeck
+        case drawCard
+        case showOptions
+        var description: String {
+            switch self {
+            case .drawCard: return "카드 뽑기"
+            case .resetDeck: return "덱 초기화"
+            case .showOptions: return "선택지 보기"
+            case .shuffleDeck: return "덱 섞기"
+            }
+        }
+    }
+    
     func playCardGame() {
         
     }
