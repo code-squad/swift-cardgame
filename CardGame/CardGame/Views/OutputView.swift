74eec4d565b6b9c511d000d695ef9adeac6f3bbc
Sunghyun Kim <sunghyun_k@icloud.com>
2019-06-10 22:05:27 +0900

    함수가 한줄로 작성되어 있던 것을 다음 줄로 내림

--- CardGame/CardGame/Views/OutputView.swift
+++ CardGame/CardGame/Views/OutputView.swift
@@ -2,9 +2,15 @@ import Foundation
 
 struct OutputView {
     
-    static func show(_ text: String) { print(text) }
+    static func show(_ text: String) {
+        print(text)
+    }
     
-    static func nextLine() { print() }
+    static func nextLine() {
+        print()
+    }
     
-    static func show(card: Card) { print("[\(card)]") }
+    static func show(card: Card) {
+        print("[\(card)]")
+    }
 }
