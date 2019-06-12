2d61afbd177c3b0153235c9171a49b36938ac055
Sunghyun Kim <sunghyun_k@icloud.com>
2019-06-12 14:50:02 +0900

    askForNumber 작성

--- CardGame/CardGame/Views/InputView.swift
+++ CardGame/CardGame/Views/InputView.swift
@@ -7,6 +7,17 @@ class InputView: TextView {
         return readLine() ?? ""
     }
     
+    func askForNumber(_ something: String) -> Int {
+        print("\(something): ", terminator: "")
+        while true {
+            guard let number = Int(readLine() ?? "") else {
+                    show("숫자가 아닙니다. 다시 입력하세요.")
+                    continue
+            }
+            return number
+        }
+    }
+    
     /// 선택지에 번호를 붙여 텍스트로 나타냅니다.
     private func show(options: [String]) {
         for index in options.indices {
@@ -18,7 +29,7 @@ class InputView: TextView {
         show(options: options)
         // 제공된 선택지 중 유효한 선택지가 선택될 때 까지 반복합니다.
         while true {
-            guard let choice = Int(ask("> ")),
+            guard let choice = Int(ask("선택지")),
                 (1...options.count).contains(choice) else {
                     show("유효하지 않은 선택입니다. 다시 입력하세요.")
                     continue
