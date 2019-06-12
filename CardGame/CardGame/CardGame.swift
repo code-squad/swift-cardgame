920f9d90b46502b218cea5519a4a7cd64d1bc195
Sunghyun Kim <sunghyun_k@icloud.com>
2019-06-12 16:07:39 +0900

    덱을 섞도록 작성함

--- CardGame/CardGame/CardGame.swift
+++ CardGame/CardGame/CardGame.swift
@@ -26,6 +26,7 @@ class CardGame {
     }
     
     func giveCardsToPlayers(rule: GameRule) throws {
+        deck.shuffle()
         for index in players.indices {
             players[index].hand = try drawCards(count: rule.rawValue)
         }
