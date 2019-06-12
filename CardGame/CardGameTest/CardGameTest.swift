10be20793bf03e93db887224705eea00b149bf6c
kiljh <kiljh@me.com>
2019-06-12 16:30:50 +0900

    추가 - 유닛 테스트 생성

---
+++ CardGame/CardGameTest/CardGameTest.swift
@@ -0,0 +1,16 @@
+//
+//  CardGameTest.swift
+//  CardGameTest
+//
+//  Created by joon-ho kil on 6/12/19.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import XCTest
+
+class CardGameTest: XCTestCase {
+    func testCardGame() {
+        
+    }
+
+}
