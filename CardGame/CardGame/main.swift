2f3e56b14b22af3554f505e2f2970cb07f755740
hwj0623 <hwj0623@naver.com>
2019-06-12 21:05:19 +0900

    9-1 (3) 메인 함수 호출 생성

--- CardGame/CardGame/main.swift
+++ CardGame/CardGame/main.swift
@@ -8,4 +8,18 @@
 
 import Foundation
 
+struct Main {
+    
+    static func main(){
+        let heartJack = Card(type: CardType.heart, number: CardNumber.jack)
+        print("\(heartJack)")
+        let cloverQueen = Card(type: CardType.clover, number: CardNumber.queen)
+        print("\(cloverQueen)")
+        let diamondKing = Card(type: CardType.diamond, number: CardNumber.king)
+        print("\(diamondKing)")
+        let spadeAce = Card(type: CardType.spade, number: CardNumber.ace)
+        print("\(spadeAce)")
+    }
+}
 
+Main.main()
