c71ca196206512f09aa0a30ce23f27d8290a1ff9
Sunghyun Kim <sunghyun_k@icloud.com>
2019-06-12 16:08:13 +0900

    main에서 입/출력 및 카드게임을 제어하도록 작성

--- CardGame/CardGame/main.swift
+++ CardGame/CardGame/main.swift
@@ -1,5 +1,55 @@
 import Foundation
 
-var control = InputControl()
+var outputView = OutputView()
+var inputView = InputView()
 
-control.playCardGame()
+func main() {
+    
+    var game = CardGame()
+    
+    // 카드 게임의 종류를 저장합니다.
+    inputView.show("카드 게임 종류를 선택하세요.")
+    let ruleChoice = inputView.askForChoice(options: ["7카드", "5카드"])
+    
+    // 반드시 options의 개수만큼 케이스를 작성해야 합니다.
+    let rule: CardGame.GameRule!
+    switch ruleChoice {
+    case 1: rule = .sevenCardStud
+    case 2: rule = .fiveCardStud
+    default: rule = nil
+    }
+    
+    // 참여 인원을 결정합니다. 인원의 범위를 초과한 경우 다시 입력 받습니다.
+    var isPlayerReady = false
+    while !isPlayerReady {
+        inputView.show("참여할 사람의 인원을 입력하세요.")
+        let players = inputView.askForNumber("사람 수")
+        
+        do {
+            try game.addPlayers(count: players)
+            isPlayerReady = true
+        } catch CardGame.Error.playerLimitExceeded {
+            outputView.show("플레이어 제한 초과")
+        } catch {
+            outputView.showUnexpectedError(error)
+        }
+    }
+    
+    
+    var hasEnoughCards = true
+    while hasEnoughCards {
+        // 카드를 뽑습니다. 카드가 다 떨어지면 게임을 종료합니다.
+        do {
+            try game.giveCardsToPlayers(rule: rule)
+            // 플레이어 및 딜러 패를 출력합니다.
+            outputView.showAllHands(game: game)
+        } catch CardGame.Error.outOfCards {
+            outputView.show("카드가 다 떨어짐")
+            hasEnoughCards = false
+        } catch {
+            outputView.showUnexpectedError(error)
+        }
+    }
+}
+
+main()
