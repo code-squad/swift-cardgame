c3d211235d37b56941cd9cc37796ba371b889008
Sunghyun Kim <sunghyun_k@icloud.com>
2019-06-12 16:06:05 +0900

    모든 패를 보여주는 메소드 작성

--- CardGame/CardGame/Views/OutputView.swift
+++ CardGame/CardGame/Views/OutputView.swift
@@ -2,7 +2,19 @@ import Foundation
 
 class OutputView: TextView {
     
-    func showPlayerHand(player: Player) {
+    func showPlayerHand(_ player: Player) {
         show("\(player.name): \(player.hand)")
+    }
+    
+    func showAllHands(game: CardGame) {
+        for player in game.players {
+            outputView.showPlayerHand(player)
+        }
+        outputView.showPlayerHand(game.dealer)
+        nextLine()
+    }
+    
+    func showUnexpectedError(_ error: Error) {
+        show("예상치 못한 오류: \(error)")
     }
 }
