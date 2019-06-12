e08805228f6073458c7c205f9c6fda42772f9082
Sunghyun Kim <sunghyun_k@icloud.com>
2019-06-12 12:29:49 +0900

    플레이어 추가, 카드 나눠주기, 카드 여러장 뽑기 메소드 추가

--- CardGame/CardGame/CardGame.swift
+++ CardGame/CardGame/CardGame.swift
@@ -2,38 +2,40 @@ import Foundation
 
 class CardGame {
     
-    var deck = Deck()
+    private var deck = Deck()
+    private var players = [Player]()
+    private var dealer = Player(name: "딜러")
     
-    enum GameType {
-        
-        case fiveCardStud, sevenCardStud
-        
-        var numberOfCards: Int {
-            switch self {
-            case .fiveCardStud: return 5
-            case .sevenCardStud: return 7
-            }
-        }
+    enum GameRule: Int {
+        case fiveCardStud = 5
+        case sevenCardStud = 7
     }
     
     enum CardGameError: Error {
         case notEnoughCards
     }
     
-    func playGame(playerCount: Int, gameType: GameType) throws {
-        
-        deck.shuffle()
-        var playerHands = [[Card]](repeating: [], count: playerCount)
-        
-        for index in playerHands.indices {
+    func addPlayers(count: Int) {
+        for index in 1...count {
+            players.append(Player(name: "참가자#\(index)"))
+        }
+    }
+    
+    func giveCardsToPlayers(rule: GameRule) throws {
+        for index in players.indices {
+            players[index].hand = try drawCards(count: rule.rawValue)
+        }
+        dealer.hand = try drawCards(count: rule.rawValue)
+    }
+    
+    func drawCards(count: Int) throws -> [Card] {
+        var cards = [Card]()
+        for _ in 1...count {
             guard let card = deck.drawCard() else {
                 throw CardGameError.notEnoughCards
             }
-            playerHands[index].append(card)
+            cards.append(card)
         }
-        
-        
-        
+        return cards
     }
-    
 }
