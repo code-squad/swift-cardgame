0975b68616ff1af2d78c01874303e237d11dfc91
hwj0623 <hwj0623@naver.com>
2019-06-12 21:03:00 +0900

    9-1 (1) 카드의 종류와 숫자를 나타내는 enum 생성

---
+++ CardGame/Enums/CardNumber.swift
@@ -0,0 +1,30 @@
+//
+//  CardNumber.swift
+//  CardGame
+//
+//  Created by hw on 12/06/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+/// 각각의 DataType을 enum으로 구성합니다.
+/// 둘은 서로의 상하관계가 존재하지 않으므로 별도의 enum으로 분류하였습니다.
+enum CardNumber: Int, CustomStringConvertible{
+    case ace = 1, two, three, four, five, six, seven, eight, nine, jack, queen, king
+    var description: String {
+        switch self {
+        case .ace:
+            return "A"
+        case .two, .three, .four, .five, .six, .seven, .eight, .nine :
+            return "\(self.rawValue)"
+        case .jack:
+            return "J"
+        case .queen:
+            return "Q"
+        case .king:
+            return "K"
+        }
+    }
+}
+
