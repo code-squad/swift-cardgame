8298b9b2fc97045df2595b3723dbba1b383bc0bd
hwj0623 <hwj0623@naver.com>
2019-06-13 22:24:20 +0900

    STEP 9-2 (5) OutputView 구현

--- CardGame/Views/OutputView.swift
+++ CardGame/Views/OutputView.swift
@@ -7,3 +7,15 @@
 //
 
 import Foundation
+
+class OutputView {
+    static func displayMenuResult(_ selected:GameMenu, deck: CardDeck){
+        print("\(selected.getResultMessage(cardDeck: deck))")
+    }
+    static func printCard(_ card: Card){
+        print(card)
+    }
+    static func displayDrawCardErrorAutoHandleMessage(){
+        print("잔여카드가 없으므로 카드덱을 초기화합니다.")
+    }
+}
