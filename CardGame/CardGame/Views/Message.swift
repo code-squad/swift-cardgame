e551c7985caeaee8d885c1329d26a611085d249e
Sunghyun Kim <sunghyun_k@icloud.com>
2019-06-13 16:11:12 +0900

    Message 작성

---
+++ CardGame/CardGame/Views/Message.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+struct Message {
+    static let gameRuleQuestion = "카드 게임 종류를 선택하세요."
+    static let playerCountQuestion = "참여할 사람의 인원을 입력하세요."
+    static let playerCount = "사람 수"
+}
+
+struct ErrorMessage {
+    static let playerLimitExceeded = "플레이어 제한 초과함"
+    static let outOfCards = "카드가 다 떨어짐"
+}
