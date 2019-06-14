34bc161060ffa539ba2ea9ff6d8fb3eadef72e37
Sunghyun Kim <sunghyun_k@icloud.com>
2019-06-14 16:55:02 +0900

    Card 구조체가 Hashable 프로토콜을 채택하도록 수정

--- CardGame/CardGame/Card.swift
+++ CardGame/CardGame/Card.swift
@@ -1,14 +1,20 @@
 import Foundation
 
-class Card: CustomStringConvertible {
+class Card: Hashable, CustomStringConvertible {
+    
     /* 카드 숫자는 종류가 13가지로 제한되어 있기 때문에 열거형을 선택함. 또한 Card 유형과 연관된 유형이기에 내포되도록 함.
      RawValue를 통한 인스턴스 생성도 가능하도록 카드 실제 숫자를 나타내는 Int를 RawValue로 가지도록 함.
      카드 덱을 만들기 용이하게 하기 위하여 CaseIterable을 Rank와 Suit에 채택함.
      */
-    enum Rank: Int, CaseIterable, CustomStringConvertible {
+    enum Rank: Int, Comparable, CaseIterable, CustomStringConvertible {
+        
         case ace = 1
         case two, three, four, five, six, seven, eight, nine, ten
         case jack, queen, king
+        
+        static func < (lhs: Card.Rank, rhs: Card.Rank) -> Bool {
+            return lhs.rawValue < rhs.rawValue
+        }
         
         var description: String {
             switch self {
@@ -35,12 +41,21 @@ class Card: CustomStringConvertible {
         }
     }
     
-    private var rank: Rank
-    private var suit: Suit
+    private(set) var rank: Rank
+    private(set) var suit: Suit
     
     init(rank: Rank, suit: Suit) {
         self.rank = rank
         self.suit = suit
+    }
+    
+    static func == (lhs: Card, rhs: Card) -> Bool {
+        return lhs.rank == rhs.rank && lhs.suit == rhs.suit
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(rank)
+        hasher.combine(suit)
     }
     
     var description: String {
