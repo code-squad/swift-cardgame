893f1f47e6ced0014d06832bc61697734b8adbc0
Sunghyun Kim <sunghyun_k@icloud.com>
2019-06-14 17:44:24 +0900

    함수 수정

--- CardGame/CardGame/Score.swift
+++ CardGame/CardGame/Score.swift
@@ -2,21 +2,22 @@ import Foundation
 
 struct Score: Comparable {
     
-    var matchCount: [Int : [Card.Rank]]
+    private(set) var matchCount: [Int : [Card.Rank]]
     
     init(cards: [Card]) {
         let groupedByRank = Dictionary(grouping: cards) { $0.rank }
         matchCount = Dictionary(grouping: groupedByRank) { $1.count }.mapValues { $0.map { $0.key } }
         for (cardCount, _) in matchCount {
-            matchCount[cardCount]!.sort()
+            matchCount[cardCount]!.sort { $0 > $1 }
         }
     }
     
     
     static func < (lhs: Score, rhs: Score) -> Bool {
-        for cardCount in 1...4 {
-            
-        }
+        
+        
+        
+        
         
     }
 }
