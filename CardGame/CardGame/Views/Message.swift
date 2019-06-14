ff6c1fb00966c8ccd77288ff8ea5454811d5a587
Sunghyun Kim <sunghyun_k@icloud.com>
2019-06-14 11:44:13 +0900

    InputMessage 제거

--- CardGame/CardGame/Views/Message.swift
+++ CardGame/CardGame/Views/Message.swift
@@ -4,10 +4,5 @@ struct Message {
     static let gameRuleQuestion = "카드 게임 종류를 선택하세요."
     static let playerCountQuestion = "참여할 사람의 인원을 입력하세요."
     static let playerCount = "사람 수"
-}
-
-struct InputMessage {
-    static let notNumber = "숫자가 아닙니다. 다시 입력하세요."
-    static let invalidChoice = "유효하지 않은 선택입니다. 다시 입력하세요."
-    static let choice = "선택지"
+    static let selectOption = "옵션 선택"
 }
