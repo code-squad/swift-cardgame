2344679be6b03ad67d07114d94616dd101a87653
Sunghyun Kim <sunghyun_k@icloud.com>
2019-06-16 15:11:21 +0900

    점수 체계 작성

--- CardGame/CardGame/Score.swift
+++ CardGame/CardGame/Score.swift
@@ -1,9 +1,41 @@
 import Foundation
 
+
+private enum PokerHand: Comparable {
+    
+    case highCard(Card.Rank)
+    case onePair(Card.Rank)
+    case twoPair(Card.Rank)
+    case threeOfAKind(Card.Rank)
+    case straight(Card.Rank)
+    case fourOfAKind(Card.Rank)
+    
+    static func < (lhs: PokerHand, rhs: PokerHand) -> Bool {
+        
+        
+        return true
+    }
+    
+    var score: Int {
+        switch self {
+        case .highCard(_): return 1
+        case .onePair(_): return 2
+        case .twoPair(_): return 3
+        case .threeOfAKind(_): return 4
+        case .straight(_): return 5
+        case .fourOfAKind(_): return 6
+        }
+    }
+}
+
 struct Score: Comparable {
     
+    
+    
+    
+    
     private var cards: [Card]
-    private var matchCount: [Int: [Card.Rank]] {
+    private var matchedRanks: [Int: [Card.Rank]] {
         let groupedByRank = Dictionary(grouping: cards) { $0.rank }
         let groupedByCount = Dictionary(grouping: groupedByRank) { $1.count }
         let matchInfo = groupedByCount.mapValues { $0.map { $0.key } }
@@ -12,22 +44,42 @@ struct Score: Comparable {
     
     static func < (lhs: Score, rhs: Score) -> Bool {
         
-        // 4카드를 확인합니다.
-        if var lhs = lhs.matchCount[4],
-            var rhs = rhs.matchCount[4] {
-            while !(lhs.isEmpty || rhs.isEmpty) {
-                if lhs.removeFirst() < rhs.removeFirst() {
-                    return true
+        func compare(matchCount: Int) -> Bool? {
+            if var lhs = lhs.matchedRanks[matchCount],
+                var rhs = rhs.matchedRanks[matchCount] {
+                while !(lhs.isEmpty || rhs.isEmpty) {
+                    if lhs.removeFirst() < rhs.removeFirst() {
+                        return true
+                    }
                 }
                 return false
+            } else if lhs.matchedRanks[matchCount] == nil,
+                rhs.matchedRanks[matchCount] != nil {
+                return true
+            } else if lhs.matchedRanks[matchCount] != nil,
+                rhs.matchedRanks[matchCount] == nil {
+                return false
             }
-        } else if lhs.matchCount[4] == nil,
-            rhs.matchCount[4] != nil {
-            return true
-        } else if lhs.matchCount[4] != nil,
-            rhs.matchCount[4] == nil {
-            return false
+            return nil
         }
+        
+        // 포카드를 확인합니다.
+        if let fourOfAKind = compare(matchCount: 4) {
+            return fourOfAKind
+        }
+        
+        // 스트레이트를 확인합니다.
+        
+        
+        // 트리플을 확인합니다.
+        if let threeOfAKind = compare(matchCount: 3) {
+            return threeOfAKind
+        }
+        
+        // 투페어를 확인합니다.
+        
+        
+        // 원페어를 확인합니다.
         
         
         
