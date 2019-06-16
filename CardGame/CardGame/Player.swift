7f9c98bcd0ba99f0b859b7c144fb82db832ac05c
Sunghyun Kim <sunghyun_k@icloud.com>
2019-06-16 09:19:20 +0900

    Player에서 점수계산을 하지 않도록 변경 Score 작성

--- CardGame/CardGame/Player.swift
+++ CardGame/CardGame/Player.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-class Player: Comparable {
+class Player {
     
     private(set) var name: String
     private(set) var hand = [Card]()
@@ -25,46 +25,6 @@ class Player: Comparable {
     func resetHand() {
         hand.removeAll()
     }
-    
-    private var matchCount: [Int: [Card.Rank]] {
-        let groupedByRank = Dictionary(grouping: hand) { $0.rank }
-        let groupedByCount = Dictionary(grouping: groupedByRank) { $1.count }
-        let matchInfo = groupedByCount.mapValues { $0.map { $0.key } }
-        return matchInfo.mapValues { $0.sorted { $0 > $1 } }
-    }
-    
-    static func < (lhs: Player, rhs: Player) -> Bool {
-        
-        // 4카드를 확인합니다.
-        if var lhs = lhs.matchCount[4],
-            var rhs = rhs.matchCount[4] {
-            while !(lhs.isEmpty || rhs.isEmpty) {
-                if lhs.removeFirst() < rhs.removeFirst() {
-                    return true
-                }
-                return false
-            }
-        } else if lhs.matchCount[4] == nil,
-            rhs.matchCount[4] != nil {
-            return true
-        } else if lhs.matchCount[4] != nil,
-            rhs.matchCount[4] == nil {
-            return false
-        }
-        
-        
-        
-        return true
-    }
-    
-    static func == (lhs: Player, rhs: Player) -> Bool {
-        
-        
-        
-        
-        return true
-    }
-    
 }
 
 class Dealer: Player {
--- CardGame/CardGame/Score.swift
+++ CardGame/CardGame/Score.swift
@@ -3,33 +3,31 @@ import Foundation
 struct Score: Comparable {
     
     private var cards: [Card]
-    
-    private(set) var matchCount: [Int : [Card.Rank]] {
+    private var matchCount: [Int: [Card.Rank]] {
         let groupedByRank = Dictionary(grouping: cards) { $0.rank }
-        matchCount = Dictionary(grouping: groupedByRank) { $1.count }.mapValues { $0.map { $0.key } }
-        matchCount = matchCount.mapValues { $0.sorted() }
-        for (cardCount, _) in matchCount {
-            matchCount[cardCount]!.sort { $0 > $1 }
-        }
-        
+        let groupedByCount = Dictionary(grouping: groupedByRank) { $1.count }
+        let matchInfo = groupedByCount.mapValues { $0.map { $0.key } }
+        return matchInfo.mapValues { $0.sorted { $0 > $1 } }
     }
-    
-    init(cards: [Card]) {
-        self.cards = cards
-        
-        let groupedByRank = Dictionary(grouping: cards) { $0.rank }
-        matchCount = Dictionary(grouping: groupedByRank) { $1.count }.mapValues { $0.map { $0.key } }
-        matchCount = matchCount.mapValues { $0.sorted() }
-        for (cardCount, _) in matchCount {
-            matchCount[cardCount]!.sort { $0 > $1 }
-        }
-    }
-    
     
     static func < (lhs: Score, rhs: Score) -> Bool {
         
-        
-        
+        // 4카드를 확인합니다.
+        if var lhs = lhs.matchCount[4],
+            var rhs = rhs.matchCount[4] {
+            while !(lhs.isEmpty || rhs.isEmpty) {
+                if lhs.removeFirst() < rhs.removeFirst() {
+                    return true
+                }
+                return false
+            }
+        } else if lhs.matchCount[4] == nil,
+            rhs.matchCount[4] != nil {
+            return true
+        } else if lhs.matchCount[4] != nil,
+            rhs.matchCount[4] == nil {
+            return false
+        }
         
         
         
