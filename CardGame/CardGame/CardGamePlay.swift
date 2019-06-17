a9cb76fac4e47f820fc22249db8838c045ad52bd
hwj0623 <hwj0623@naver.com>
2019-06-17 13:29:58 +0900

    STEP 9-3 (6) main 로직 분리, 플레이 전용 클래스 추가

--- CardGame/CardGame/CardGamePlay.swift
+++ CardGame/CardGame/CardGamePlay.swift
@@ -7,3 +7,49 @@
 //
 
 import Foundation
+
+class CardGamePlay {
+    func playGame() throws -> Void{
+        var cardDeck : CardDeck = CardDeck.init()
+        var input: String
+        var selectedGameType: GameType
+        var numberOfPlayers: Int
+        while true {
+            //게임 종류
+            GameInputView.selectGame()
+            do {
+                input = try InputView.readInput()
+                selectedGameType = try Validation.validateGameType(input)
+            } catch let errorType as GameMenuError {
+                print(errorType.description)
+                continue
+            }
+            //인원
+            GameInputView.selectNumberOfPlayers()
+            do {
+                input = try InputView.readInput()
+                numberOfPlayers = try Validation.validateGamePlayerNumbers(input)
+            } catch let errorType as GameMenuError {
+                print(errorType.description)
+                continue
+            }
+            //처리
+            var playerList = buildPlayersList(num: numberOfPlayers)
+        }
+    }
+    
+    private func buildPlayersList(num numbers: Int) -> [GamePlayer] {
+        var playerList = [GamePlayer]()
+        for index in 1..<(numbers+1) {
+            let player = GamePlayer("참가자#\(index)")
+            playerList.append(player)
+        }
+        let player = GamePlayer("딜러")
+        playerList.append(player)
+        return playerList
+    }
+    
+    private func fillDeckOfPlayers(players: [GamePlayer]) -> [GamePlayer]{
+        return players
+    }
+}
