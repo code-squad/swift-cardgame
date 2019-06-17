2519b0e257dbbb0790db7ddaece914c117d779c7
hwj0623 <hwj0623@naver.com>
2019-06-17 17:04:57 +0900

    STEP 9-3 (12) rebase 후 Validation 로직 개선

--- CardGame/Errors/GameMenuError.swift
+++ CardGame/Errors/GameMenuError.swift
@@ -10,14 +10,14 @@ import Foundation
 
 enum GameMenuError : Error, CustomStringConvertible{
     case notANumber
-    case outOfRange
+    case outOfRangeMenu
     case outOfRangePlayer
     
     var description: String {
         switch self{
         case .notANumber:
             return "입력값이 숫자가 아닙니다."
-        case .outOfRange:
+        case .outOfRangeMenu:
             return "입력값이 메뉴 선택범위 밖입니다."
         case .outOfRangePlayer:
             return "입력값이 게임 참여 인원을 초과했습니다.\n1~4명을 입력하세요."
---
+++ CardGame/Validation/Validation.swift
@@ -0,0 +1,48 @@
+//
+//  Validation.swift
+//  CardGame
+//
+//  Created by hw on 13/06/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+class Validation {
+    static func validateGamePlayerNumbers(_ input: String) throws -> Int {
+        let number = try convertInteger(input)
+        if !isInRange(number, base: 1, limit: 3) {
+            throw GameMenuError.outOfRangePlayer
+        }
+        return number
+    }
+    
+    static func validateGameType(_ input: String) throws -> GameType{
+        let number = try convertInteger(input)
+        if !isInRange(number, base: 1, limit: 3) {
+            throw GameMenuError.outOfRangeMenu
+        }
+        let instruction : GameType = try GameType(number)
+        return instruction
+    }
+    
+    static func validateGameMenu (_ input: String) throws -> GameMenu {
+        let number = try convertInteger(input)
+        let instruction : GameMenu = try GameMenu(number)
+        return instruction
+    }
+    
+    static private func convertInteger(_ input: String) throws -> Int {
+        guard let selectedNumber = Int(input) else {
+            throw GameMenuError.notANumber
+        }
+        return selectedNumber
+    }
+    
+    static private func isInRange(_ number : Int, base: Int, limit: Int) -> Bool {
+        if number >= base && number <= limit {
+            return true
+        }
+        return false
+    }
+}
