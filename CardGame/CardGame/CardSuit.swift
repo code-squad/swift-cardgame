12151b308b41bc99f569c62678a0f7c9346ab309
gukjin jang <jgj455@gmail.com>
2019-06-17 14:24:02 +0900

    Add: CardSuit 열거형 - 카드 모양을 속성으로 가지는 열거형 추가

---
+++ CardGame/CardGame/CardSuit.swift
@@ -0,0 +1,16 @@
+//
+//  CardSuit.swift
+//  CardGame
+//
+//  Created by jang gukjin on 17/06/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+enum CardSuit: Character {
+    case diamond = "♦️"
+    case heart = "♥️"
+    case clover = "♣️"
+    case spade = "♠️"
+}
