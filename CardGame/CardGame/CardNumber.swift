d677f36c5f238425524e24a20c2f7061d1088176
gukjin jang <jgj455@gmail.com>
2019-06-17 14:54:16 +0900

    Modify: CardSuit 열거형 수정

---
+++ CardGame/CardGame/CardNumber.swift
@@ -0,0 +1,13 @@
+//
+//  CardNumber.swift
+//  CardGame
+//
+//  Created by jang gukjin on 17/06/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+enum CardNumber {
+    case one, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve
+}
