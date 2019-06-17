a42a9319b35bb7baea87ca6f419402cc177a4565
Sunghyun Kim <sunghyun_k@icloud.com>
2019-06-17 18:51:01 +0900

    테스트 케이스 추가

--- CardGame/ScoreTest/ScoreTest.swift
+++ CardGame/ScoreTest/ScoreTest.swift
@@ -30,10 +30,30 @@ class ScoreTest: XCTestCase {
         XCTAssert(Score(cards: cardsA) < Score(cards: cardsB))
     }
     
+    func testStraight() {
+        let cardsA = [
+            Card(rank: .ace, suit: .diamonds),
+            Card(rank: .two, suit: .spades),
+            Card(rank: .three, suit: .hearts),
+            Card(rank: .four, suit: .clubs),
+            Card(rank: .five, suit: .clubs),
+        ]
+        
+        let cardsB = [
+            Card(rank: .two, suit: .spades),
+            Card(rank: .three, suit: .clubs),
+            Card(rank: .four, suit: .diamonds),
+            Card(rank: .five, suit: .hearts),
+            Card(rank: .six, suit: .clubs),
+        ]
+        
+        XCTAssert(Score(cards: cardsA) < Score(cards: cardsB))
+    }
+    
     func testThreeOfAKind() {
         let cardsA = [
             Card(rank: .ace, suit: .diamonds),
-            Card(rank: .ace, suit: .spades),
+            Card(rank: .five, suit: .spades),
             Card(rank: .ace, suit: .hearts),
             Card(rank: .ace, suit: .clubs),
             Card(rank: .eight, suit: .clubs),
@@ -42,13 +62,71 @@ class ScoreTest: XCTestCase {
         let cardsB = [
             Card(rank: .two, suit: .spades),
             Card(rank: .two, suit: .clubs),
-            Card(rank: .two, suit: .diamonds),
+            Card(rank: .five, suit: .diamonds),
             Card(rank: .two, suit: .hearts),
             Card(rank: .six, suit: .clubs),
         ]
         
         XCTAssert(Score(cards: cardsA) < Score(cards: cardsB))
     }
-
+    
+    func testTwoPair() {
+        let cardsA = [
+            Card(rank: .ace, suit: .diamonds),
+            Card(rank: .ace, suit: .spades),
+            Card(rank: .six, suit: .hearts),
+            Card(rank: .six, suit: .clubs),
+            Card(rank: .eight, suit: .clubs),
+        ]
+        
+        let cardsB = [
+            Card(rank: .two, suit: .spades),
+            Card(rank: .two, suit: .clubs),
+            Card(rank: .five, suit: .diamonds),
+            Card(rank: .five, suit: .hearts),
+            Card(rank: .six, suit: .clubs),
+        ]
+        
+        XCTAssert(Score(cards: cardsA) > Score(cards: cardsB))
+    }
+    
+    func testOnePair() {
+        let cardsA = [
+            Card(rank: .ace, suit: .diamonds),
+            Card(rank: .ace, suit: .spades),
+            Card(rank: .seven, suit: .hearts),
+            Card(rank: .six, suit: .clubs),
+            Card(rank: .eight, suit: .clubs),
+        ]
+        
+        let cardsB = [
+            Card(rank: .two, suit: .spades),
+            Card(rank: .two, suit: .clubs),
+            Card(rank: .eight, suit: .diamonds),
+            Card(rank: .five, suit: .hearts),
+            Card(rank: .six, suit: .clubs),
+        ]
+        
+        XCTAssert(Score(cards: cardsA) < Score(cards: cardsB))
+    }
+    
+    func testHighCard() {
+        let cardsA = [
+            Card(rank: .ace, suit: .diamonds),
+            Card(rank: .seven, suit: .spades),
+            Card(rank: .king, suit: .hearts),
+            Card(rank: .six, suit: .clubs),
+            Card(rank: .eight, suit: .clubs),
+        ]
+        
+        let cardsB = [
+            Card(rank: .two, suit: .spades),
+            Card(rank: .seven, suit: .clubs),
+            Card(rank: .eight, suit: .diamonds),
+            Card(rank: .five, suit: .hearts),
+            Card(rank: .six, suit: .clubs),
+        ]
+        XCTAssert(Score(cards: cardsA) > Score(cards: cardsB))
+    }
 
 }
