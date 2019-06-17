5a67b43c1f2cce051b7798e6f08b71f6a97dd82c
hwj0623 <hwj0623@naver.com>
2019-06-17 14:50:15 +0900

    REV (3) Enum init으로 validation 개선, 시스템 메시지 enum 추가

--- CardGame/Views/Validation.swift
+++ CardGame/Views/Validation.swift
@@ -12,20 +12,7 @@ class Validation {
     
     static func validate (_ input: String) throws -> GameMenu {
         let number = try convertInteger(input)
-        if !isInRange(number) {
-            throw GameMenuError.outOfRange
-        }
-        var instruction : GameMenu = GameMenu.initialization
-        switch number {
-        case 1:
-            instruction = GameMenu.initialization
-        case 2:
-            instruction = GameMenu.shuffle
-        case 3:
-            instruction = GameMenu.drawOne
-        default:
-            break
-        }
+        let instruction : GameMenu = try GameMenu(number)
         return instruction
         
     }
@@ -34,12 +21,5 @@ class Validation {
             throw GameMenuError.notANumber
         }
         return selectedNumber
-    }
-    
-    static func isInRange(_ input: Int) -> Bool {
-        if input >= 1 && input <= 3 {
-            return true
-        }
-        return false
     }
 }
