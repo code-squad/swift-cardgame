628742ffa5c8c5182b2030da52843f3b08b38476
Sunghyun Kim <sunghyun_k@icloud.com>
2019-06-18 17:16:12 +0900

    플레이어의 메소드를 Hand로 옮김

--- CardGame/CardGame/Player.swift
+++ CardGame/CardGame/Player.swift
@@ -3,31 +3,10 @@ import Foundation
 class Player {
     
     private(set) var name: String
-    private(set) var hand = [Card]()
+    private(set) var hand = Hand()
     
     init(name: String) {
         self.name = name
-    }
-    
-    func drawCard(from deck: inout Deck) throws {
-        guard let card = deck.drawCard() else {
-            throw CardGameError.outOfCards
-        }
-        hand.append(card)
-    }
-    
-    func drawCards(from deck: inout Deck, count: Int) throws {
-        for _ in 1...count {
-            try drawCard(from: &deck)
-        }
-    }
-    
-    func resetHand() {
-        hand.removeAll()
-    }
-    
-    var score: Score {
-        return Score(cards: hand)
     }
     
 }
