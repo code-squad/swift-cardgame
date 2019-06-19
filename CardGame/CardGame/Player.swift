c08d5a9ee905ce8c76f14ae1eea6ca727f72da39
Sunghyun Kim <sunghyun_k@icloud.com>
2019-06-19 14:02:15 +0900

    Player를 비교하여 바로 점수를 비교할 수 있도록 만듦

--- CardGame/CardGame/Player.swift
+++ CardGame/CardGame/Player.swift
@@ -1,12 +1,20 @@
 import Foundation
 
-class Player {
+class Player: Comparable {
     
     private(set) var name: String
     private(set) var hand = Hand()
     
     init(name: String) {
         self.name = name
+    }
+    
+    static func < (lhs: Player, rhs: Player) -> Bool {
+        return lhs.hand.score < rhs.hand.score
+    }
+    
+    static func == (lhs: Player, rhs: Player) -> Bool {
+        return lhs.hand.score == rhs.hand.score
     }
     
 }
