ad0885987e6fc5ba6551583e46d5b8cd1904f3fd
Sunghyun Kim <sunghyun_k@icloud.com>
2019-06-19 18:26:17 +0900

    흐름 제어를 상위 모듈로 옮김

--- CardGame/CardGame/Controller.swift
+++ CardGame/CardGame/Controller.swift
@@ -1,11 +1,3 @@
-//
-//  Controller.swift
-//  CardGame
-//
-//  Created by 김성현 on 18/06/2019.
-//  Copyright © 2019 JK. All rights reserved.
-//
-
 import Foundation
 
 class Controller {
@@ -35,46 +27,40 @@ class Controller {
         }
     }
     
-    func generateGame() -> CardGame {
-        while true {
-            let rule = readGameRule()
-            let playerCount = readPlayerCount()
-            do {
-                return try CardGame(rule: rule, playerCount: playerCount)
-            } catch let error as CardGameError {
-                outputView.showError(error)
-            } catch {
-                outputView.showError(error)
-            }
+    func generateGame(rule: CardGame.Rule, playerCount: Int) -> CardGame? {
+        do {
+            return try CardGame(rule: rule, playerCount: playerCount)
+        } catch CardGameError.playerLimitExceeded {
+            outputView.showError(CardGameError.playerLimitExceeded)
+        } catch {
+            outputView.showError(error)
+        }
+        return nil
+    }
+    
+    
+    func readGameRule() -> CardGame.Rule? {
+        inputView.show(Message.gameRuleQuestion)
+        inputView.showOptions(CardGame.Rule.allCases)
+        let choiceInput = inputView.ask(Message.selectOption)
+        switch inputControl.choice(from: choiceInput, options: CardGame.Rule.allCases) {
+        case .failure(let error):
+            outputView.showError(error)
+            return nil
+        case .success(let choice):
+            return choice
         }
     }
     
-    func readGameRule() -> CardGame.Rule {
-        while true {
-            inputView.show(Message.gameRuleQuestion)
-            inputView.showOptions(CardGame.Rule.allCases)
-            let choiceInput = inputView.ask(Message.selectOption)
-            switch inputControl.choice(from: choiceInput, options: CardGame.Rule.allCases) {
-            case .failure(let error):
-                outputView.showError(error)
-                continue
-            case .success(let choice):
-                return choice
-            }
-        }
-    }
-    
-    func readPlayerCount() -> Int {
-        while true {
-            inputView.show(Message.playerCountQuestion)
-            let playerCountInput = inputView.ask(Message.playerCount)
-            switch inputControl.number(from: playerCountInput) {
-            case .failure(let error):
-                outputView.showError(error)
-                continue
-            case .success(let number):
-                return number
-            }
+    func readPlayerCount() -> Int? {
+        inputView.show(Message.playerCountQuestion)
+        let playerCountInput = inputView.ask(Message.playerCount)
+        switch inputControl.number(from: playerCountInput) {
+        case .failure(let error):
+            outputView.showError(error)
+            return nil
+        case .success(let number):
+            return number
         }
     }
     
--- CardGame/CardGame/main.swift
+++ CardGame/CardGame/main.swift
@@ -1,7 +1,37 @@
 import Foundation
 
-var controller = Controller()
+class Main {
+    
+    var controller = Controller()
+    
+    func gameRule() -> CardGame.Rule {
+        while true {
+            if let rule = controller.readGameRule() {
+                return rule
+            }
+        }
+    }
+    
+    func playerCount() -> Int {
+        while true {
+            if let count = controller.readPlayerCount() {
+                return count
+            }
+        }
+    }
+    
+    func makeGame() -> CardGame {
+        while true {
+            if let game = controller.generateGame(rule: gameRule(), playerCount: playerCount()) {
+                return game
+            }
+        }
+    }
+    
+    func startGame() {
+        controller.startGame(game: makeGame())
+    }
+    
+}
 
-var game = controller.generateGame()
-
-controller.startGame(game: game)
+Main().startGame()
