2255be8212bbef9d8601b600a8663bb14de4ceed
gukjin jang <jgj455@gmail.com>
2019-06-21 01:04:57 +0900

    Add: Player 프로토콜 추가 및 이를 채택하는 Participant, Dealer 구조체 추가, 이에 따른 코드 수정

---
+++ CardGame/CardGame/Dealer.swift
@@ -0,0 +1,22 @@
+//
+//  Dealer.swift
+//  CardGame
+//
+//  Created by jang gukjin on 21/06/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+struct Dealer: Player {
+    private let name: String = "딜러"
+    private (set) var cards: [Card]
+    
+    init(cards: [Card]) {
+        self.cards = cards
+    }
+    
+    func getName() -> String {
+        return name
+    }
+}
