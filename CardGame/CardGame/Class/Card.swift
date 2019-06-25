e1e5ecea55cc68c7973ed924d8b28dbe937a8da9
이동영 <ldcpaul3@gmail.com>
2019-06-26 03:01:41 +0900

    feat: Define nest enum Suit,Rank

--- CardGame/CardGame/Class/Card.swift
+++ CardGame/CardGame/Class/Card.swift
@@ -11,3 +11,39 @@ import Foundation
 class Card: CustomStringConvertible {
     
 }
+// - MARK: - Nested Enum (Suit, Rank)
+extension Card {
+    enum Suit: Character, CustomStringConvertible {
+        case spades = "♠️"
+        case hearts = "♥️"
+        case diamonds = "♦️"
+        case clubs = "♣️"
+        
+        var description: String {
+            return "\(rawValue)"
+        }
+        
+    }
+    
+    enum Rank: Int, CustomStringConvertible,CaseIterable {
+        case ace = 1
+        case two,three,four,five,six,seven,eight,nine,ten
+        case jack,queen,king
+        
+        var description: String {
+            switch self {
+            case .ace:
+                return "A"
+            case .two,.three,.four,.five,.six,.seven,.eight,.nine,.ten:
+                return "\(rawValue)"
+            case .jack:
+                return "J"
+            case .queen:
+                return "Q"
+            case .king:
+                return "K"
+            }
+        }
+    }
+    
+}
