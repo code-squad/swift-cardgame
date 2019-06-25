fdbfdf644a683d0ebf6dda52eec56efa2150cbb2
이동영 <ldcpaul3@gmail.com>
2019-06-26 03:00:54 +0900

    feat: Define Card class

---
+++ CardGame/CardGame/Class/Card.swift
@@ -0,0 +1,13 @@
+//
+//  Card.swift
+//  CardGame
+//
+//  Created by 이동영 on 25/06/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+class Card: CustomStringConvertible {
+    
+}
