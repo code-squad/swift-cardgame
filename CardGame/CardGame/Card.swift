a29f124c3a10916428179c1eed10f94127fa0c10
Daheen Lee <allwhite.dev@gmail.com>
2019-06-26 16:59:32 +0900

    feat: Add Rank enumeration in Card class

--- CardGame/CardGame/Card.swift
+++ CardGame/CardGame/Card.swift
@@ -22,5 +22,13 @@ class Card {
         }
     }
     
+    // 1~13 - 케이스 유한집합이므로 enum 선택
+    // class 시도해보았으나, 범위 확인하는 처리로 인해 복잡도 증가했음
+    enum Rank: Int {
+        case ace = 1
+        case two, three, four, five, six, seven, eight, nine, ten
+        case jack, queen, king
+    }
+    
 }
 
