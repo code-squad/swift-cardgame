46fcbc443bf702bd02ee8d1749aa91692ec9b785
이동영 <ldcpaul3@gmail.com>
2019-06-26 14:17:01 +0900

    feat: Define Deck protocol

---
+++ CardGame/CardGame/Protocols/Deck.swift
@@ -0,0 +1,18 @@
+//
+//  Deck.swift
+//  CardGame
+//
+//  Created by 이동영 on 26/06/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+
+protocol Deck {
+    associatedtype T
+    func count() -> Int
+    func removeOne() -> T
+    mutating func shuffle()
+    mutating func reset()
+}
