576982a5a3d29f2234fa2ae41813908e186a429b
이동영 <ldcpaul3@gmail.com>
2019-06-26 17:01:21 +0900

    feat: Define Menu

--- CardGame/CardGame/Enums/Menu.swift
+++ CardGame/CardGame/Enums/Menu.swift
@@ -7,3 +7,24 @@
 //
 
 import Foundation
+
+enum Menu: Int, CaseIterable, CustomStringConvertible {
+    var description: String {
+        switch self {
+        case .reset:
+            return "1. 카드 초기화"
+        case .shuffle:
+            return "2. 카드 섞기"
+        case .draw:
+            return "3. 카드 하나 뽑기"
+        case .exit:
+            return "4. 게임 종료"
+        }
+    }
+    
+    case reset = 1
+    case shuffle
+    case draw
+    case exit
+}
+
