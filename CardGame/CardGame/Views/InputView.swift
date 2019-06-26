d3c038027ecf93c4a7adb9e4f7f70a263d2b11f7
이동영 <ldcpaul3@gmail.com>
2019-06-26 15:53:50 +0900

    feat: Define InputView

--- CardGame/CardGame/Views/InputView.swift
+++ CardGame/CardGame/Views/InputView.swift
@@ -7,3 +7,40 @@
 //
 
 import Foundation
+
+struct InputView {
+    enum Menu: CaseIterable, CustomStringConvertible {
+        var description: String {
+            switch self {
+            case .reset:
+                return "1. 카드 초기화"
+            case .shuffle:
+                return "2. 카드 섞기"
+            case .draw:
+                return "3. 카드 하나 뽑기"
+            }
+        }
+        
+        case reset
+        case shuffle
+        case draw
+    }
+    
+    static func run() -> String {
+        showMenu()
+        return fetchInput()
+    }
+    
+    static private func showMenu(){
+        print("다음 메뉴를 선택해주세요.")
+        Menu.allCases.forEach{
+            print($0.description)
+        }
+        print(">", terminator: " ")
+    }
+    
+    static private func fetchInput() -> String {
+        return readLine() ?? ""
+    }
+
+}
