17fcdb44108ddcf75cb5d18f29ed90cf66426b88
Daheen Lee <allwhite.dev@gmail.com>
2019-06-26 17:03:25 +0900

    feat: Add CustomStringConvertible protocol conformance to Card class and inner enums(Suit, Rank)

--- CardGame/CardGame/Card.swift
+++ CardGame/CardGame/Card.swift
@@ -8,10 +8,10 @@
 
 import Foundation
 
-class Card {
+class Card: CustomStringConvertible {
     
     // 4가지 기호 - case 별로 처리하기에 적절하다고 판단하여 enum 으로 구현
-    enum Suit: Character {
+    enum Suit: Character, CustomStringConvertible {
         case spade   = "\u{2660}" //♠︎
         case club    = "\u{2663}" //♣︎
         case heart   = "\u{2665}" //♥︎
@@ -24,10 +24,25 @@ class Card {
     
     // 1~13 - 케이스 유한집합이므로 enum 선택
     // class 시도해보았으나, 범위 확인하는 처리로 인해 복잡도 증가했음
-    enum Rank: Int {
+    enum Rank: Int, CustomStringConvertible {
         case ace = 1
         case two, three, four, five, six, seven, eight, nine, ten
         case jack, queen, king
+        
+        var description: String {
+            switch self {
+            case .ace:
+                return "A"
+            case .jack:
+                return "J"
+            case .queen:
+                return "Q"
+            case .king:
+                return "K"
+            default:
+                return "\(self.rawValue)"
+            }
+        }
     }
     
     let rank: Rank
@@ -36,6 +51,10 @@ class Card {
     init(_ rank: Rank, of suit: Suit) {
         self.suit = suit
         self.rank = rank
+    }
+    
+    var description: String {
+        return "\(suit)\(rank)"
     }
 }
 
