fc10b538e832642858c54e152b2b97d69e197970
cmindy <minjidev@naver.com>
2019-06-27 14:51:46 +0900

    feat: Add InputView struct

---
+++ CardGame/CardGame/InputView.swift
@@ -0,0 +1,13 @@
+//
+//  InputView.swift
+//  CardGame
+//
+//  Created by CHOMINJI on 27/06/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+struct InputView {
+    
+}
