9f1c6ad456cde34edf66e6a4295ab7dc60b779f2
Daheen Lee <allwhite.dev@gmail.com>
2019-06-27 15:39:09 +0900

    feat: Make Card.Rank and Card.Suit adopt CaseIterable protocol to provide allCases property

--- CardGame/CardGame/Card.swift
+++ CardGame/CardGame/Card.swift
@@ -11,7 +11,7 @@ import Foundation
 class Card: CustomStringConvertible {
     
     // 4가지 기호 - case 별로 처리하기에 적절하다고 판단하여 enum 으로 구현
-    enum Suit: Character, CustomStringConvertible {
+    enum Suit: Character, CustomStringConvertible, CaseIterable {
         case spade   = "\u{2660}" //♠︎
         case club    = "\u{2663}" //♣︎
         case heart   = "\u{2665}" //♥︎
@@ -24,7 +24,7 @@ class Card: CustomStringConvertible {
     
     // 1~13 - 케이스 유한집합이므로 enum 선택
     // class 시도해보았으나, 범위 확인하는 처리로 인해 복잡도 증가했음
-    enum Rank: Int, CustomStringConvertible {
+    enum Rank: Int, CustomStringConvertible, CaseIterable {
         case ace = 1
         case two, three, four, five, six, seven, eight, nine, ten
         case jack, queen, king
