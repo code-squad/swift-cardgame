0c010e5d88956d2edbaaef3177c897beb2ffa6c7
cmindy <minjidev@naver.com>
2019-06-27 14:50:32 +0900

    feat: Add CardDeck struct

---
+++ CardGame/CardGame/CardDeck.swift
@@ -0,0 +1,13 @@
+//
+//  CardDeck.swift
+//  CardGame
+//
+//  Created by CHOMINJI on 27/06/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+struct CardDeck {
+    
+}
