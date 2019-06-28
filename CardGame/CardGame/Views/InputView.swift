ff07845e6162c519b8348bbd6ae2666e35065d44
부엉이 <ldcpaul@hanmail.net>
2019-06-28 16:27:43 +0900

    refactor: Modify Access Modifier

--- CardGame/CardGame/Views/InputView.swift
+++ CardGame/CardGame/Views/InputView.swift
@@ -11,22 +11,22 @@ import Foundation
 struct InputView {
     
     static func getMenuNumber() -> Int? {
-        showMenu()
+        print(">", terminator: " ")
         guard let menuNumber = Int(fetchInput()) else {
             return nil
         }
         return menuNumber
     }
     
-    static private func showMenu(){
+    static func showMenu(){
         let message = """
         다음 메뉴를 선택해주세요.
         1. 카드 초기화
         2. 카드 섞기
         3. 카드 하나 뽑기
-        >
+        
         """
-        print(message, terminator: " ")
+        print(message)
     }
     
     static private func fetchInput() -> String {
