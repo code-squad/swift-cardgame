2cdefc53cbb0577b79b90266776e48ed26c8da97
junyng <pom0894@naver.com>
2019-06-28 13:00:27 +0900

    CardSet 프로토콜 추가, CardDeck 구조체 추가

---
+++ CardGame/CardGame/CardDeck.swift
@@ -0,0 +1,20 @@
+//
+//  CardDeck.swift
+//  CardGame
+//
+//  Created by BLU on 27/06/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+protocol CardSet {
+    func shuffle()
+    func reset()
+    func removeOne() -> Card?
+    func count() -> Int
+}
+
+struct CardDeck {
+    private let cards: [Card]
+}
