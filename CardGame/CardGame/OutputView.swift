2318ab644794cf987916c9bef635a678eaca71c7
junyng <pom0894@naver.com>
2019-06-28 22:40:30 +0900

    OutputView 추가, 게임 결과를 출력하는 printGameResult 함수 추가

---
+++ CardGame/CardGame/OutputView.swift
@@ -0,0 +1,19 @@
+//
+//  OutputView.swift
+//  CardGame
+//
+//  Created by BLU on 28/06/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+struct OutputView {
+    
+    static func printGameResult(game: OutputViewPrintable) {
+        game.printGameResult (handler:
+            { result in
+                print(result)
+        })
+    }
+}
