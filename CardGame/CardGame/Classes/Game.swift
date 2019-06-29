2913a59bbfd4f026789532e093f63dfaba7b6e88
부엉이 <ldcpaul@hanmail.net>
2019-06-29 19:48:42 +0900

    feat: Define Game

--- CardGame/CardGame/Classes/Game.swift
+++ CardGame/CardGame/Classes/Game.swift
@@ -7,3 +7,56 @@
 //
 
 import Foundation
+
+struct Game {
+    var players: [MyPlayer]
+    let dealer: MyDealer = MyDealer()
+    let setting: Setting
+    var isContinue: Bool {
+        let needs = setting.mode.numOfCard*(setting.entry.numOfPlayer+1)
+        return self.dealer.canContinue(needs:needs)
+    }
+    
+    init(setting: Setting?) throws {
+        guard let setting = setting else {
+            throw GameException.initFailure
+        }
+        self.setting = setting
+        self.players = try Game.participate(entry: setting.entry.numOfPlayer)
+        self.players.append(dealer)
+    }
+    
+    func start() throws {
+        playerReset()
+        try drawAll()
+        OutputView.show(players: self.players)
+    }
+    
+    private static func participate(entry: Int) throws -> [MyPlayer] {
+        var players:[MyPlayer] = []
+        var index = 0
+        try loop(times: entry) {
+            players.append(MyPlayer(name: "참가자#\(++index)"))
+        }
+        return players
+    }
+    
+    private func drawAll() throws {
+        try loop(times: setting.mode.numOfCard) {
+            try drawOne()
+        }
+    }
+    
+    private func drawOne() throws {
+        try self.players.forEach {
+            try self.dealer.handsOut(to: $0)
+        }
+    }
+    
+    private func playerReset() {
+        self.players.forEach{
+            $0.clearHand()
+        }
+    }
+
+}
