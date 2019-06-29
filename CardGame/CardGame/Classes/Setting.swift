a10005b9b8ca69baac7fcd2d2cc64ea842f80599
부엉이 <ldcpaul@hanmail.net>
2019-06-29 19:49:02 +0900

    feat: Define Setting

--- CardGame/CardGame/Classes/Setting.swift
+++ CardGame/CardGame/Classes/Setting.swift
@@ -7,3 +7,42 @@
 //
 
 import Foundation
+
+struct Setting {
+    let mode: Mode
+    let entry: Entry
+    
+    enum Mode: Int {
+        case seven = 1
+        case five
+        
+        var numOfCard: Int {
+            switch self {
+            case .seven:
+                return 7
+            case .five:
+                return 5
+            }
+        }
+    }
+    
+    enum Entry: Int {
+        case one = 1
+        case two
+        case three
+        case four
+        
+        var numOfPlayer: Int {
+            return rawValue
+        }
+    }
+    
+    init?(mode: Int, entry: Int) {
+        guard let mode = Mode(rawValue: mode), let entry = Entry(rawValue: entry) else {
+            return nil
+        }
+        
+        self.mode = mode
+        self.entry = entry
+    }
+}
