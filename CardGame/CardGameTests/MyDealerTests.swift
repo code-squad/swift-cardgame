2724dd4823a169891e353f2a6bfaa957415641af
부엉이 <ldcpaul@hanmail.net>
2019-06-29 20:23:08 +0900

    test: Add CardHandTests

---
+++ CardGame/CardGameTests/MyDealerTests.swift
@@ -0,0 +1,15 @@
+//
+//  MyPlayerTests.swift
+//  CardGameTests
+//
+//  Created by 이동영 on 29/06/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import XCTest
+
+class MyDealerTests: XCTestCase {
+
+    
+
+}
