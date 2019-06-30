856f5798c8302bea19e8b6b137920bf6e34c209a
junyng <pom0894@naver.com>
2019-06-30 22:41:19 +0900

    OutputViewPrintable 프로토콜 추가, CardGame 구조체에 채택

---
+++ CardGame/CardGame/OutputViewPrintable.swift
@@ -0,0 +1,13 @@
+//
+//  OutputViewPrintable.swift
+//  CardGame
+//
+//  Created by BLU on 30/06/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+protocol OutputViewPrintable {
+    func printGameResult(handler: (String) -> ())
+}
