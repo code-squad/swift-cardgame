84e43f191322da1a36edcb192362f38d1eaa884c
junyng <pom0894@naver.com>
2019-06-30 20:16:08 +0900

    게임 메뉴 열거형 타입 추가

---
+++ CardGame/CardGame/GameMenu.swift
@@ -0,0 +1,15 @@
+//
+//  GameMenu.swift
+//  CardGame
+//
+//  Created by BLU on 30/06/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+enum GameMenu: Int {
+    case initialize = 1
+    case shuffle
+    case draw
+}
