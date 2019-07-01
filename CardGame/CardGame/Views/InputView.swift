00a9540279eb2466d26c0e8427f6243a3f69b007
Kroisttt <yago0315@gmail.com>
2019-07-01 15:12:52 +0900

    InputView 추가

---
+++ CardGame/CardGame/Views/InputView.swift
@@ -0,0 +1,17 @@
+//
+//  InputView.swift
+//  CardGame
+//
+//  Created by 이진영 on 27/06/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+struct InputView {
+    static func readInput() -> String {
+        let input = readLine() ?? ""
+        
+        return input
+    }
+}
