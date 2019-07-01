2736a60442b7e7be8636fc99d4914ea18bfd5eb9
Kroisttt <yago0315@gmail.com>
2019-07-01 17:29:36 +0900

    출력 용도의 GameResult 추가

---
+++ CardGame/CardGame/GameResult.swift
@@ -0,0 +1,15 @@
+//
+//  GameResult.swift
+//  CardGame
+//
+//  Created by 이진영 on 01/07/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+enum GameResult {
+    case reset(Int)
+    case shuffle(Int)
+    case draw(Card, Int)
+}
