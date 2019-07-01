da3857d51a85f7b977520a0a687211a1c1c2534f
Daheen Lee <allwhite.dev@gmail.com>
2019-07-01 14:16:00 +0900

    feat: Add GameInputView struct which takes menu selection and number of players, and returns them

--- CardGame/CardGame/GameInputView.swift
+++ CardGame/CardGame/GameInputView.swift
@@ -7,3 +7,27 @@
 //
 
 import Foundation
+
+struct GameInputView {
+    static let questionForMenuSelecton = """
+                                        카드 게임 종류를 선택하세요.
+                                        1. 7카드
+                                        2. 5카드
+                                        >
+                                        """
+    static let questionForNumberOfPlayers = """
+                                            참여할 사람의 인원을 입력하세요.
+                                            >
+                                            """
+    
+    static func read() -> (Menu, Int) {
+        print(questionForMenuSelecton)
+        let selection = readLine() ?? ""
+        let menu = Menu(selection: selection)
+        print(questionForNumberOfPlayers)
+        let numberOfPlayers = readLine() ?? ""
+        let convertedNumber = Int(numberOfPlayers) ?? 0
+        return (menu, convertedNumber)
+    }
+    
+}
