59e121989d116136e57820ac9e0383cfe2a6ac18
cmindy <minjidev@naver.com>
2019-07-01 21:11:27 +0900

    feat: Add GameInputView for read game input

---
+++ CardGame/CardGame/Views/GameInputView.swift
@@ -0,0 +1,26 @@
+//
+//  GameInputView.swift
+//  CardGame
+//
+//  Created by CHOMINJI on 01/07/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+struct GameInputView {
+    static func readGameMode() -> Int {
+        print(GuideSentence.typeOfGame)
+        for category in GameMenu.allCases {
+            print(category)
+        }
+        let input = InputView.readInput()
+        return Int(input) ?? 0
+    }
+    
+    static func readPlayerCount() -> Int {
+        print(GuideSentence.numOfPlayers)
+        let input = InputView.readInput()
+        return Int(input) ?? 0
+    }
+}
