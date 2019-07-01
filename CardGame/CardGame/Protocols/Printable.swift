4e89e223ba877baa3a54b2516ad493f50cf21e27
부엉이 <ldcpaul@hanmail.net>
2019-07-01 18:15:46 +0900

    feat: Define Printable protocol

---
+++ CardGame/CardGame/Protocols/Printable.swift
@@ -0,0 +1,13 @@
+//
+//  Printable.swift
+//  CardGame
+//
+//  Created by 이동영 on 01/07/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+protocol Printable {
+    var printOut: String { get }
+}
