2245e5ec95bf22005c0119d48db5b5590aec8d1e
junyng <pom0894@naver.com>
2019-07-01 15:17:16 +0900

    GameResult 타입에 CustomStringConvertible 프로토콜 제거, OutputView printGameResult 입력 파라미터 변경

--- CardGame/CardGame/OutputView.swift
+++ CardGame/CardGame/OutputView.swift
@@ -10,9 +10,14 @@ import Foundation
 
 struct OutputView {
     
-    static func printGameResult(game: OutputViewPrintable) {
-        game.printGameResult { result in
-            print(result)
+    static func printGameResult(result: GameResult) {
+        switch result {
+        case .initialize(let count):
+            print("카드 전체를 초기화했습니다.\n총 \(count)장의 카드가 있습니다.")
+        case .shuffle(let count):
+            print("전체 \(count)장의 카드를 섞었습니다.")
+        case .draw(let card, let count):
+            print("\(card)\n총 \(count)장의 카드가 남아있습니다.")
         }
     }
 }
