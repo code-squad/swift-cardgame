9936654356db606d47ed8cc14aa551a8bd8ffc18
junyng <pom0894@naver.com>
2019-07-01 17:30:01 +0900

    GameInputView 구조체에 Menu 열거형 타입 추가

---
+++ CardGame/CardGame/GameInputView.swift
@@ -0,0 +1,31 @@
+//
+//  GameInputView.swift
+//  CardGame
+//
+//  Created by BLU on 01/07/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+struct GameInputView {
+    
+    enum Menu: CustomStringConvertible {
+        case selectGame
+        case enterNumberOfPlayers
+        
+        var description: String {
+            switch self {
+            case .selectGame:
+                return "카드 게임 종류를 선택하세요."
+            case .enterNumberOfPlayers:
+                return "참여할 사람의 인원을 입력하세요."
+            }
+        }
+    }
+    
+    static private func ask(message: String) -> String {
+        print(message)
+        return readLine() ?? ""
+    }
+}
