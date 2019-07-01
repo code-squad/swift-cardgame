a51587c38b73ea7bc515ea11269210a6f1fdc895
Daheen Lee <allwhite.dev@gmail.com>
2019-07-01 17:39:31 +0900

    fix: Fix typo in argument name in canPlayMore() method of GameValidator

--- CardGame/CardGame/GameValidator.swift
+++ CardGame/CardGame/GameValidator.swift
@@ -12,8 +12,8 @@ struct GameValidator {
     static let validRangeOfPartipants = 1...4
     
     static func canPlayMore(numberOfCards: Int, numberOfParticipants: Int, remainingCardsCount: Int) -> Bool {
-        let numberOfCardsInNeed = numberOfCards * (numberOfPaticipants + 1)
-        return validRangeOfPartipants.contains(numberOfPaticipants)
+        let numberOfCardsInNeed = numberOfCards * (numberOfParticipants + 1)
+        return validRangeOfPartipants.contains(numberOfParticipants)
             && numberOfCardsInNeed <= remainingCardsCount
     }
 }
--- CardGame/CardGame/View/GameInputView.swift
+++ CardGame/CardGame/View/GameInputView.swift
@@ -20,8 +20,6 @@ struct GameInputView {
                                             >
                                             """
     
-    
-    
     static func askForMenuSelection() {
         print(questionForMenuSelecton, terminator: "")
     }
