e2c79815e67ebbfbd6944558c09244aa9f913c33
cmindy <minjidev@naver.com>
2019-07-01 21:29:17 +0900

    feat: Add CardGame for manage card game

---
+++ CardGame/CardGame/CardGame.swift
@@ -0,0 +1,38 @@
+//
+//  CardGame.swift
+//  CardGame
+//
+//  Created by CHOMINJI on 01/07/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+struct CardGame {
+    let info: GameInfoable
+    var players: [Playerable] = []
+    let dealer: Dealerable & Playerable = Dealer(deck: CardDeck())
+    
+    init(info: GameInfoable) {
+        self.info = info
+    }
+    
+    private mutating func setPlayer() {
+        let numOfPlayers = info.playerCount()
+        for order in 1...numOfPlayers {
+            players.append(Player(order: order))
+        }
+        players.append(dealer)
+    }
+    
+    private func setHand() {
+        let numOfCards = info.gameMode().numOfCards
+        
+        for _ in 1...numOfCards {
+            for player in players {
+                guard let card = dealer.deal() else { return }
+                player.take(card: card)
+            }
+        }
+    }
+}
