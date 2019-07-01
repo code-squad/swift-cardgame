7d4340d2f0c0d41fbf0b864152d4645be6077866
cmindy <minjidev@naver.com>
2019-07-01 21:19:00 +0900

    feat: Add Player and Playable protocol

---
+++ CardGame/CardGame/Player.swift
@@ -0,0 +1,45 @@
+//
+//  Player.swift
+//  CardGame
+//
+//  Created by CHOMINJI on 01/07/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+protocol Playerable {
+    func take(card: Card)
+    func playerName() -> String
+    func playerCards() -> [Card]
+}
+
+class Player {
+    private var name: String = "참가자"
+    private var order: Int
+    private var cards: [Card]
+    
+    init(order: Int = 0, cards: [Card] = []) {
+        name = name + "#\(order)"
+        self.order = order
+        self.cards = cards
+    }
+    
+    func playerOrder() -> Int {
+        return order
+    }
+}
+
+extension Player: Playerable {
+    func take(card: Card) {
+        cards.append(card)
+    }
+    
+    func playerName() -> String {
+        return name
+    }
+    
+    func playerCards() -> [Card] {
+        return cards
+    }
+}
