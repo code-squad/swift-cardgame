cbb1b55d82110a16da851047e467615fdf1e9433
부엉이 <ldcpaul@hanmail.net>
2019-07-02 17:40:38 +0900

    test: Add CardHand compare

--- CardGame/CardGameTests/CardHandtests.swift
+++ CardGame/CardGameTests/CardHandtests.swift
@@ -51,4 +51,87 @@ class CardHandtests: XCTestCase {
         //Then
         XCTAssertEqual(result, expected)
     }
+    
+    func testCompareWithFourcard() {
+        
+        // Given
+        let higher = CardHand()
+        let lower = CardHand()
+        
+        //When
+        higher.append(Card(suit: .clubs, rank: .ace))
+        higher.append(Card(suit: .diamonds, rank: .ace))
+        higher.append(Card(suit: .spades, rank: .ace))
+        higher.append(Card(suit: .hearts, rank: .ace))
+        higher.append(Card(suit: .hearts, rank: .eight))
+        
+        lower.append(Card(suit: .clubs, rank: .five))
+        lower.append(Card(suit: .diamonds, rank: .five))
+        lower.append(Card(suit: .spades, rank: .five))
+        lower.append(Card(suit: .hearts, rank: .five))
+        lower.append(Card(suit: .hearts, rank: .eight))
+        
+        //Then
+        XCTAssertTrue(higher>lower) // four-card(A) vs four-card(5)
+        
+    }
+    
+    func testCompareCaseWithOnePair() {
+        // Given
+        let higher = CardHand()
+        let lower = CardHand()
+        
+        //When
+        higher.append(Card(suit: .clubs, rank: .ace))
+        higher.append(Card(suit: .diamonds, rank: .ace))
+        
+        lower.append(Card(suit: .clubs, rank: .five))
+        lower.append(Card(suit: .diamonds, rank: .five))
+        
+        //Then
+        XCTAssertTrue(higher>lower) // one-pair(A) vs one-pair(5)
+    }
+    
+    func testCompareCaseWithEachOther() {
+        // Given
+        let higher = CardHand()
+        let lower = CardHand()
+        
+        //When
+        higher.append(Card(suit: .clubs, rank: .two))
+        higher.append(Card(suit: .diamonds, rank: .two))
+        higher.append(Card(suit: .spades, rank: .two))
+      
+        
+        lower.append(Card(suit: .clubs, rank: .five))
+        lower.append(Card(suit: .diamonds, rank: .five))
+        
+        //Then
+        XCTAssertTrue(higher>lower) // triple(2) vs one-pair(5)
+        
+    }
+    
+    func testCompareTwoPair() {
+        
+        // Given
+        let higher = CardHand()
+        let lower = CardHand()
+        
+        //When
+        higher.append(Card(suit: .clubs, rank: .ace))
+        higher.append(Card(suit: .diamonds, rank: .ace))
+        higher.append(Card(suit: .spades, rank: .eight))
+        higher.append(Card(suit: .hearts, rank: .eight))
+        
+        lower.append(Card(suit: .clubs, rank: .five))
+        lower.append(Card(suit: .diamonds, rank: .five))
+        lower.append(Card(suit: .spades, rank: .king))
+        lower.append(Card(suit: .hearts, rank: .king))
+        
+        //Then
+        XCTAssertTrue(higher>lower) // twopair - A vs twopair - K
+        
+    }
+    
+    
 }
