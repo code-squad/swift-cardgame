92b77873e7fee0c894697d9a988a337d3b01de12
Daheen Lee <allwhite.dev@gmail.com>
2019-07-02 23:45:17 +0900

    feat: Add enum NumberOfParticipants with four cases representing possible number of participants for game

--- CardGame/CardGame/NumberOfParticipants.swift
+++ CardGame/CardGame/NumberOfParticipants.swift
@@ -7,3 +7,20 @@
 //
 
 import Foundation
+
+enum NumberOfParticipants: Int {
+    case one = 1, two, three, four
+    
+    init?(number: String) {
+        let convertedNumber = Int(number) ?? 0
+        guard let numberOfParticipants = NumberOfParticipants(rawValue: convertedNumber) else {
+            return nil
+        }
+        self = numberOfParticipants
+    }
+    
+    var count: Int {
+        return self.rawValue
+    }
+}
+
