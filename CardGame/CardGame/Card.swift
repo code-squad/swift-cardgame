d6d0b79e3b0893de9d931077d0c71fa23b02e065
kimjieun <zieunv@gmail.com>
2019-07-03 19:18:27 +0900

    feat: Define Enum Suit, Rank

--- CardGame/CardGame/Card.swift
+++ CardGame/CardGame/Card.swift
@@ -9,5 +9,39 @@
 import Foundation
 
 class Card: CustomStringConvertible {
+    
     var description: String = ""
+}
+
+extension Card {
+    
+    enum Suits: Character, CustomStringConvertible {
+        case spade = "\u{2660}"
+        case heart = "\u{2665}"
+        case diamond = "\u{2666}"
+        case clovar = "\u{2633}"
+        
+        var description: String{
+            return String(rawValue)
+        }
+    }
+    
+    enum Ranks: Int, CustomStringConvertible {
+        case ace = 1,two, three, four, five, six, seven, eight, nine, ten, jack, queen, king
+        
+        var description: String {
+            switch self {
+            case .ace:
+                return "A"
+            case .jack:
+                return "J"
+            case .queen:
+                return "Q"
+            case .king:
+                return "K"
+            default:
+                return String(rawValue)
+            }
+        }
+    }
 }
