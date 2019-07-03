efda1a28e5c8dcea9c952e2d3b6f13880242fe6e
부엉이 <ldcpaul@hanmail.net>
2019-07-03 18:56:45 +0900

    feat: Define CardScore

--- CardGame/CardGame/Protocols/CardScore.swift
+++ CardGame/CardGame/Protocols/CardScore.swift
@@ -8,56 +8,60 @@
 
 import Foundation
 
+
+// 이런 형태로의 구현은 어떤지 궁금하여서 파일로 남겨보겠습니다.
 protocol CardScore: Comparable {
-    var ranking: Int { get }
+    var ranking: Double { get }
+    // 수와 수 사이에 무한한 갯수의 수가 존재하여 삽입이 쉬울거라고 판단하여서 Double로 구현
     var top: Card { get }
-    
 }
 extension CardScore {
     static func < <T: CardScore> (lhs: Self, rhs: T) -> Bool {
-        let higherRanking = lhs.ranking > rhs.ranking
-        let higherTop = lhs.ranking == rhs.ranking && lhs.self.top > rhs.self.top
-        return higherRanking || higherTop
-    }
-}
-
-
-struct HighCard: CardScore {
-    let ranking: Int = 1
-    
-    typealias E = Card
-    var top: Card
-    
-    static func < <T: CardScore> (lhs: HighCard, rhs: T) -> Bool {
         let higherRanking = lhs.ranking > rhs.ranking
         let higherTop = lhs.ranking == rhs.ranking && lhs.top > rhs.top
         return higherRanking || higherTop
     }
 }
 
-struct OnePair: CardScore {
-    let  ranking: Int = 2
-    
+struct HighCard: CardScore {
     typealias E = Card
+    
+    let ranking: Double = 1
     var top: Card
+}
+
+struct OnePair: CardScore {
+    typealias E = Card
     
-    
+    let ranking: Double = 2
+    var top: Card
 }
 
 struct TwoPair: CardScore {
     typealias E = Card
+    
+    let  ranking: Double = 3
     var top: Card
-    let  ranking: Int = 3
-    static func < (lhs: TwoPair, rhs: TwoPair) -> Bool {
-        return true
-    }
+    
 }
 
 struct Trips: CardScore {
     typealias E = Card
+    
+    let ranking: Double = 5
     var top: Card
-    let  ranking: Int = 5
-    static func < (lhs: Trips, rhs: Trips) -> Bool {
-        return true
-    }
+}
+
+struct Stright: CardScore {
+    typealias E = Card
+    
+    let ranking: Double = 6
+    var top: Card
+}
+
+struct FourCard: CardScore {
+    typealias E = Card
+    
+    let ranking: Double = 7
+    var top: Card
 }
