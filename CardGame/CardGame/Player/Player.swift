8ed65bf19700996d89225fd6240b09f00de4cc02
Daheen Lee <allwhite.dev@gmail.com>
2019-07-04 16:28:44 +0900

    feat: Add determineWinningPokerHand() method to protocol Player to get highest Decision pair of player instance

--- CardGame/CardGame/Player/Player.swift
+++ CardGame/CardGame/Player/Player.swift
@@ -12,11 +12,16 @@ protocol Player {
     var name: String { get }
     var cards: [Card] { get set }
     mutating func take(newCards: [Card])
+    func determineWinningPokerHand() -> Decision
 }
 
 extension Player {
     mutating func take(newCards: [Card]) {
         self.cards.append(contentsOf: newCards)
+    }
+    
+    func determineWinningPokerHand() -> Decision {
+        return HandsDeterminator.determine(using: cards)
     }
 }
 
