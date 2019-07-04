de171108d4eb6abcbbd90e81e34712fac7b19f4f
Daheen Lee <allwhite.dev@gmail.com>
2019-07-04 16:30:17 +0900

    feat: Add WinnerDeterminator for getting winner among players

--- CardGame/CardGame/WinnerDeterminator.swift
+++ CardGame/CardGame/WinnerDeterminator.swift
@@ -7,3 +7,25 @@
 //
 
 import Foundation
+
+struct WinnerDeterminator {
+    static func determineWinner(among players: [Player]) -> Player {
+        var playerDecisionPairs = players.map { ($0, $0.determineWinningPokerHand()) }
+        var (winner, winningDecision) = playerDecisionPairs[0]
+        for i in 1..<playerDecisionPairs.count {
+            let (comparingPlayer, comparingDecision) = playerDecisionPairs[i]
+            if check(comparingDecision: comparingDecision, higherThan: winningDecision) {
+                winner = comparingPlayer
+                winningDecision = comparingDecision
+            }
+        }
+        return winner
+    }
+    
+    static func check(comparingDecision: Decision, higherThan currentWinningDecision: Decision) -> Bool {
+        if currentWinningDecision.hand == comparingDecision.hand {
+            return currentWinningDecision.highestRank < comparingDecision.highestRank
+        }
+        return currentWinningDecision.hand < comparingDecision.hand
+    }
+}
