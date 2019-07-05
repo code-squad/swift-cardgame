41e7af3c6d0f56beab7dab4d254a932a67226494
junyng <pom0894@naver.com>
2019-07-05 18:04:35 +0900

    PokerGame 구조체에 OutputViewPrintable 프로토콜 채택

--- CardGame/CardGame/PokerGame.swift
+++ CardGame/CardGame/PokerGame.swift
@@ -42,3 +42,11 @@ struct PokerGame {
         }
     }
 }
+
+extension PokerGame: OutputViewPrintable {
+    func printPlayerInfo(handler: (String, String) -> ()) {
+        players.forEach { player in
+            handler(player.name, String(describing: player.hand))
+        }
+    }
+}
