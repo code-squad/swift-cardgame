f2465968f489c214b5d604dc805b1e25084f1259
부엉이 <ldcpaul@hanmail.net>
2019-07-05 10:18:37 +0900

    feat: Define PlayerEntry conforming Entry

--- CardGame/CardGame/Classes/PlayerEntry.swift
+++ CardGame/CardGame/Classes/PlayerEntry.swift
@@ -11,22 +11,38 @@ import Foundation
 class PlayerEntry: Entry {
     typealias P = MyPlayer
     
+    var count: Int { return players.count }
     private var players: [MyPlayer]
+    private var dealer: MyDealer
     
-    init(players: [P]) {
-        self.players = players
+    init(num: Int, dealer: MyDealer) {
+        self.players = Array(1...num).map { MyPlayer(name: "참가자#\($0)") }
+        self.players.append(dealer)
+        self.dealer = dealer
     }
     
-    private func playerReset() {
+    func win() -> P {
+        let winner = self.players.reduce(players[0]) { $0 < $1 ? $1 : $0 }
+        return winner
+    }
+    
+    func reset() {
         self.players.forEach{ $0.clearHand() }
     }
     
-    func count() -> Int {
-        return self.players.count
+    func draw(numOfCard: Int, completion: () -> (Void)) throws {
+        try Array(1...numOfCard).forEach { _ in try drawOne(); completion() }
     }
     
-    private func win() -> P {
-        let winner = players.reduce(players[0]) { $0 < $1 ? $1 : $0 }
-        return winner
+    private func drawOne() throws {
+        try self.players.forEach { $0.receive(try dealer.draw()) }
     }
+    
+}
+extension PlayerEntry: PrintableTwo {
+    
+    func print(logic: @escaping (String, String) -> Void, input: String = "" ) {
+        self.players.forEach{ $0.print(logic: logic ) }
+    }
+    
 }
