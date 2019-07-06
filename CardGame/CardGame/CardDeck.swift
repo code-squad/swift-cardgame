679ba4d06f7fa4eae1666de9ed443d69ca5b5d37
kimjieun <zieunv@gmail.com>
2019-07-07 01:33:03 +0900

    feat: Add CardDeck struct

---
+++ CardGame/CardGame/CardDeck.swift
@@ -0,0 +1,39 @@
+//
+//  CardDeck.swift
+//  CardGame
+//
+//  Created by JieunKim on 05/07/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+protocol Deck {
+    mutating func count() -> Int
+    mutating func reset()
+    mutating func shuffle()
+    mutating func removeOne() -> Card?
+}
+
+struct CardDeck: Deck {
+    
+    private var cards = [Card]()
+    
+    mutating func count() -> Int {
+        return cards.count
+    }
+    
+    mutating func reset() {
+        cards.removeAll()
+        self.cards = DeckMaker.makeCard()
+    }
+    
+    mutating func shuffle() {
+        cards = cards.shuffled()
+    }
+    
+    mutating func removeOne() -> Card? {
+        guard !cards.isEmpty else { return nil }
+        return cards.popLast()
+    }
+}
