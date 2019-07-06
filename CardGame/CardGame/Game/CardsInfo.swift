50c96e0f610f9910dc932e7493b0cce20b0597f2
cmindy <minjidev@naver.com>
2019-07-06 21:09:27 +0900

    feat: Add property and functions in CardsInfo for represent hand

--- CardGame/CardGame/Game/CardsInfo.swift
+++ CardGame/CardGame/Game/CardsInfo.swift
@@ -9,5 +9,29 @@
 import Foundation
 
 struct CardsInfo {
-
+    private var cards: [Card]
+    private var collectedCards: [Card: Int]
+    private var hands: [Card: CardSetRanking] = [:]
+    
+    init(cards: [Card]) {
+        self.cards = cards
+        self.collectedCards = cards.reduce(into: [:]) { counts, card in
+            counts[card, default: 0] += 1
+        }
+        makeHands()
+    }
+    
+    mutating func makeHands() {
+        self.hands = HandDecider.decidePair(of: collectedCards)
+        let straight =  HandDecider.decideStraight(of: collectedCards)
+        if straight.isStraight {
+            hands[straight.maxRank] = .straight
+        }
+    }
+    
+    mutating func maxHand() -> (key: Card, value: CardSetRanking) {
+        var sortedHands = hands.sorted(by: { $0.value < $1.value })
+        let maxHand = sortedHands[sortedHands.count - 1]
+        return maxHand
+    }
 }
