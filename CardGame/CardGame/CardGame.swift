7c05bee85693ee6cc0a6ce32df9766131b1928ad
kimjieun <zieunv@gmail.com>
2019-07-07 01:44:26 +0900

    feat: Add CardGame struct

---
+++ CardGame/CardGame/CardGame.swift
@@ -0,0 +1,44 @@
+//
+//  CardGame.swift
+//  CardGame
+//
+//  Created by JieunKim on 05/07/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+struct CardGame {
+    enum Error: Swift.Error {
+        case isCardDeckEmpty
+        
+        var localizedDescription: String {
+            switch self {
+            case .isCardDeckEmpty:
+                return "카드덱이 비어있습니다."
+            }
+        }
+    }
+    
+   private var deck: Deck
+    init(deck: Deck) {
+        self.deck = deck
+    }
+    
+    mutating func play(menu: GameMenu) throws -> CardGameResult {
+        switch menu {
+        case .reset:
+            deck.reset()
+            return CardGameResult.reset(deck.count())
+        case .shuffle:
+            deck.shuffle()
+            return CardGameResult.shuffle(deck.count())
+        case .draw:
+            guard let card = deck.removeOne() else {
+                throw Error.isCardDeckEmpty
+            }
+            return CardGameResult.draw(card, deck.count())
+        }
+    }
+    
+}
