75642dd15cca6005f6898f47c91e8f566dbde4c1
kimjieun <zieunv@gmail.com>
2019-07-07 01:41:04 +0900

    feat: Add print menu and read number to InputView for read users choice & Add ErrorMessage

---
+++ CardGame/CardGame/CardGameError.swift
@@ -0,0 +1,13 @@
+//
+//  CardGameError.swift
+//  CardGame
+//
+//  Created by JieunKim on 05/07/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+struct ErrorMessage {
+    static let invalidMenu = "올바르지 않은 메뉴가 나왔습니다."
+        }
--- CardGame/CardGame/InputView.swift
+++ CardGame/CardGame/InputView.swift
@@ -17,9 +17,28 @@ struct InputView {
     >
     """
     
+    enum Error: Swift.Error {
+        case invalidMenuNumber
+        
+        var localizedDescription: String {
+            switch self {
+            case .invalidMenuNumber:
+                return "유효하지 않은 메뉴 번호입니다."
+            }
+        }
+    }
+    
     static func read(message: String) -> String {
         print(message)
         return readLine() ?? ""
     }
-   
+    
+    static func readMenu() throws -> GameMenu {
+        let menuNumber = read(message: guideSentence)
+        if let gameMenu = GameMenu(rawValue: menuNumber ) {
+            return gameMenu
+            
+        }
+        throw Error.invalidMenuNumber
+    }
 }
