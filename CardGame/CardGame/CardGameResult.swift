db80a12a976aefc27d603cf170855c3d87e1f198
kimjieun <zieunv@gmail.com>
2019-07-07 01:46:23 +0900

    feat: Add enum CardGameResult

---
+++ CardGame/CardGame/CardGameResult.swift
@@ -0,0 +1,15 @@
+//
+//  CardGameResult.swift
+//  CardGame
+//
+//  Created by JieunKim on 05/07/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+enum CardGameResult {
+    case reset(Int)
+    case shuffle(Int)
+    case draw(Card?, Int)
+}
