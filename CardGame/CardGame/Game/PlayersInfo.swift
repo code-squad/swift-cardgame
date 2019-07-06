3137b352fd8bfd5167f35ebdf83746df11ea6d73
cmindy <minjidev@naver.com>
2019-07-06 21:15:27 +0900

    perf: Add PlayersInfo for represent Player list

---
+++ CardGame/CardGame/Game/PlayersInfo.swift
@@ -0,0 +1,45 @@
+//
+//  PlayersInfo.swift
+//  CardGame
+//
+//  Created by CHOMINJI on 06/07/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+struct PlayersInfo {
+    var players: [Playerable] = []
+    private let dealer: Dealerable & Playerable
+    
+    init(playersNum: Int, dealer: Dealerable & Playerable) {
+        self.dealer = dealer
+        setPlayers(num: playersNum)
+    }
+    
+    private mutating func setPlayers(num: Int) {
+        for order in 1...num {
+            self.players.append(Player(order: order))
+        }
+        self.players.append(dealer)
+    }
+    
+    func setCards(num: Int) {
+        for _ in 1...num {
+            for player in players {
+                guard let card = dealer.deal() else { return }
+                player.take(card: card)
+            }
+        }
+    }
+    
+    func showSettingResult(with result: (Bool, String, String) -> Void) {
+        for player in players {
+            let participant = player as? Player
+            let isPlayer = participant != nil
+            let order = isPlayer ? participant!.showOrder() : ""
+            let cards = player.showCards()
+            result(isPlayer, order, cards)
+        }
+    }
+}
