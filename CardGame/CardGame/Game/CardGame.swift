e121e6b39e28421ce4e01f566e2a087d63218d1b
cmindy <minjidev@naver.com>
2019-07-06 21:18:31 +0900

    perf: Modify [Playerable] to PlayersInfo in CardGame for abstract players

--- CardGame/CardGame/Game/CardGame.swift
+++ CardGame/CardGame/Game/CardGame.swift
@@ -9,18 +9,19 @@
 import Foundation
 
 protocol OutputViewPrintable {
-    func printSettingResult(_ result: (Bool, String, String) -> Void)
+    func printSettingResult()
+    func printWinner()
 }
 
 struct CardGame: OutputViewPrintable {
     private let info: GameInfoable
-    private var players: [Playerable]
+    private var players: PlayersInfo
     private let dealer: Dealerable & Playerable
     
-    init(info: GameInfoable, dealer: Dealerable & Playerable, players: [Playerable] = []) {
+    init(info: GameInfoable, dealer: Dealerable & Playerable) {
         self.info = info
-        self.players = players
         self.dealer = dealer
+        self.players = PlayersInfo(playersNum: info.participants().count, dealer: dealer)
     }
     
     private func canContinueGame() -> Bool {
@@ -31,39 +32,10 @@ struct CardGame: OutputViewPrintable {
     
     mutating func run() {
         guard canContinueGame() else { return }
-        setPlayer()
-        setCards()
+        players.setCards(num: info.gameMode().numOfCards)
     }
     
-    func printSettingResult(_ result: (Bool, String, String) -> Void) {
-        for player in players {
-            let participant = player as? Player
-            let isPlayer = participant != nil
-            let order = isPlayer ? participant!.showOrder() : ""
-            let cards = player.showCards()
-            result(isPlayer, order, cards)
-        }
-    }
-}
-
-// MARK: Setting Games
-
-extension CardGame {
-    private mutating func setPlayer() {
-        let numOfPlayers = info.participants().count
-        for order in 1...numOfPlayers {
-            players.append(Player(order: order))
-        }
-        players.append(dealer)
     }
     
-    private func setCards() {
-        let numOfCards = info.gameMode().numOfCards
-        for _ in 1...numOfCards {
-            for player in players {
-                guard let card = dealer.deal() else { return }
-                player.take(card: card)
-            }
-        }
     }
 }
