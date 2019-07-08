53160fab91426b5af6f83b424d38179f7166557e
Daheen Lee <allwhite.dev@gmail.com>
2019-07-08 22:00:12 +0900

    feat: Define Decision class standing for winning poker pair of card list of each player

--- CardGame/CardGame/Decision.swift
+++ CardGame/CardGame/Decision.swift
@@ -7,3 +7,15 @@
 //
 
 import Foundation
+
+class Decision {
+    static let lowest = Decision(highestRank: Card.Rank.ace, hand: Hands.highCard)
+    let highestRank: Card.Rank
+    let hand: Hands
+    
+    init(highestRank: Card.Rank, hand: Hands) {
+        self.highestRank = highestRank
+        self.hand = hand
+    }
+}
+
