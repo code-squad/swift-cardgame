0bc7fa1600a9daf5b491cb9913b948248b55f9c8
kimjieun <zieunv@gmail.com>
2019-07-09 21:35:13 +0900

    feat: Add player protocol

---
+++ CardGame/CardGame/Player/Player.swift
@@ -0,0 +1,13 @@
+//
+//  Player.swift
+//  CardGame
+//
+//  Created by JieunKim on 09/07/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+protocol Player {
+    static var playerType: String { get }
+}
