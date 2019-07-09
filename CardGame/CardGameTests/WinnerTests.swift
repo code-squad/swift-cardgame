24f9e003f8bdbbd9ad0eca756e22d535892e4a4d
Daheen Lee <allwhite.dev@gmail.com>
2019-07-09 15:04:40 +0900

    refactor: Modify way of comparing players using comparison operator

--- CardGame/CardGameTests/WinnerTests.swift
+++ CardGame/CardGameTests/WinnerTests.swift
@@ -13,33 +13,29 @@ class WinnerTests: XCTestCase {
     let twoPairCards = Cards([Card(.eight, of: .club), Card(.eight, of: .spade), Card(.five, of: .heart), Card(.three, of: .diamond), Card(.three, of: .club)])
     let highCardsWithSmallerRanks = Cards([Card(.two, of: .club), Card(.six, of: .spade), Card(.ten, of: .heart), Card(.five, of: .diamond), Card(.four, of: .club)])
     let highCardsWithHigherRanks = Cards([Card(.ace, of: .club), Card(.nine, of: .spade), Card(.seven, of: .heart), Card(.queen, of: .diamond), Card(.king, of: .club)])
-    var winingDealer: Dealer!
-    var losingParticipant: Participant!
+    var winner: Player!
+    var loser: Player!
     
 
     override func setUp() {
-        winingDealer = Dealer()
-        losingParticipant = Participant()
+        winner = Player()
+        loser = Player()
     }
 
     override func tearDown() {
-        winingDealer = nil
-        losingParticipant = nil
+        winner = nil
+        loser = nil
     }
 
     func testPlayerWithHigherHandWins() {
-        winingDealer.take(newCards: twoPairCards)
-        losingParticipant.take(newCards: onePairCards)
-        let players = Players(dealer: winingDealer, participants: [losingParticipant])
-        let nameOfWinner = players.determineWinner()
-        XCTAssertEqual(nameOfWinner, winingDealer.role)
+        winner.take(newCards: twoPairCards)
+        loser.take(newCards: onePairCards)
+        XCTAssertTrue(winner > loser)
     }
 
     func testPlayerWithHigherRankWinds() {
-        winingDealer.take(newCards: highCardsWithHigherRanks)
-        losingParticipant.take(newCards: highCardsWithSmallerRanks)
-        let players = Players(dealer: winingDealer, participants: [losingParticipant])
-        let nameOfWinner = players.determineWinner()
-        XCTAssertEqual(nameOfWinner, winingDealer.role)
+        winner.take(newCards: highCardsWithHigherRanks)
+        loser.take(newCards: highCardsWithSmallerRanks)
+        XCTAssertTrue(winner > loser)
     }
 }
