13e5f001b304d113a91c9d32138afa0091516c33
kimjieun <zieunv@gmail.com>
2019-07-10 20:23:09 +0900

    feat: Modify player protocol

--- CardGame/CardGame/Player/Player.swift
+++ CardGame/CardGame/Player/Player.swift
@@ -2,12 +2,14 @@
 //  Player.swift
 //  CardGame
 //
-//  Created by JieunKim on 09/07/2019.
+//  Created by JieunKim on 10/07/2019.
 //  Copyright © 2019 JK. All rights reserved.
 //
 
 import Foundation
 
 protocol Player {
-    static var playerType: String { get }
+    var name: String { get }
+    var hand: Hand { get }
+    mutating func receive(newCards: Card)
 }
