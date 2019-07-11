cea2757da5da2eb6e37c847c4ea42933563da11f
kimjieun <zieunv@gmail.com>
2019-07-11 19:24:46 +0900

    feat: Add readSelectMode & numberOfParticipant

--- CardGame/CardGame/Views/GameInputView.swift
+++ CardGame/CardGame/Views/GameInputView.swift
@@ -9,23 +9,46 @@
 import Foundation
 
 struct GameInputView {
-    static let questionForMenu = """
+    private static let questionForMenu = """
     카드 게임 종류를 선택하세요.
     1. 7카드
     2. 5카드
     >
     """
     
-    static let questionForParticipant = """
+    private static let questionForParticipant = """
     참여할 사람의 인원을 입력하세요.
     >
     """
     
-    static func askForMenu() {
+    private static func askForMenu() {
         print(questionForMenu, terminator: "")
     }
     
-    static func askForParticipant() {
+    private static func askForParticipant() {
         print(questionForParticipant, terminator: "")
+    }
+    
+    static func readModeSelection() -> CardGameMode? {
+        askForMenu()
+        let selection = readLine() ?? ""
+        return CardGameMode(selectMode: selection)
+    }
+    
+    static func readNumberOfParticipants() -> CardGameParticipant? {
+        askForParticipant()
+        let numberOfParticipants = readLine() ?? ""
+        return CardGameParticipant(selectNumber: numberOfParticipants)
+    }
+    
+    enum Error: Swift.Error {
+        case invalidMenuNumber
+        
+        var localizedDescription: String {
+            switch self {
+            case .invalidMenuNumber:
+                return "유효하지 않은 메뉴 번호입니다."
+            }
+        }
     }
 }
