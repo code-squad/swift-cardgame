d2815efa2d86a8c3e33a3d1c287e715a5a06e1fb
박준희 <junibibi04@gmail.com>
2019-07-14 22:47:39 +0900

    OutputView 구조체생성후 show메소드 작성함

---
+++ CardGame/CardGame/OutputView.swift
@@ -0,0 +1,17 @@
+//
+//  OutputView.swift
+//  CardGame
+//
+//  Created by JH on 14/07/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+struct  OutputView {
+    
+    func show(_ string: String) {
+        print("\(string)")
+    }
+    
+}
