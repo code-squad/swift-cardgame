90700d00402a3ae3807e7a7c179bc31b289a6f5a
박준희 <junibibi04@gmail.com>
2019-07-14 22:14:15 +0900

    CardDeck에 count/shuffle/removeOne/reset 메소드 작성함.

--- CardGame/CardGame/CardDeck.swift
+++ CardGame/CardGame/CardDeck.swift
@@ -8,11 +8,7 @@
 
 import Foundation
 
-//count() 갖고 있는 카드 개수를 반환한다.
-//shuffle() 기능은 전체 카드를 랜덤하게 섞는다.
-//removeOne() 기능은 카드 인스턴스 중에 하나를 반환하고 목록에서 삭제한다.
-//reset() 처음처럼 모든 카드를 다시 채워넣는다.
-//모든 종류의 카드 객체 인스턴스를 포함하는 카드덱 구조체를 구현한다.
+///모든 종류의 카드 객체 인스턴스를 포함하는 카드덱 구조체를 구현한다.
 struct CardDeck {
     
     var cards: [Card]
@@ -50,4 +46,26 @@ struct CardDeck {
         cards = makeCardDeck()
     }
     
+    ///count() 갖고 있는 카드 개수를 반환한다.
+    func count() -> Int {
+        return cards.count
+    }
+    ///shuffle() 기능은 전체 카드를 랜덤하게 섞는다.
+    mutating func shuffle() {
+        cards.shuffle()
+    }
+    
+    ///removeOne() 기능은 카드 인스턴스 중에 하나를 반환하고 목록에서 삭제한다.
+    mutating func removeOne() -> Card? {
+        guard !cards.isEmpty else {
+            return nil
+        }
+        return cards.removeFirst()
+    }
+    
+    ///reset() 처음처럼 모든 카드를 다시 채워넣는다.
+    mutating func reset() {
+        self = CardDeck.init()
+        
+    }
 }
