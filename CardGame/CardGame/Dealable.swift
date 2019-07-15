a2a28089f397266cd9cf98e6d27392ea97d2a237
Kroisttt <yago0315@gmail.com>
2019-07-15 15:42:01 +0900

    Dealable 프로토콜 추가

---
+++ CardGame/CardGame/Dealable.swift
@@ -0,0 +1,13 @@
+//
+//  Dealable.swift
+//  CardGame
+//
+//  Created by 이진영 on 15/07/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+protocol Dealable: Playable {
+    func give() -> Card?
+}
