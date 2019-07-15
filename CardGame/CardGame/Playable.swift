66251150e59819a0d608459eb34b82206ec184fd
Kroisttt <yago0315@gmail.com>
2019-07-15 13:52:19 +0900

    Playable 프로토콜 추가

---
+++ CardGame/CardGame/Playable.swift
@@ -0,0 +1,13 @@
+//
+//  Playable.swift
+//  CardGame
+//
+//  Created by 이진영 on 15/07/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+protocol Playable {
+    func receive(card: Card)
+}
