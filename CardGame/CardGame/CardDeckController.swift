7ef79205f8a8c70e284137945381f8240a776437
박준희 <junibibi04@gmail.com>
2019-07-15 22:47:05 +0900

    play메소드 생성. Partial application of 'mutating' method is not allowed 오류를 해결하기 위해 구조체->클래스로 변경함

--- CardGame/CardGame/CardDeckController.swift
+++ CardGame/CardGame/CardDeckController.swift
@@ -8,18 +8,24 @@
 
 import Foundation
 
-struct CardDeckController {
-    let inputView = InputView()
+class CardDeckController {
+    let input = InputView()
     let output = OutputView()
     var deck = CardDeck()
     
-    mutating func resetDeck() {
+    func play() {
+        let options = ["카드 초기화": resetDeck, "카드 섞기": shuffleDeck, "카드 하나 뽑기": removeOne]
+        let option = input.askForChoice(options: options)
+        option()
+    }
+    
+    func resetDeck() {
         deck.reset()
         output.show("카드 전체를 초기화했습니다.")
         showDeckCount()
     }
     
-    mutating func shuffleDeck() {
+    func shuffleDeck() {
         deck.shuffle()
         output.show("전체 \(deck.count())장의 카드를 섞었습니다.")
     }
@@ -28,7 +34,7 @@ struct CardDeckController {
         output.show("총 \(deck.count())장의 카드가 있습니다.")
     }
     
-    mutating func removeOne() {
+    func removeOne() {
         if let card = deck.removeOne() {
             output.show("\(card)")
             showDeckCount()
