e92a2a6602c868027073572a094ce2917ba6463a
junyng <pom0894@naver.com>
2019-07-16 23:52:53 +0900

    유저 수를 나타내는 열거형 타입 추가

---
+++ CardGame/CardGame/NumberOfPlayers.swift
@@ -0,0 +1,13 @@
+//
+//  NumberOfPlayers.swift
+//  CardGame
+//
+//  Created by BLU on 16/07/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+enum NumberOfPlayers: Int {
+    case one = 1, two, three, four
+}
