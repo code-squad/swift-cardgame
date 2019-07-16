cf065424c8dde4528f8288ad883d40fa7746d9d6
junyng <pom0894@naver.com>
2019-07-16 16:07:41 +0900

    Player 배열을 담당하는 PlayerInfo 구조체 추가

---
+++ CardGame/CardGame/PlayerInfo.swift
@@ -0,0 +1,21 @@
+//
+//  PlayerInfo.swift
+//  CardGame
+//
+//  Created by BLU on 16/07/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+struct PlayerInfo {
+    private var pokerPlayers = [Player]()
+    
+    mutating func addPlayer(_ player: Player) {
+        pokerPlayers.append(player)
+    }
+    
+    mutating func players() -> [Player] {
+        return pokerPlayers
+    }
+}
