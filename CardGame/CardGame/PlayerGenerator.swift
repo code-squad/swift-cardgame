2ef7e46c6c5cf1e047fae088bb5e2ae9ca8b7427
Kroisttt <yago0315@gmail.com>
2019-07-16 16:41:48 +0900

    PlayerGenerator 추가

---
+++ CardGame/CardGame/PlayerGenerator.swift
@@ -0,0 +1,15 @@
+//
+//  PlayerGenerator.swift
+//  CardGame
+//
+//  Created by 이진영 on 15/07/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+struct PlayerGenerator {
+    static func generatePlayer(numberOfPlayer: NumberOfPlayer) -> [Player] {
+        return (1...numberOfPlayer.rawValue).map { Player(number: $0) }
+    }
+}
