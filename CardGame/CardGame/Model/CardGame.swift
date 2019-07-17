f36f99a18f9ef7d82393795346a4208f6c9647c2
박준희 <junibibi04@gmail.com>
2019-07-17 21:27:07 +0900

    사용하지않는 인스턴스 삭제함. makeCardGame메소드 수정함.

--- CardGame/CardGame/Model/CardGame.swift
+++ CardGame/CardGame/Model/CardGame.swift
@@ -10,15 +10,18 @@ import Foundation
 
 struct CardGame {
     var cardDeck = CardDeck()
-    let cardDeckController = CardDeckController()
     
     //매개변수 숫자 2개 -> 카드종류(보여줄카드갯수), 반복출력(참여인원+1(딜러까지..))
-    mutating func makeCardGame(type: Int, playerCount: Int) -> [[Card]] {
-        var cardGame = [[Card]](repeating: [], count: playerCount)
+    mutating func makeCardGame(type: Int, playerCount: Int) -> [[Card]]? {
+        let palyer = playerCount + 1
+        var cardGame = [[Card]](repeating: [], count: palyer)
         
-        for playerNumber in 0 ..< playerCount {
-            for index in 0 ..< type {
-                cardGame[playerNumber].append(cardDeck.cards[index])
+        for playerNumber in 0 ..< palyer {
+            for _ in 0 ..< type {
+                guard let card = cardDeck.removeOne() else {
+                    return nil
+                }
+                cardGame[playerNumber].append(card)
                 cardDeck.shuffle()
             }
         }
