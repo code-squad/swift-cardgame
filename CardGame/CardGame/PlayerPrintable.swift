23337cd20e18eaa7d6ca2b71cc1028219df1fcbe
Kroisttt <yago0315@gmail.com>
2019-07-18 15:51:21 +0900

    PlayerPrintable 프로토콜 추가

---
+++ CardGame/CardGame/PlayerPrintable.swift
@@ -0,0 +1,13 @@
+//
+//  PlayerPrintable.swift
+//  CardGame
+//
+//  Created by 이진영 on 18/07/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+protocol PlayerPrintable {
+    func getPlayerInformation(handler: @escaping (String, [Card]) -> ())
+}
