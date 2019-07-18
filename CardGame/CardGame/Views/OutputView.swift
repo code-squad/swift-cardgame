587bfb10ba1a8dd94583f200f93e3d03acf37df9
Kroisttt <yago0315@gmail.com>
2019-07-18 15:53:59 +0900

    OutputView 수정

--- CardGame/CardGame/Views/OutputView.swift
+++ CardGame/CardGame/Views/OutputView.swift
@@ -9,21 +9,7 @@
 import Foundation
 
 struct OutputView {
-    static func printResult(of result: GameResult) {
-        switch result {
-        case .reset(let count):
-            print("카드 전체를 초기화했습니다.")
-            print("총 \(count)장의 카드가 있습니다.")
-        case .shuffle(let count):
-            print("전체 \(count)장의 카드를 섞었습니다.")
-        case .draw(let card, let count):
-            if let card = card {
-                print(card)
-            }
-            
-            print("총 \(count)장의 카드가 남아있습니다.")
-        case .exit:
-            print("프로그램 종료")
-        }
+    static func printResult(of game: CardGame) {
+        game.printPlayerInformation { print("\($0) \($1)")}
     }
 }
