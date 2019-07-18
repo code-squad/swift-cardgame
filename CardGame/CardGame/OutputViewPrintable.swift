b8ee03120f1e0bbf9ca510015de84249067e6182
Kroisttt <yago0315@gmail.com>
2019-07-18 13:15:51 +0900

    OutputViewPrintable 프로토콜 추가

---
+++ CardGame/CardGame/OutputViewPrintable.swift
@@ -0,0 +1,13 @@
+//
+//  OutputViewPrintable.swift
+//  CardGame
+//
+//  Created by 이진영 on 18/07/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+protocol OutputViewPrintable {
+    func printGameResult(handler: (String, [Card]) -> ())
+}
