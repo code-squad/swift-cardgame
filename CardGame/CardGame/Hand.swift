65e9b9ff13f14e1cf603568bb11d25aa30721fcf
Kroisttt <yago0315@gmail.com>
2019-07-22 16:28:54 +0900

    Hand 추가

---
+++ CardGame/CardGame/Hand.swift
@@ -0,0 +1,13 @@
+//
+//  Hand.swift
+//  CardGame
+//
+//  Created by 이진영 on 22/07/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+enum Hand: Int {
+    case highCard = 0, pair, twoPairs, trips, straight, quads
+}
