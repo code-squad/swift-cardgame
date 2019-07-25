9d6e8607e2c866f519ffcf33a34a0348a68b43d5
박준희 <junibibi04@gmail.com>
2019-07-25 16:39:39 +0900

    CardGame구조체 수정 -카드게임종류를 enum타입으로 설정하고 셋팅하는 메소드 생성함.

--- CardGame/CardGame/Model/CardGame.swift
+++ CardGame/CardGame/Model/CardGame.swift
@@ -9,25 +9,17 @@
 import Foundation
 
 struct CardGame {
-    var cardDeck = CardDeck()
     
-    //매개변수 숫자 2개 -> 카드종류(보여줄카드갯수), 반복출력(참여인원+1(딜러까지..))
-    mutating func makeCardGame(type: Int, playerCount: Int) -> [[Card]]? {
-        let palyer = playerCount + 1
-        var cardGame = [[Card]](repeating: [], count: palyer)
-        
-        for playerNumber in 0 ..< palyer {
-            for _ in 0 ..< type {
-                guard let card = cardDeck.removeOne() else {
-                    return nil
-                }
-                cardGame[playerNumber].append(card)
-                cardDeck.shuffle()
-            }
-        }
-        return cardGame
+    enum GameType: Int {
+        case sevenCard = 7
+        case fiveCard = 5
     }
     
+    var gameType: GameType = .fiveCard
+    
+    mutating func setGameType(_ type: GameType) {
+        self.gameType = type
+    }
     
     
 }
