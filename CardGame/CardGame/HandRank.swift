8d2ba6caa2d43c67befa3bfbaf3067b9c1e1bc0a
Kroisttt <yago0315@gmail.com>
2019-07-25 16:58:55 +0900

    Hand HandRank로 이름 수정

---
+++ CardGame/CardGame/HandRank.swift
@@ -0,0 +1,13 @@
+//
+//  HandRank.swift
+//  CardGame
+//
+//  Created by 이진영 on 25/07/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+enum HandRank: Int {
+    case highCard, pair, twoPairs, trips, straight, quads
+}
