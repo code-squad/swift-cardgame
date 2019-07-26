f8e40e86c0112e001afe7b1fcd9789ed1e8a8ad4
박준희 <junibibi04@gmail.com>
2019-07-26 15:05:02 +0900

    player에게 카드를 나눠주는 메소드 작성함.

--- CardGame/CardGame/Model/CardGame.swift
+++ CardGame/CardGame/Model/CardGame.swift
@@ -14,7 +14,7 @@ struct CardGame {
         case sevenCard = 7
         case fiveCard = 5
     }
-    
+    var deck = CardDeck()
     var gameType: GameType = .fiveCard
     var players = [Player]()
     
@@ -24,6 +24,22 @@ struct CardGame {
     
     mutating func setPlayer(_ playerCount: Int) {
         players = [Player](repeating: Player(), count: playerCount)
+    }
+    
+    // 특정 플레이어에케 카드를 게임종류에 따라 나눠줌.
+    private mutating func giveCards(to playerIndex: Int) {
+        for _ in 0 ..< gameType.rawValue {
+            guard let card = deck.removeOne() else {
+                return
+            }
+            players[playerIndex].addCard(card: card)
+        }
+    }
+    
+    mutating func giveCardsToAllPlayers() {
+        for playerindex in 0 ..< players.count {
+            giveCards(to: playerindex)
+        }
     }
     
 }
