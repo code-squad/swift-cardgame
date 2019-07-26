340347b4df1a8360c080573b7748dbdbf545a8e7
kimjieun <zieunv@gmail.com>
2019-07-26 15:50:10 +0900

    refactor: change HandRank Type

--- CardGame/CardGame/Player/HandRank.swift
+++ CardGame/CardGame/Player/HandRank.swift
@@ -8,14 +8,8 @@
 
 import Foundation
 
-enum HandRank: Double {
-    //  숫자가 없을 경우 또는 같은 핸즈인 경우 숫자가 높은 카드가 우승
-    case highcard = 0
-    case onePair = 2
-    case twoPair = 2.5
-    case triple = 3.0
-    case straight = 3.5
-    case fourcard = 4
+enum HandRank: Int {
+    case highcard, onePair, twoPair, triple, straight, quads
 }
 
 extension HandRank: Comparable {
