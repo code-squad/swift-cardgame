95fa551042efe2496c011cb3a6f0ecd0903ed3e4
박준희 <junibibi04@gmail.com>
2019-07-26 18:19:51 +0900

    GameError 케이스추가함

---
+++ CardGame/CardGame/Model/Error.swift
@@ -0,0 +1,14 @@
+//
+//  Error.swift
+//  CardGame
+//
+//  Created by JH on 26/07/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+enum GameError: Error {
+    case incorrectPlayerCount
+    case notEnoughCard
+}
