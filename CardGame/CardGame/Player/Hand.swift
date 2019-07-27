2cb93d636b13e6d719aee59afcd935f5b3443b1e
kimjieun <zieunv@gmail.com>
2019-07-27 15:15:59 +0900

    refactor: Reorganization of Code Structure

--- CardGame/CardGame/Player/Hand.swift
+++ CardGame/CardGame/Player/Hand.swift
@@ -22,6 +22,37 @@ class Hand: CustomStringConvertible {
         self.cards.removeAll()
     }
     
+    func isStraight() -> (Bool,Int) {
+        let cardCount = cards.reduce([Card.Ranks: Int](), { (cardCount: [Card.Ranks: Int], card: Card) -> [Card.Ranks: Int] in
+            var cardCount = cardCount
+            cardCount[card.rank] = (cardCount[card.rank] ?? 0) + 1
+            return cardCount
+        })
+        let cardSortValue = cardCount.sorted(by: { $0.0 < $1.0 })
+        for index in 0...cardSortValue.count {
+            let reversedIndex = cardSortValue.count-index
+            let rank1 = cardSortValue[reversedIndex].0.rawValue
+            let rank2 = cardSortValue[reversedIndex-1].0.rawValue
+            var maxRank = 0
+            var count = 0
+            
+            if rank1-1 == rank2 {
+                if count == 0 {
+                    maxRank = rank1
+                }
+                count = count + 1
+            } else {
+                maxRank = 0
+                count = 0
+            }
+            
+            if count >= 4 {
+                return (true,maxRank)
+            }
+        }
+        return (false,0)
+    }
+    
     func makeHandRank() -> (HandRank,Int) {
         let cardCount = cards.reduce([Card.Ranks: Int](), { (cardCount: [Card.Ranks: Int], card: Card) -> [Card.Ranks: Int] in
             var cardCount = cardCount
@@ -29,8 +60,8 @@ class Hand: CustomStringConvertible {
             return cardCount
         })
         
-        let filtered = cardCount.filter{ $0.value == 4 }
-        for (key,value) in filtered {
+        let quadCards = cardCount.filter{ $0.value == 4 }
+        for (key,value) in quadCards {
             if value == 4 {
                 return (HandRank.quads,key.rawValue)
             }
@@ -40,39 +71,36 @@ class Hand: CustomStringConvertible {
         if result.0 {
             return (HandRank.straight, result.1)
         }
-        return (HandRank.onePair,0)
-    }
-    
-        func isStraight() -> (Bool,Int) {
-            let cardCount = cards.reduce([Card.Ranks: Int](), { (cardCount: [Card.Ranks: Int], card: Card) -> [Card.Ranks: Int] in
-                var cardCount = cardCount
-                cardCount[card.rank] = (cardCount[card.rank] ?? 0) + 1
-                return cardCount
-            })
-            let cardSortValue = cardCount.sorted(by: { $0.0 < $1.0 })
-            for index in 0...cardSortValue.count {
-                let reversedIndex = cardSortValue.count-index
-                let rank1 = cardSortValue[reversedIndex].0.rawValue
-                let rank2 = cardSortValue[reversedIndex-1].0.rawValue
-                var maxRank = 0
-                var count = 0
-                
-                if rank1-1 == rank2 {
-                    if count == 0 {
-                        maxRank = rank1
-                    }
-                    count = count + 1
-                } else {
-                    maxRank = 0
-                    count = 0
-                }
-                
-                if count >= 4 {
-                    return (true,maxRank)
-                }
+        
+        let tripleCards = cardCount.filter{ $0.value == 3 }
+        for (key,value) in tripleCards {
+            if value == 3 {
+                return (HandRank.triple, key.rawValue)
             }
-            return (false,0)
         }
         
+        var max = 0
+        let anotherCards = cardCount.filter { $0.value == 2 }
+        
+        if anotherCards.count >= 2 {
+            for (key,value) in anotherCards {
+                if value == 2 {
+                    if max < key.rawValue {
+                        max = key.rawValue
+                    }
+                }
+            }
+            return (HandRank.twoPair, max)
+        }
+        
+        if anotherCards.count == 1 {
+            for (key,value) in anotherCards {
+                if value == 2 {
+                    return (HandRank.onePair, key.rawValue)
+                }
+            }
+        }
+        return (HandRank.highcard,0)
     }
+}
 
