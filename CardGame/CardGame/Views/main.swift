201cb6c3fcb0c76e2c0b6e2025eb50dab94c0572
kimjieun <zieunv@gmail.com>
2019-07-29 18:32:59 +0900

    feat: Add winner() and print Output

--- CardGame/CardGame/Views/main.swift
+++ CardGame/CardGame/Views/main.swift
@@ -18,7 +18,7 @@ func main() {
     while(cardGame.continueGame()){
         cardGame.gameStart()
         OutputView.printPlayers(game: cardGame)
-//        OutputView.printRank(game: cardGame)
+        OutputView.printResultPlayer(game: cardGame)
         
     }
 }
