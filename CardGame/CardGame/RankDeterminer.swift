b496da909bc199dfa20490b60b252b35d54f5e1e
Kroisttt <yago0315@gmail.com>
2019-07-29 14:12:58 +0900

    RankDeterminer 추가

---
+++ CardGame/CardGame/RankDeterminer.swift
@@ -0,0 +1,13 @@
+//
+//  RankDeterminer.swift
+//  CardGame
+//
+//  Created by 이진영 on 29/07/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+struct RankDeterminer {
+    
+}
