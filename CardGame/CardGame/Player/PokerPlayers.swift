43cecc79721c6e88e146bca61817b8733ea64b0a
kimjieun <zieunv@gmail.com>
2019-08-02 18:28:40 +0900

    refactor: make Players

--- CardGame/CardGame/Player/PokerPlayers.swift
+++ CardGame/CardGame/Player/PokerPlayers.swift
@@ -7,3 +7,53 @@
 //
 
 import Foundation
+
+class PokerPlayers {
+    private var players = [PokerPlayer]()
+    private var dealer: Dealer
+    private let gameMode: CardGameMode
+    private let numberOfplayers: Int
+    
+    
+    init(dealer: Dealer, gameMode: CardGameMode, players: Int) {
+          self.dealer = dealer
+          self.gameMode = gameMode
+          self.numberOfplayers = players
+          reset()
+          setPlayer()
+          setCards()
+    }
+    
+    func playersInfo(handler: (String, String) -> ()) {
+        players.forEach { player in handler(player.name, String(describing: player.hand))}
+    }
+    
+    func winner() -> PokerPlayer {
+        let winner = players.max()!
+        return winner
+    }
+    
+    func reset() {
+        self.players.forEach{$0.hand.clear()}
+    }
+    
+    func setPlayer() {
+        for order in 1...numberOfplayers {
+            players.append(PokerPlayer(number: order, name:"참가자"))
+        }
+        players.append(dealer as! PokerPlayer)
+    }
+    
+    func setCards() {
+        let numberOfCards = gameMode.numberOfCards
+        
+        for _ in 1...numberOfCards {
+            for player in players {
+                guard let card = self.dealer.give() else {
+                    return
+                }
+                player.receive(newCards: card)
+            }
+        }
+    }
+}
