9c06ebbb1602b5fea322b5289b469a97cca38457
Kroisttt <yago0315@gmail.com>
2019-08-22 14:11:20 +0900

    HandRankTest 추가

---
+++ CardGame/CardGameTest/CardGameTest.swift
@@ -0,0 +1,17 @@
+//
+//  CardGameTest.swift
+//  CardGameTest
+//
+//  Created by 이진영 on 20/08/2019.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import XCTest
+
+class CardGameTest: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
