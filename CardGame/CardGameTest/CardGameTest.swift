701a7090cf2a0665de1431c0cf64fd5d012c5547
Kroisttt <yago0315@gmail.com>
2019-08-22 16:05:16 +0900

    Player 테스트 코드 추가

--- CardGame/CardGameTest/CardGameTest.swift
+++ CardGame/CardGameTest/CardGameTest.swift
@@ -9,6 +9,20 @@
 import XCTest
 
 class CardGameTest: XCTestCase {
+    let ace = Card(suit: .clubs, rank: .ace)
+    let two = Card(suit: .clubs, rank: .two)
+    let three = Card(suit: .clubs, rank: .three)
+    let four = Card(suit: .clubs, rank: .four)
+    let five = Card(suit: .clubs, rank: .five)
+    let six = Card(suit: .clubs, rank: .six)
+    let seven = Card(suit: .clubs, rank: .seven)
+    let eight = Card(suit: .clubs, rank: .eight)
+    let nine = Card(suit: .clubs, rank: .nine)
+    let ten = Card(suit: .clubs, rank: .ten)
+    let jack = Card(suit: .clubs, rank: .jack)
+    let queen = Card(suit: .clubs, rank: .queen)
+    let king = Card(suit: .clubs, rank: .king)
+    
     func testDealerHaveCard() {
         let cardDeck = CardDeck()
         let dealer = Dealer(cardDeck: cardDeck)
@@ -27,5 +41,19 @@ class CardGameTest: XCTestCase {
         }
         
         XCTAssertNil(dealer.give())
+    }
+    
+    func testPlayer() {
+        let player1 = Player(name: "1")
+        let player2 = Player(name: "2")
+        
+        let pairCards = [ace, ace, two, four, six]
+        let quadsCards = [three, three, three, three, king]
+        
+        pairCards.forEach { player1.receive(card: $0) }
+        quadsCards.forEach { player2.receive(card: $0) }
+        
+        XCTAssertTrue(player1 < player2)
+        XCTAssertFalse(player1 == player2)
     }
 }
