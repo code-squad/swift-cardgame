71a40e9b867839b886eb5ccb246ba4eed05a2af8
hoemoon <hoemoon.song@gmail.com>
2019-09-25 23:34:22 +0900

    Implement requirements

---
+++ CardGame/CardGame/Displayable.swift
@@ -0,0 +1,13 @@
+//
+//  Displayable.swift
+//  CardGame
+//
+//  Created by hoemoon on 2019/09/25.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+protocol Displayable {
+	var text: String { get }
+}
---
+++ CardGame/CardGame/OutputView.swift
@@ -0,0 +1,16 @@
+//
+//  OutputView.swift
+//  CardGame
+//
+//  Created by hoemoon on 2019/09/25.
+//  Copyright © 2019 JK. All rights reserved.
+//
+
+import Foundation
+
+struct OutputView {
+	static func display(_ displayable: Displayable) {
+		print(displayable.text)
+	}
+}
+
