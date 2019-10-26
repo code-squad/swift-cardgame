243575997a8879fc886b810135df32266c330a1e
jypthemiracle <hophfg@yahoo.co.kr>
2019-10-26 12:34:03 +0900

    (신규) main.swift 생성

---
+++ CardGame/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  swift-cardgame
+//
+//  Created by Jin Hyung Park on 10/26/19.
+//
+
+import Foundation
+
+func main() {
+    let deck = Deck()
+    playgame.play(_: Deck)
+}
+
+main()
